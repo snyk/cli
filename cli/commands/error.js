8077864a3f7d9585c65417e94ba7ea31e5d0abe3
Remy Sharp <remy@remysharp.com>
2015-09-16 14:13:52 +0100

    refactor: .snyk.yaml -> .snyk

--- cli/commands/error.js
+++ cli/commands/error.js
@@ -8,5 +8,5 @@ module.exports.ECONNREFUSED = 'Failed to connect to remote resource';
 module.exports['404'] = 'Unknown API endpoint';
 module.exports.Unauthorized = 'Unauthorized: please ensure you are ' +
   'logged using `snyk login`';
-module.exports.MISSING_DOTFILE = 'Cannot find .snyk.yaml in this ' +
+module.exports.MISSING_DOTFILE = 'Cannot find .snyk in this ' +
   'project, make to create one with `snyk protect --interactive`';
