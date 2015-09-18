f782231474c9c7144121fd175053ed86d6c8ce9f
Remy Sharp <remy@remysharp.com>
2015-09-18 18:37:35 +0100

    test: ignore format is correct
    (currently failing)

--- test/protect-ignore.test.js
+++ test/protect-ignore.test.js
@@ -2,27 +2,29 @@
 var snyk = require('..');
 var protect = require('../lib/protect');
 var test = require('tape');
+var vulns = require('./fixtures/test-jsbin-vulns.json');
 
 test('protect generates detailed ignore format', function (t) {
   t.plan(2);
 
   var save = snyk.dotfile.save;
-  snyk.dotfile.save = function (string) {
-    t.equal(string, 'remy', 'dotfile format is correct');
-
-    // restore
-    snyk.dotfile.save = save;
-  };
-
-  snyk.test('jsbin@3.11.23').then(function (res) {
+  Promise.resolve(vulns).then(function (res) {
     var vulns = res.vulnerabilities.filter(function (vuln) {
       return (vuln.name === 'semver' && vuln.version === '3.0.1');
     });
 
     t.equal(vulns.length, 1, 'narrowed to test vuln');
 
-    protect.ignore(vulns);
+    snyk.dotfile.save = function (string) {
+      t.equal(string, 'remy', 'dotfile format is correct');
+    };
+
+    return protect.ignore(vulns);
   }).catch(function (e) {
+    console.log(e.stack);
     t.fail(e);
+  }).then(function () {
+    // restore
+    snyk.dotfile.save = save;
   });
 });
