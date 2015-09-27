f456655dd66ba93a95798bf34a65ceb727c4a7ce
Danny Grander <danny@snyk.io>
2015-09-27 22:26:31 +0100

    Fixed unauthorized error message, to refer to snyk auth command instead of the non-existant snyk login

--- cli/commands/error.js
+++ cli/commands/error.js
@@ -8,6 +8,6 @@ module.exports.ECONNREFUSED = 'Failed to connect snyk.io';
 module.exports['404'] = 'Unknown API endpoint';
 module.exports['411'] = 'Unknown API endpoint'; // isn't 404, but it is...
 module.exports.Unauthorized = 'Unauthorized: please ensure you are ' +
-  'logged using `snyk login`';
+  'logged using `snyk auth`';
 module.exports.MISSING_DOTFILE = 'Cannot find .snyk in this ' +
   'project, make to create one with `snyk protect --interactive`';
