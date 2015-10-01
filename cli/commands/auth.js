e4d5cedde0d7235cd6a00b4e8f0c1a72572456ce
Remy Sharp <remy@remysharp.com>
2015-10-01 22:45:36 +0100

    feat: support callback auth process
    Fixes https://github.com/Snyk/general/issues/98

--- cli/commands/auth.js
+++ cli/commands/auth.js
@@ -8,232 +8,110 @@ module.exports = auth;
 
 var Promise = require('es6-promise').Promise; // jshint ignore:line
 var debug = require('debug')('snyk');
-var pkg = require('../../package.json');
 var open = require('open');
 var snyk = require('../../lib/');
 var config = require('../../lib/config');
-var inquirer = require('inquirer');
-var validator = require('validator');
-var crypto = require('crypto');
-var base32 = require('base32');
-var request = require('request');
+var request = require('../../lib/request');
 var url = require('url');
-var _ = require('lodash');
-
-var passwordStorage = passwordStorageService('github');
+var uuid = require('node-uuid');
+var Spinner = require('cli-spinner').Spinner;
 
 var apiUrl = url.parse(config.API);
-var authUrl = apiUrl.protocol + '//' + apiUrl.host
+var authUrl = apiUrl.protocol + '//' + apiUrl.host;
 
-function passwordStorageService(service) {
-  var key = pkg.name + ':' + service;
+function githubAuth() {
+  var token = uuid.v4(); // generate a random key
 
-  var keytar = false;
+  var url = authUrl + '/login?token=' + token;
 
-  // allow for an optional dependency: keytar, since not all systems will
-  // be able to build it (travis being one...)
-  try {
-    keytar = require('keytar');
-  } catch (e) {
-    var noop = function () {
-      return false;
-    };
-    keytar = {
-      getPassword: noop,
-      replacePassword: noop,
-    };
-  }
+  var msg =
+    '\nNow redirecting you to our github auth page, go ahead and log in,\n' +
+    'and once the auth is complete, return to this prompt and you\'ll\n' +
+    'be ready to start using snyk.\n\nIf you can\'t wait use this url:\n' +
+    url + '\n';
 
-  return {
-    get: function (username) {
-      debug('getting from keytar');
-      return keytar.getPassword(key, username);
-    },
-    set: function (username, password) {
-      debug('setting keytar');
-      keytar.replacePassword(key, username, password);
-    },
-  };
-}
+  console.log(msg.replace(/^(.)/gm, '   $1'));
 
-function ask2FA(cb) {
-  debug('require 2FA');
-  inquirer.prompt([{
-    type: 'input',
-    name: 'code',
-    message: 'What is your GitHub two-factor authentication code?',
-    validate: validator.isNumeric,
-  }, ], function (answers) {
-    cb(answers.code);
+  var spinner = new Spinner('   %s  Waiting...');
+  spinner.setSpinnerDelay(75);
+  spinner.setSpinnerString(2);
+  spinner.start();
+
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      open(url);
+    }, 2000);
+    // start checking the token immediately in case they've already
+    // opened the url manually
+    resolve(testAuthComplete(token).then(function (res) {
+      spinner.stop();
+      // send a nl
+      console.log('');
+      return res;
+    }));
   });
 }
 
-function randomId() {
-  return base32.encode(crypto.randomBytes(4));
-}
-
-function createAuthorization(info, cb) {
-  var log = info.log;
-
+function testAuthComplete(token) {
   var payload = {
-    method: 'POST',
-    url: info.github.endpoint + '/authorizations',
-    json: true,
-    auth: info.github,
-    headers: {
-      'User-Agent': 'snyk cli',
-      'X-GitHub-OTP': info.github.code,
-    },
     body: {
-      scopes: [
-        'user:email',
-      ],
-      fingerprint: 'snyk-' + randomId(),
-      note: 'snyk cli',
+      token: token,
     },
+    url: config.API + '/verify/callback',
+    json: true,
+    method: 'post',
   };
 
-  request(payload, function (err, response, body) {
-    debug('response: %s', response.statusCode, body);
-    if (err) {
-      return cb(err);
-    }
-
-    var status = response.statusCode;
-
-    if (status === 201) {
-      return cb(null, body);
-    }
-
-    if (status === 401 && response.headers['x-github-otp']) {
-      var type = response.headers['x-github-otp'].split('; ')[1];
-
-      if (info.github.retry) {
-        log.warn('Invalid two-factor authentication code.');
-      } else {
-        log.info('Two-factor authentication code needed via %s.', type);
+  return new Promise(function (resolve, reject) {
+    debug(payload);
+    request(payload, function (error, res, body) {
+      debug(error, res.statusCode, body);
+      if (error) {
+        return reject(error);
       }
 
-      ask2FA(function (code) {
-        info.github.code = code;
-        info.github.retry = true;
-        createAuthorization(info, cb);
-      });
 
-      return;
-    }
+      if (res.statusCode !== 200) {
+        var e = new Error(body.message);
+        e.code = res.statusCode;
+        return reject(e);
+      }
 
-    cb(new Error('Could not login to GitHub.'));
+      // we have success
+      if (body.api) {
+        return resolve({
+          res: res,
+          body: body,
+        });
+      }
+
+      // we need to wait and poll again in a moment
+      setTimeout(function () {
+        resolve(testAuthComplete(token));
+      }, 1000);
+    });
   });
 }
 
-function github() {
+function verifyAPI(api) {
+  var payload = {
+    body: {
+      api: api,
+    },
+    method: 'POST',
+    url: config.API + '/verify/token',
+    json: true,
+  };
+
   return new Promise(function (resolve, reject) {
-    var info = {
-      options: { keychain: true, 'ask-for-passwords': false, },
-    };
-
-    info.log = require('../log')('warn');
-
-    var prompts = [{
-      type: 'rawlist',
-      name: 'webauth',
-      message: 'How would you like to authenticate to Snyk?',
-      choices: [{
-          value: 'github',
-          name: 'Sign in on this prompt using Github credentials',
-        }, {
-          value: 'browser',
-          name: 'Browse to get an API key, and rerun snyk auth with the key',
-        }, ],
-      default: 0,
-      when: function (answers) {
-        if (answers.reauth === false) {
-          return false;
-        }
-
-        return true;
-      },
-
-    }, {
-      type: 'input',
-      name: 'username',
-      message: 'What is your GitHub username?',
-      default: process.env.USER,
-      validate: _.ary(_.bind(validator.isLength, validator, _, 1), 1),
-      when: function (answers) {
-        if (answers.reauth === false) {
-          return false;
-        }
-
-        if (answers.webauth === 'browser') {
-          return false;
-        }
-
-        return true;
-      },
-    }, {
-      type: 'password',
-      name: 'password',
-      message: 'What is your GitHub password?',
-      validate: _.ary(_.bind(validator.isLength, validator, _, 1), 1),
-      when: function (answers) {
-        if (answers.reauth === false) {
-          return false;
-        }
-
-        if (answers.webauth === 'browser') {
-          return false;
-        }
-
-        if (!info.options.keychain) {
-          return true;
-        }
-        if (info.options['ask-for-passwords']) {
-          return true;
-        }
-        return !passwordStorage.get(answers.username);
-      },
-    }, ];
-
-    // if the user has already authed - check if they want to re-auth.
-    if (snyk.api) {
-      prompts.unshift({
-        type: 'confirm',
-        default: false,
-        name: 'reauth',
-        message: 'You have a Snyk API key already, re-auth?',
-      });
-    }
-
-    inquirer.prompt(prompts, function (answers) {
-      if (answers.reauth === false) {
-        return reject(new Error('Cancelled authentication'));
+    request(payload, function (error, res, body) {
+      if (error) {
+        return reject(error);
       }
-      if (answers.webauth === 'browser') {
-        open(authUrl);
-        return reject(new Error('After logging in at ' + authUrl +
-          ', run \'snyk auth <KEY>\' command again'));
-      }
-      answers.password = answers.password ||
-                         passwordStorage.get(answers.username);
 
-      info.github = answers;
-      info.github.endpoint = info.ghepurl || 'https://api.github.com';
-
-      createAuthorization(info, function (err, data) {
-        if (err) {
-          info.log.error('Could not login to GitHub. Check your credentials.');
-          return reject(err);
-        }
-
-        if (info.options.keychain) {
-          passwordStorage.set(info.github.username, info.github.password);
-        }
-
-        info.github.token = data.token;
-        info.log.info('Successfully created GitHub token.');
-        resolve(info);
+      resolve({
+        res: res,
+        body: body,
       });
     });
   });
@@ -243,52 +121,27 @@ function auth(api) {
   var promise;
   if (api) {
     // user is manually setting the API key on the CLI - let's trust them
-    promise = new Promise(function (resolve) {
-      resolve({
-        body: {
-          api: api,
-        },
-        method: 'POST',
-        url: config.API + '/verify/token',
-        json: true,
-      });
-    });
+    promise = verifyAPI(api);
   } else {
-    promise = github().then(function (info) {
-      return {
-        body: {
-          token: info.github.token,
-          username: info.github.username,
-        },
-        method: 'POST',
-        url: config.API + '/verify/github',
-        json: true,
-      };
-    });
+    promise = githubAuth();
   }
 
-  return promise.then(function (payload) {
-    return new Promise(function (resolve, reject) {
-      debug(payload);
-      request(payload, function (error, res, body) {
-        debug(error, body);
-        if (error) {
-          return reject(error);
-        }
+  return promise.then(function (data) {
+    var res = data.res;
+    var body = res.body;
+    debug(body);
 
-        if (res.statusCode === 200 || res.statusCode === 201) {
-          snyk.config.set('api', body.api);
-          resolve('Your account has been authenicated. Snyk is now ready to ' +
-            'be used.');
-        } else if (body.message) {
-          var e = new Error(body.message);
-          e.code = res.statusCode;
-          reject(e);
-        } else {
-          reject(new Error('Authentication failed. Please check the API ' +
-            'key on https://snyk.io'));
-        }
-      });
-    });
+    if (res.statusCode === 200 || res.statusCode === 201) {
+      snyk.config.set('api', body.api);
+      return 'Your account has been authenicated. Snyk is now ready to ' +
+        'be used.';
+    } else if (body.message) {
+      var error = new Error(body.message);
+      error.code = res.statusCode;
+      throw error;
+    } else {
+      throw new Error('Authentication failed. Please check the API ' +
+        'key on https://snyk.io');
+    }
   });
 }
--- cli/commands/error.js
+++ cli/commands/error.js
@@ -5,8 +5,8 @@ module.exports = function error(command) {
 };
 
 module.exports.ECONNREFUSED = 'Failed to connect snyk.io';
-module.exports['404'] = 'Unknown API endpoint';
-module.exports['411'] = 'Unknown API endpoint'; // isn't 404, but it is...
+module.exports['404'] = 'Unknown API endpoint (404)';
+module.exports['411'] = 'Unknown API endpoint (411)'; // isn't 404, but it is...
 module.exports.Unauthorized = 'Unauthorized: please ensure you are ' +
   'logged using `snyk auth`';
 module.exports.MISSING_DOTFILE = 'Cannot find .snyk in this ' +
