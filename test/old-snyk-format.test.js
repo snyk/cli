2becc217f91e763d788359bef4f0a99e1b8904c4
Remy Sharp <remy@remysharp.com>
2015-10-21 12:22:48 +0100

    fix: gracefully handle old alpha .snyk file

---
+++ test/old-snyk-format.test.js
@@ -0,0 +1,16 @@
+var protect = require('../lib/protect');
+var test = require('tape');
+
+test('test sensibly bails if gets an old .snyk format', function (t) {
+  var vulns2 = require('./fixtures/test-jsbin-vulns-updated.json');
+  var dotfile = require('../lib/dotfile');
+
+  t.plan(1);
+  dotfile.load(__dirname + '/fixtures/old-snyk-config').then(function (config) {
+    return protect.filterIgnored(config.ignore, vulns2.vulnerabilities);
+  }).then(function (res) {
+    t.fail('was expecting an error, got ' + JSON.stringify(res));
+  }).catch(function (e) {
+    t.equal(e.code, 'OLD_DOTFILE_FORMAT');
+  });
+});
