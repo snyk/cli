973318f9de63dcd1a0457ab64b99615240188bc9
Remy Sharp <remy@remysharp.com>
2015-11-27 16:56:59 +0000

    fix: show correct error if policy is missing

--- cli/commands/policy.js
+++ cli/commands/policy.js
@@ -3,5 +3,12 @@ module.exports = displayPolicy;
 var policy = require('../../lib/policy');
 
 function displayPolicy(path) {
-  return policy.load(path || process.cwd()).then(policy.display);
+  return policy.load(path || process.cwd())
+    .then(policy.display)
+    .catch(function (e) {
+      if (e.code === 'ENOENT') {
+        e.code = 'MISSING_DOTFILE';
+      }
+      throw e;
+    });
 }
