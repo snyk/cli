fa98b9e92c4f6c70f393c846e6e7f502732cb7ad
Remy Sharp <remy@remysharp.com>
2016-01-07 11:37:52 +0000

    test: pin fixtures of uglify deps

--- test/api-key.test.js
+++ test/api-key.test.js
@@ -3,13 +3,14 @@ var snyk = require('../lib');
 
 test('api key', function (t) {
   var fromConfig = snyk.config.get('api');
-  console.log(fromConfig);
   t.equal(fromConfig, snyk.api, 'current api value matches config store');
   var value = '_____test_____';
   snyk.config.set('api', value);
   t.equal(value, snyk.api, 'dynamically set value is correct');
   if (fromConfig !== undefined) {
     snyk.config.set('api', fromConfig);
+  } else {
+    snyk.config.del('api');
   }
   t.end();
 });
