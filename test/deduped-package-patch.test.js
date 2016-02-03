d5fe9627d598e519ee6881dae38a4ab7e67b6422
Remy Sharp <remy@remysharp.com>
2016-02-03 11:46:35 +0000

    test: update tests with new module resolution

--- test/deduped-package-patch.test.js
+++ test/deduped-package-patch.test.js
@@ -1,14 +1,9 @@
-var test = require('tape');
+var test = require('tap').test;
 var protect = require('../lib/protect');
 var answers = require('./fixtures/deduped-dep/answers.json');
 
 test('npm deduped packages are found and patched correctly', function (t) {
   protect.patch(answers, false, __dirname + '/fixtures/deduped-dep/').then(function (res) {
     t.equal(Object.keys(res.patch).length, 1, 'found and patched 1 file');
-    t.end();
-  }).catch(function (e) {
-    console.log(e.stack);
-    t.fail('failed to complete patch');
-    t.end();
-  });
+  }).catch(t.threw).then(t.end);
 });
