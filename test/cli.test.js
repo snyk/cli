830de2d1b14c3026941aef63e2750b6297823658
Remy Sharp <remy@remysharp.com>
2016-02-18 14:19:46 +0000

    test: remove private deps for tests
    Should also speed up the tests, since the cli.test.js takes anything from 30-60 seconds - which is a bit too close to the mark.

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,18 +1,3 @@
-require('babel/register')({
-  ignore: function (filename) {
-    if (filename.indexOf('@snyk/vuln/lib') !== -1) {
-      return false;
-    }
-    if (filename.indexOf('@snyk/registry/test') !== -1) {
-      return false;
-    }
-    if (filename.indexOf('@snyk/registry/lib') !== -1) {
-      return false;
-    }
-    return true;
-  },
-//  only: /@snyk\/register\//,
-});
 var test = require('tap-only');
 var testUtils = require('./utils');
 var apiKey = '123456789';
@@ -29,16 +14,14 @@ process.env.SNYK_API = 'http://localhost:' + port + '/api/v1';
 process.env.SNYK_HOST = 'http://localhost:' + port;
 process.env.LOG_LEVEL = 0;
 
-var server = require('@snyk/registry/test/fixtures/demo-registry-server');
-var db = require('@snyk/registry/lib/models');
-var utils = require('@snyk/registry/test/fixtures/utils');
+var server = require('./cli-server')(process.env.SNYK_API, apiKey);
 
 // ensure this is required *after* the demo server, since this will
 // configure our fake configuration too
 var cli = require('../cli/commands');
 
 test('setup', function (t) {
-  t.plan(4);
+  t.plan(3);
   cli.config('get', 'api').then(function (key) {
     oldkey = key; // just in case
     t.pass('existing user config captured');
@@ -49,39 +32,19 @@ test('setup', function (t) {
     t.pass('existing user endpoint captured');
   });
 
-  server(port, function () {
+  server.listen(port, function () {
     t.pass('started demo server');
-
-    utils.pgSetup().then(function () {
-      t.pass('setup pg database');
-    });
   });
 });
 
-test('prime database', function (t) {
-  t.plan(3);
-
-  db.models.User.create({
-    ApiKeys: [{
-      key: apiKey,
-    }, ],
-    email: 'test@example.com',
-  }, {
-    include: [db.models.ApiKey],
-  }).then(function () {
-    t.pass('demo user created');
-
-    return cli.config('set', 'api=' + apiKey).then(function () {
-      t.pass('api key set');
-    });
+test('prime config', function (t) {
+  cli.config('set', 'api=' + apiKey).then(function () {
+    t.pass('api key set');
   }).then(function () {
     return cli.config('unset', 'endpoint').then(function () {
       t.pass('endpoint removed');
     });
-  }).catch(function (err) {
-    t.fail(err);
-    return t.bailout();
-  });
+  }).catch(t.bailout).then(t.end);
 });
 
 test('cli', function (t) {
@@ -123,29 +86,9 @@ test('multiple test arguments', function (t) {
 test('auth via key', function (t) {
   t.plan(1);
 
-  var spy = sinon.spy(function (req, callback) {
-    process.nextTick(function () {
-      var body = {
-        ok: true,
-        api: req.body.api,
-      };
-      callback(null, {
-        statusCode: 200,
-        body: body,
-      }, body);
-    });
-  });
-
-  var auth = proxyquire('../cli/commands/auth', {
-    '../../lib/request': spy,
-  });
-
-  auth(apiKey).then(function (res) {
+  cli.auth(apiKey).then(function (res) {
     t.notEqual(res.toLowerCase().indexOf('ready'), -1, 'snyk auth worked');
-  }).catch(function (e) {
-    console.log(e.stack);
-    t.fail(e);
-  });
+  }).catch(t.threw);
 });
 
 test('auth via invalid key', function (t) {
@@ -153,23 +96,7 @@ test('auth via invalid key', function (t) {
 
   var errors = require('../lib/error');
 
-  var spy = sinon.spy(function (req, callback) {
-    process.nextTick(function () {
-      var body = {
-        ok: false,
-      };
-      callback(null, {
-        statusCode: 401,
-        body: body,
-      }, body);
-    });
-  });
-
-  var auth = proxyquire('../cli/commands/auth', {
-    '../../lib/request': spy,
-  });
-
-  auth('_____________').then(function (res) {
+  cli.auth('_____________').then(function (res) {
     t.fail('auth should not succeed: ' + res);
   }).catch(function (e) {
     var message = chalk.stripColor(errors.message(e));
@@ -185,25 +112,7 @@ test('auth via github', function (t) {
     tokenRequest.token = tokenRequest.query.split('=').pop();
   });
 
-  var spy = sinon.spy(function (req, callback) {
-    process.nextTick(function () {
-      var body = {};
-
-      if (tokenRequest !== null) {
-        if (req.body.token === tokenRequest.token) {
-          body.api = apiKey;
-        }
-      }
-
-      callback(null, {
-        statusCode: 200,
-        body: body,
-      }, body);
-    });
-  });
-
   var auth = proxyquire('../cli/commands/auth', {
-    '../../lib/request': spy,
     'open': openSpy,
     '../../lib/is-ci': false,
   });
@@ -218,60 +127,6 @@ test('auth via github', function (t) {
   });
 });
 
-test('wizard and multi-patch', function (t) {
-
-  var authSpy = sinon.spy(function (req, callback) {
-    process.nextTick(function () {
-      var body = {
-        ok: true,
-        api: req.body.api,
-      };
-      callback(null, {
-        statusCode: 200,
-        body: body,
-      }, body);
-    });
-  });
-
-  var auth = proxyquire('../cli/commands/auth', {
-    '../../lib/request': authSpy
-  });
-
-  var vulns = require('./fixtures/uglify-contrived.json');
-  var answers = require('./fixtures/wizard-patch-answers.json');
-
-  var wizard = proxyquire('../cli/commands/protect/wizard', {
-    '../../../lib/': {
-      test: function () {
-        return Promise.resolve(vulns);
-      }
-    },
-    inquirer: {
-      prompt: function (questions, callback) {
-        if (questions.name === 'misc-start-over') {
-          return callback({ 'misc-start-over': false });
-        }
-
-        return callback(answers);
-      },
-    },
-  });
-
-  var cwd = process.cwd();
-  process.chdir(__dirname + '/fixtures/uglify-package');
-  var unhook = testUtils.silenceLog();
-  wizard().then(function () {
-    t.pass('ok');
-  }).catch(function (e) {
-    t.fail(e);
-  }).then(function () {
-    unhook();
-    process.chdir(cwd);
-    t.end();
-  });
-
-});
-
 test('teardown', function (t) {
   t.plan(4);
 
@@ -280,7 +135,7 @@ test('teardown', function (t) {
   delete process.env.SNYK_PORT;
   t.notOk(process.env.SNYK_PORT, 'fake env values cleared');
 
-  server.app.close(function () {
+  server.close(function () {
     t.pass('server shutdown');
     var key = 'set';
     var value = 'api=' + oldkey;
