b022b6f8dee4c5b8f2419e0492252ef7af88bdc7
Remy Sharp <remy@remysharp.com>
2016-03-03 11:48:52 +0000

    test: don't be so brittle when parsing text

--- test/policy-trust-deep.test.js
+++ test/policy-trust-deep.test.js
@@ -7,7 +7,7 @@ test('`snyk test` sees suggested ignore policies', function (t) {
   cli.test(dir).catch(function (res) {
     var vulns = res.message.toLowerCase();
     t.notEqual(vulns.indexOf('suggests ignoring this issue, with reason: test trust policies'), -1, 'found suggestion to ignore');
-    t.equal(vulns.split('\n\n').length, 4 + 1, 'all 4 vulns found');
+    t.equal(count('vulnerability found', vulns), 4, 'all 4 vulns found');
   }).then(t.end);
 });
 
@@ -15,6 +15,11 @@ test('`snyk test` ignores when applying `--trust-policies`', function (t) {
   cli.test(dir, { 'trust-policies': true }).catch(function (res) {
     var vulns = res.message.trim();
     // note: it's 2 vulns + the summary line
-    t.equal(vulns.split('\n\n').length, 2 + 1, 'only 2 vulns left');
+    t.equal(count('vulnerability found', vulns), 2, 'only 2 vulns left');
   }).then(t.end);
 });
+
+function count(needle, haystack) {
+  return (haystack.toLowerCase().match(new RegExp(needle.toLowerCase(), 'g')) ||
+   []).length;
+}
