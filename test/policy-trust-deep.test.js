26078bb79db36c45e64b9370bcc6d6bc9e5c8023
Remy Sharp <remy@remysharp.com>
2016-03-31 15:05:45 +0100

    test: update vuln count
    Due to new vuln in request

--- test/policy-trust-deep.test.js
+++ test/policy-trust-deep.test.js
@@ -4,19 +4,19 @@ var cli = require('../cli/commands');
 var dir = __dirname + '/fixtures/qs-package';
 
 test('`snyk test` sees suggested ignore policies', function (t) {
-  cli.test(dir).catch(function (res) {
+  return cli.test(dir).catch(function (res) {
     var vulns = res.message.toLowerCase();
     t.notEqual(vulns.indexOf('suggests ignoring this issue, with reason: test trust policies'), -1, 'found suggestion to ignore');
-    t.equal(count('vulnerability found', vulns), 4, 'all 4 vulns found');
-  }).then(t.end);
+    t.equal(count('vulnerability found', vulns), 5, 'all 4 vulns found');
+  });
 });
 
 test('`snyk test` ignores when applying `--trust-policies`', function (t) {
-  cli.test(dir, { 'trust-policies': true }).catch(function (res) {
+  return cli.test(dir, { 'trust-policies': true }).catch(function (res) {
     var vulns = res.message.trim();
     // note: it's 2 vulns + the summary line
-    t.equal(count('vulnerability found', vulns), 2, 'only 2 vulns left');
-  }).then(t.end);
+    t.equal(count('vulnerability found', vulns), 3, 'only 3 vulns left');
+  });
 });
 
 function count(needle, haystack) {
