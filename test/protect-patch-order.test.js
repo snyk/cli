a72b468192fab7dbc63997424c6ed84643c4d3af
Remy Sharp <remy@remysharp.com>
2016-04-19 14:05:59 +0100

    style: linting fixes

--- test/protect-patch-order.test.js
+++ test/protect-patch-order.test.js
@@ -1,15 +1,14 @@
-var patch = require('../lib/protect/patch');
-var test = require('tap-only');
+var dedupe = require('../lib/protect/dedupe-patches');
+var test = require('tap').test;
 var Promise = require('es6-promise').Promise; // jshint ignore:line
 var policy = require('snyk-policy');
 var dir = __dirname + '/fixtures/bugs/SC-1076';
 var vulns = require(dir + '/vulns.json');
 
 test('protect patches in the correct order - SC-1076', t => {
-  return policy.load(dir).then(policy => {
-    if (policy.patch) {
-      return patch(vulns, { 'dry-run': true });
-    }
-    return 'Nothing to do';
-  });
+  var res = dedupe(vulns.vulnerabilities);
+  t.equal(res.packages[0].id, 'npm:sequelize:20160329', 'latest patch is picked');
+  t.equal(res.removed[0].id, 'npm:sequelize:20160115', 'old patch is removed');
+  t.pass('ok');
+  t.end();
 });
