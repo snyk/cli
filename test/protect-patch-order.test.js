5e325f037caa61b6595030e7d5095068a893f47c
Remy Sharp <remy@remysharp.com>
2016-04-19 18:31:46 +0100

    chore: de-ES6ify

--- test/protect-patch-order.test.js
+++ test/protect-patch-order.test.js
@@ -5,7 +5,7 @@ var policy = require('snyk-policy');
 var dir = __dirname + '/fixtures/bugs/SC-1076';
 var vulns = require(dir + '/vulns.json');
 
-test('protect patches in the correct order - SC-1076', t => {
+test('protect patches in the correct order - SC-1076', function (t) {
   var res = dedupe(vulns.vulnerabilities);
   t.equal(res.packages[0].id, 'npm:sequelize:20160329', 'latest patch is picked');
   t.equal(res.removed[0].id, 'npm:sequelize:20160115', 'old patch is removed');
