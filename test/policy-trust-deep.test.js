f89d19420223beb9c7cf7bfb72ad4dc740d7b496
Danny Grander <grnd@users.noreply.github.com>
2016-07-25 20:48:04 +0300

    fix: skip on missing patches that were required by policy (#117)

--- test/policy-trust-deep.test.js
+++ test/policy-trust-deep.test.js
@@ -7,7 +7,7 @@ test('`snyk test` sees suggested ignore policies', function (t) {
   return cli.test(dir).catch(function (res) {
     var vulns = res.message.toLowerCase();
     t.notEqual(vulns.indexOf('suggests ignoring this issue, with reason: test trust policies'), -1, 'found suggestion to ignore');
-    t.equal(count('vulnerability found', vulns), 5, 'all 4 vulns found');
+    t.equal(count('vulnerability found', vulns), 6, 'all 6 vulns found');
   });
 });
 
@@ -15,7 +15,7 @@ test('`snyk test` ignores when applying `--trust-policies`', function (t) {
   return cli.test(dir, { 'trust-policies': true }).catch(function (res) {
     var vulns = res.message.trim();
     // note: it's 2 vulns + the summary line
-    t.equal(count('vulnerability found', vulns), 3, 'only 3 vulns left');
+    t.equal(count('vulnerability found', vulns), 4, 'only 4 vulns left');
   });
 });
 
