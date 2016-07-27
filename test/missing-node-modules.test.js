13df4e4fefcbd648a5c58ed11b069de6a55d3f95
Remy Sharp <remy@remysharp.com>
2016-07-27 17:01:00 +0100

    fix: error if node_modules
    Also handle tests that run against directly vulnerable packages (before it would throw an unexpected error)

---
+++ test/missing-node-modules.test.js
@@ -0,0 +1,26 @@
+const test = require('tap-only');
+const fs = require('then-fs');
+const dir = __dirname + '/fixtures/bugs/SC-1615/';
+
+test('throws when missing node_moduless', t => {
+  var snyk = require('../');
+  return snyk.test(dir).then(() => {
+    t.fail('should have thrown');
+  }).catch(e => {
+    t.matches(e.message, /Missing node_modules folder/);
+  });
+});
+
+test('is able to test root level vulns', t => {
+  var snyk = require('../');
+  process.chdir(dir);
+  return fs.mkdir('node_modules').then(() => {
+    return snyk.test(process.cwd()).then(res => {
+      t.notOk(res.ok);
+    }).catch(e => {
+      t.fail(e); // catch because we want to clean up
+    });
+  }).then(() => {
+    return fs.rmdir(process.cwd() + '/node_modules');
+  });
+});
