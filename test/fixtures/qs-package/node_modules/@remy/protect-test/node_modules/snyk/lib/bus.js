9365604237e73395e4ee0dbe676502f077dab325
Remy Sharp <remy@remysharp.com>
2016-07-28 16:12:06 +0100

    chore: reset and remove qs-package test

--- test/fixtures/qs-package/node_modules/@remy/protect-test/node_modules/snyk/lib/bus.js
+++
@@ -1,9 +0,0 @@
-var events = require('events');
-var util = require('util');
-
-function Bus() {
-  events.EventEmitter.call(this);
-}
-
-util.inherits(Bus, events.EventEmitter);
-module.exports = new Bus();
