6e8819e4e91d972ce352bcd1033d7f5f180a980d
Alon Niv <oakfang@gmail.com>
2016-08-22 10:17:58 +0300

    chore: add analytics event for patch missing

--- test/patch-ensure-patch.js
+++ test/patch-ensure-patch.js
@@ -2,10 +2,19 @@ var test = require('tap-only');
 var proxyquire = require('proxyquire');
 
 var hasPatch = true;
+var analyticsEvent;
 
 var ensurePatchExists = proxyquire('../lib/protect/ensure-patch', {
   hasbin: function (_, cb) {
     cb(hasPatch);
+  },
+  '../analytics': {
+    add: function (type, data) {
+      analyticsEvent = {
+        type: type,
+        data: data,
+      };
+    },
   },
 });
 
@@ -15,7 +24,12 @@ test('Ensure patch works when patch exists', t => {
 });
 
 test('Ensure patch throws when patch does not exist', t => {
-  t.plan(1);
+  t.plan(2);
   hasPatch = false;
-  ensurePatchExists().then(t.fail).catch(t.pass);
+  ensurePatchExists()
+    .then(t.fail)
+    .catch(() => {
+      t.is(analyticsEvent.type, 'patch-binary-missing');
+      t.is(analyticsEvent.data.message, 'GNU "patch" binary does not exist.');
+    });
 });
