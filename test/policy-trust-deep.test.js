82bc676147dd28ced48197aa96f0d4f8f17fbf3e
Aviad Reich <aviad@snyk.io>
2017-05-15 18:22:36 +0300

    fix: update snyk policy

--- test/policy-trust-deep.test.js
+++ test/policy-trust-deep.test.js
@@ -8,7 +8,7 @@ test('`snyk test` sees suggested ignore policies', function (t) {
     var vulns = res.message.toLowerCase();
     t.notEqual(vulns.indexOf('suggests ignoring this issue, with reason: test trust policies'), -1, 'found suggestion to ignore');
 
-    t.equal(count('vulnerability found', vulns), 8, 'all 8 vulns found');
+    t.equal(count('vulnerability found', vulns), 22, 'all 22 vulns found');
   });
 });
 
@@ -16,7 +16,7 @@ test('`snyk test` ignores when applying `--trust-policies`', function (t) {
   return cli.test(dir, { 'trust-policies': true }).catch(function (res) {
     var vulns = res.message.trim();
     // note: it's 2 vulns
-    t.equal(count('vulnerability found', vulns), 6, 'only 6 vulns left');
+    t.equal(count('vulnerability found', vulns), 20, 'only 20 vulns left');
   });
 });
 
