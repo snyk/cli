b1b31d15634c9c20ca4fd84aea2f66f256a2634f
Josh Emerson <josh.j.emerson@gmail.com>
2017-05-31 17:29:05 +0100

    feat: prevent wizard from running when test limits are reached
    The CLI command `snyk wizard` should not run when test limits
    are reached.

--- test/alerts.test.js
+++ test/alerts.test.js
@@ -6,35 +6,52 @@ var exampleAlert = function (id, type) {
   type = type || 'info';
   return {
     msg: 'Example alert ' + id,
+    name: 'example-' + id,
     type: type,
   };
 }
 
-test('register an alert', function (t) {
-  t.plan(1);
+test('no alerts', function (t) {
+  t.plan(2);
 
-  alerts.registerAlerts([ exampleAlert(1) ]);
-  t.equal(alerts.displayAlerts().trim(), exampleAlert(1).msg,
+  var alert = exampleAlert(1);
+  t.false(alerts.hasAlert(alert.name), 'alert is not found');
+  t.equal(alerts.displayAlerts(), '', 'alerts are not displayed');
+});
+
+test('register an alert', function (t) {
+  t.plan(2);
+
+  var alert = exampleAlert(1);
+  alerts.registerAlerts([ alert ]);
+  t.true(alerts.hasAlert(alert.name), 'alert is found');
+  t.equal(alerts.displayAlerts().trim(), alert.msg,
     'alert is displayed');
 });
 
 test('register the same alert multiple times', function (t) {
-  t.plan(1);
+  t.plan(2);
 
-  alerts.registerAlerts([ exampleAlert(1) ]);
-  alerts.registerAlerts([ exampleAlert(1) ]);
-  t.equal(alerts.displayAlerts().trim(), exampleAlert(1).msg,
+  var alert = exampleAlert(1);
+  alerts.registerAlerts([ alert ]);
+  alerts.registerAlerts([ alert ]);
+  t.true(alerts.hasAlert(alert.name), 'alert is found');
+  t.equal(alerts.displayAlerts().trim(), alert.msg,
     'alert is only displayed once');
 });
 
 test('register two different alerts', function (t) {
-  t.plan(2);
+  t.plan(4);
 
-  alerts.registerAlerts([ exampleAlert(1) ]);
-  alerts.registerAlerts([ exampleAlert(2) ]);
+  var alert1 = exampleAlert(1);
+  var alert2 = exampleAlert(2);
+  alerts.registerAlerts([ alert1 ]);
+  alerts.registerAlerts([ alert2 ]);
+  t.true(alerts.hasAlert(alert1.name), 'first alert is found');
+  t.true(alerts.hasAlert(alert2.name), 'second alert is found');
   const displayedAlerts = alerts.displayAlerts();
-  t.contains(displayedAlerts, exampleAlert(1).msg,
+  t.contains(displayedAlerts, alert1.msg,
     'first alert is displayed');
-  t.contains(displayedAlerts, exampleAlert(2).msg,
+  t.contains(displayedAlerts, alert2.msg,
     'second alert is displayed');
 });
