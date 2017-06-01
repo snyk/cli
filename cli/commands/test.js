18d56d5493f6241d27f1698e96496205bfd47f3c
Dror Ben-Gai <dror@snyk.io>
2017-06-01 19:40:55 +0300

    feat: test and monitor support for Gradle

--- cli/commands/test.js
+++ cli/commands/test.js
@@ -233,8 +233,7 @@ function test(path, options) {
         if (vuln.type === 'license') {
           // do not display fix (there isn't any), remove newline
           res = res.slice(0, -1);
-        } else if (packageManager !== 'maven' ||
-            packageManager !== 'rubygems') {
+        } else if (packageManager === 'npm') {
           res += chalk.magenta(
             'Fix: None available. Consider removing this dependency.');
         }
