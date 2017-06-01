adea054b9388f68750b81c122befd9bcbad666ff
Gareth Visagie <gareth@gjvis.com>
2017-06-01 17:25:00 +0100

    test: verify --show-vulnerable-paths=false

---
+++ test/vulnerable-path-output.js
@@ -0,0 +1,53 @@
+var _ = require('lodash');
+var sinon = require('sinon');
+var tap = require('tap');
+var test = tap.test;
+
+var cli = require('../cli/commands');
+var snyk = require('..');
+
+sinon.stub(snyk, 'test', function() {
+  return require('./fixtures/more-vuln-paths-than-vulns');
+});
+
+tap.tearDown(function() {
+  snyk.test.restore();
+});
+
+test('"snyk test --show-vulnerable-paths=false"', function(t) {
+  var options = { 'show-vulnerable-paths': 'false' };
+  return cli.test('more-vuln-paths-than-vulns', options)
+  .then(function(res) { t.fail('Should have found vulns!'); })
+  .catch(function(res) {
+    var vulnUrls = res.message
+      .match(/^- info: (.*)$/gm)
+      .map(function(result) {
+        return result.replace(/^- info:\s*/, '');
+      });
+    t.assert(
+      _(vulnUrls)
+        .countBy() // count the occurrances of each vulnUrl
+        .values()
+        .every(function(occurances) { return occurances === 1 }),
+      'displays each vuln only once');
+  });
+});
+
+test('"snyk test"', function(t) {
+  return cli.test('more-vuln-paths-than-vulns')
+  .then(function() { t.fail('Should have found vulns!'); })
+  .catch(function(res) {
+    var vulnUrls = res.message
+      .match(/^- info: (.*)$/gm)
+      .map(function(result) {
+        return result.replace(/^- info:\s*/, '');
+      });
+    t.assert(
+      _(vulnUrls)
+        .countBy() // count the occurrances of each vulnUrl
+        .values()
+        .some(function(occurances) { return occurances > 1 }),
+      'duplicates vuln data for each vulnerable-path');
+  });
+
+});
