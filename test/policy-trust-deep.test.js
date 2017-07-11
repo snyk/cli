5f811e1a55ffc59e2fcbe9e253c5adcc5b601b52
Anton Drukh <anton@snyk.io>
2017-07-11 13:53:34 +0300

    test: update vuln count

--- test/policy-trust-deep.test.js
+++ test/policy-trust-deep.test.js
@@ -8,7 +8,7 @@ test('`snyk test` sees suggested ignore policies', function (t) {
     var vulns = res.message.toLowerCase();
     t.notEqual(vulns.indexOf('suggests ignoring this issue, with reason: test trust policies'), -1, 'found suggestion to ignore');
 
-    t.equal(count('vulnerability found', vulns), 22, 'all 22 vulns found');
+    t.equal(count('vulnerability found', vulns), 25, 'all 25 vulns found');
   });
 });
 
@@ -16,7 +16,7 @@ test('`snyk test` ignores when applying `--trust-policies`', function (t) {
   return cli.test(dir, { 'trust-policies': true }).catch(function (res) {
     var vulns = res.message.trim();
     // note: it's 2 vulns
-    t.equal(count('vulnerability found', vulns), 20, 'only 20 vulns left');
+    t.equal(count('vulnerability found', vulns), 23, 'only 23 vulns left');
   });
 });
 
