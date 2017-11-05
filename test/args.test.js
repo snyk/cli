40b91ce1b3e658b0dc7b71d861ef331d753b9ea7
Levan Giguashvili <levan@snyk.io>
2017-11-05 19:06:42 +0200

    feat: support package-manager flag and add it to monitor

--- test/args.test.js
+++ test/args.test.js
@@ -8,8 +8,33 @@ test('test command line arguments', function(t) {
     'test',
     '--',
     '-Paxis',
-    '-Pjaxen'
+    '-Pjaxen',
   ];
   var result = args(cliArgs);
   t.deepEqual(result.options._doubleDashArgs, ['-Paxis', '-Pjaxen']);
+  t.end();
+});
+
+test('test command line test --package-manager', function(t) {
+  t.plan(1);
+  var cliArgs = [ '/Users/dror/.nvm/versions/node/v6.9.2/bin/node',
+    '/Users/dror/work/snyk/snyk-internal/cli',
+    'test',
+    '--package-manager=pip',
+  ];
+  var result = args(cliArgs);
+  t.equal(result.options.packageManager, 'pip');
+  t.end();
+});
+
+test('test command line monitor --package-manager', function(t) {
+  t.plan(1);
+  var cliArgs = [ '/Users/dror/.nvm/versions/node/v6.9.2/bin/node',
+    '/Users/dror/work/snyk/snyk-internal/cli',
+    'monitor',
+    '--package-manager=pip',
+  ];
+  var result = args(cliArgs);
+  t.equal(result.options.packageManager, 'pip');
+  t.end();
 });
