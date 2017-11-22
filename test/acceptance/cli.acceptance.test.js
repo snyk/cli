e43d91d5705725544ddc4ac0ce4dfa41ee092e97
eavichay <eavichay@gmail.com>
2017-11-22 14:07:13 +0200

    fix: nuget support for custom json manifest in 1.3.2

--- test/acceptance/cli.acceptance.test.js
+++ test/acceptance/cli.acceptance.test.js
@@ -254,6 +254,38 @@ function (t) {
   });
 });
 
+test('`test nuget-app --file=custom-manifest.json`', function (t) {
+  chdirWorkspaces();
+  var plugin = {
+    inspect: function () {
+      return Promise.resolve({package: {}});
+    },
+  };
+  sinon.spy(plugin, 'inspect');
+
+  sinon.stub(plugins, 'loadPlugin');
+  t.teardown(plugins.loadPlugin.restore);
+  plugins.loadPlugin
+  .withArgs('nuget')
+  .returns(plugin);
+
+  return cli.test('nuget-app', {
+    file: 'custom-manifest.json',
+    packageManager: 'nuget',
+  })
+  .then(function () {
+    var req = server.popRequest();
+    t.equal(req.method, 'POST', 'makes POST request');
+    t.match(req.url, '/vuln/nuget', 'posts to correct url');
+    t.same(plugin.inspect.getCall(0).args,
+      ['nuget-app', 'custom-manifest.json', {
+        args: null,
+        file: 'custom-manifest.json',
+        packageManager: 'nuget',
+      },], 'calls nuget plugin');
+  });
+});
+
 test('`test nuget-app --file=app.csproj`', function (t) {
   chdirWorkspaces();
   var plugin = {
