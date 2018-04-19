d846181d4cb27dc3f736f1957967a70892823a32
Josh Emerson <josh.j.emerson@gmail.com>
2018-04-19 15:23:50 +0100

    feat: support yarn for protect scripts
    The command for yarn is now added as `yarn run snyk-protect`.
    For npm the command remains `npm run snyk-protect`.

--- test/wizard-prepare.test.js
+++ test/wizard-prepare.test.js
@@ -3,7 +3,8 @@ var proxyquire = require('proxyquire');
 var Promise = require('es6-promise').Promise; // jshint ignore:line
 var sinon = require('sinon');
 var spy = sinon.spy();
-var fixture = require(__dirname + '/fixtures/protect-via-snyk/package.json');
+var _ = require('lodash');
+var fixture = require('./fixtures/protect-via-snyk/package.json');
 
 var snyk = require('../');
 
@@ -31,7 +32,8 @@ var wizard = proxyquire('../cli/commands/protect/wizard', {
   }
 });
 
-test('prepare is added and postinstall is removed', function (t) {
+test('npm - prepare is added and postinstall is removed', function (t) {
+  var expectedResults = _.cloneDeep(fixture);
   return wizard.processAnswers({
     // answers
     'misc-test-no-monitor': true,
@@ -43,9 +45,32 @@ test('prepare is added and postinstall is removed', function (t) {
     var pkg = JSON.parse(spy.args[0][0]);
     t.pass('package was valid JSON');
 
-    fixture.scripts.postinstall = 'true';
-    fixture.scripts.prepare = 'npm run snyk-protect';
+    expectedResults.scripts.postinstall = 'true';
+    expectedResults.scripts.prepare = 'npm run snyk-protect';
 
-    t.deepEqual(fixture, pkg, 'package is correct');
+    t.deepEqual(expectedResults, pkg, 'package is correct');
+  });
+});
+
+test('yarn - prepare is added and postinstall is removed', function (t) {
+  var expectedResults = _.cloneDeep(fixture);
+  spy.reset();
+  return wizard.processAnswers({
+    // answers
+    'misc-test-no-monitor': true,
+    'misc-add-protect': true,
+  }, {
+    save: () => Promise.resolve()
+  }, {
+    packageManager: 'yarn',
+  }).then(function () {
+    t.equal(spy.callCount, 1, 'write function was only called once');
+    var pkg = JSON.parse(spy.args[0][0]);
+    t.pass('package was valid JSON');
+
+    expectedResults.scripts.postinstall = 'true';
+    expectedResults.scripts.prepare = 'yarn run snyk-protect';
+
+    t.deepEqual(expectedResults, pkg, 'package is correct');
   });
 });
