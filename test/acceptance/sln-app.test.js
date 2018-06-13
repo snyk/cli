28a1dad2030d2bcbf9823eec1c2a2b8e96cfdee7
Anton Drukh <anton@snyk.io>
2018-06-13 20:20:58 +0300

    feat: robust `*.sln` file parsing for project paths
    We now parse more complex solution files, and extract ALL project paths from them (not just those pointing at `.csproj` files).
    Once extracted, each project folder is tested for the presence of a relevant manifest file. Only valid folders are then processed.

--- test/acceptance/sln-app.test.js
+++ test/acceptance/sln-app.test.js
@@ -1,5 +1,4 @@
 var test = require('tap-only');
-var sinon = require('sinon');
 var path = require('path');
 var sln = require('../../lib/sln');
 
@@ -9,8 +8,8 @@ test('parseFoldersFromSln when passed an existant filename', function (t) {
     "dotnet2_new_mvc_project/new_mvc_project.csproj",
     "WebApplication2/WebApplication2.csproj"
   ]);
-  var actual = JSON.stringify(sln.parseFoldersFromSln(slnFile));
-  t.equal(expected, actual, 'should parse & extract csproj folders');
+  var actual = JSON.stringify(sln.parsePathsFromSln(slnFile));
+  t.equal(actual, expected, 'should parse & extract csproj folders');
   t.end();
 });
 
@@ -18,7 +17,7 @@ test('parseFoldersFromSln when non existant filename', function (t) {
   var response;
   var slnFile = 'test/acceptance/workspaces/sln-example-app/mySolution1.sln'; 
   try {
-    response = sln.parseFoldersFromSln(slnFile);
+    response = sln.parsePathsFromSln(slnFile);
     t.fail('an exception should be thrown');
   }
   catch (e) {
@@ -33,6 +32,7 @@ test('sln.updateArgs for existing sln with regular paths', function (t) {
     file: 'test/acceptance/workspaces/sln-example-app/mySolution.sln', _: []}};
 
   sln.updateArgs(args);
+  t.notOk(args.options.file, '`file` option is removed');
   args.options._.pop();
   t.same(args.options._.map(function (r) { return path.basename(r); }),
     [ 'dotnet2_new_mvc_project', 'WebApplication2' ], 'args should be added');
@@ -44,17 +44,34 @@ test('sln.updateArgs for existing sln with relative paths', function (t) {
     file: 'test/acceptance/workspaces/slnSolution.sln', _: []}};
 
   sln.updateArgs(args);
+  t.notOk(args.options.file, '`file` option is removed');
   args.options._.pop();
   t.same(args.options._.map(function (r) { return path.basename(r); }),
     [  'nuget-app', 'nuget-app-2.1'], 'args should be added');
   t.end();
 });
 
+test('sln.updateArgs for sln with no relevant projects', function (t) {
+  var args = {options: {
+    file: 'test/acceptance/workspaces/emptySolution.sln', _: []}};
+
+  try {
+    sln.updateArgs(args);
+    t.fail('should have exploded');
+  } catch (e) {
+    t.equal(e.message, 'No relevant projects found in Solution',
+      'Error thrown on solution with no valid projects');
+  }
+  t.end();
+});
+
+
 test('sln.updateArgs for non-existing sln', function (t) {
   var args = {options: {file: 'non_existent', _: []}};
 
   try {
     sln.updateArgs(args);
+    t.fail('should have exploded');
   } catch (e) {
     t.equal(args.options.file, 'non_existent', 'file parameter should remain');
     t.equal(args.options._.length, 0, 'params not added');
