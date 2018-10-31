41720d978bcf9d4ca4d7640f1d8474884772a1a9
weyusi <weyusi@users.noreply.github.com>
2018-10-31 10:45:10 +1030

    test: ensure patch is gnu version

--- src/lib/protect/ensure-patch.js
+++ src/lib/protect/ensure-patch.js
@@ -3,27 +3,38 @@ module.exports = ensurePatchExists;
 var hasbin = require('hasbin');
 var analytics = require('../analytics');
 var chalk = require('chalk');
+var exec = require('child_process').exec;
+var err = new Error(
+  'Snyk couldn\'t patch the specified ' +
+  'vulnerabilities because GNU\'s patch is not ' +
+  'available. Please install \'patch\' ' +
+  'and try again. \n For more information ' +
+  'please see our support page: '
+  + chalk.underline(
+    'https://support.snyk.io' +
+    '/snyk-cli/snyk-protect-requires-the-patch-binary.'
+  ));
 
 function ensurePatchExists() {
   return new Promise(function (resolve, reject) {
     return hasbin('patch', function (exists) {
+      // Output error if patch binary is not installed
       if (!exists) {
-        var err =
-        new Error(
-          'Snyk couldn\'t patch the specified ' +
-          'vulnerabilities because GNU\'s patch is not ' +
-          'available. Please install \'patch\' ' +
-          'and try again. \n For more information ' +
-          'please see our support page: '
-          + chalk.underline(
-            'https://support.snyk.io' +
-            '/snyk-cli/snyk-protect-requires-the-patch-binary.'
-          ));
         analytics.add('patch-binary-missing', {
           message: err.message,
         });
         return reject(err);
       }
+      // Output error if patch binary is not GNU version
+      exec('patch --version', function (error, stdout) { // stderr is ignored
+        var out = stdout.trim();
+        if (out.indexOf('GNU') === -1) {
+          analytics.add('patch-binary-missing', {
+            message: err.message,
+          });
+          return reject(err);
+        }
+      });
       return resolve();
     });
   });
