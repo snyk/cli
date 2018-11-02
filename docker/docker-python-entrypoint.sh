0207e9eac9b591c49140dee1af1df8290ecfb983
Julien Duchesne <jduchesne@coveo.com>
2018-11-02 11:15:11 -0400

    Support Pipfile in python docker image Support multiple python versions

--- docker/docker-python-entrypoint.sh
+++ docker/docker-python-entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-virtualenv -p python snyk
+RUN_DIR=$(mktemp -d)
+
+PIPENV_FILE=$PROJECT_PATH/Pipfile
+REQUIREMENTS_FILE=$RUN_DIR/requirements.txt
+if [ -f $PIPENV_FILE ]; then
+    CURRENT_PATH=$(pwd)
+    cd $PROJECT_PATH
+    pipenv lock --requirements --keep-outdated > $RUN_DIR/requirements.txt
+    cd $CURRENT_PATH
+else
+    cp $PROJECT_PATH/requirements.txt $RUN_DIR/requirements.txt
+fi
+
+virtualenv -p python$PYTHON_VERSION snyk
 source snyk/bin/activate
-pip install -U -r $PROJECT_PATH/requirements.txt
+pip install -U -r $REQUIREMENTS_FILE
+
+PROJECT_PATH=$RUN_DIR
 bash docker-entrypoint.sh "$@"
