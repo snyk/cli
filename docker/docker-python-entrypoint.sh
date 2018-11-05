09ca5d8175930effd7b81c6b2486b8b7ad9a4776
Julien Duchesne <jduchesne@coveo.com>
2018-11-05 09:45:35 -0500

    Run in PROJECT_PATH so the snyk files are in a determinate location

--- docker/docker-python-entrypoint.sh
+++ docker/docker-python-entrypoint.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-RUN_DIR=$(mktemp -d)
-
 PIPENV_FILE=$PROJECT_PATH/Pipfile
-REQUIREMENTS_FILE=$RUN_DIR/requirements.txt
+REQUIREMENTS_FILE=$PROJECT_PATH/requirements.txt
 if [ -f $PIPENV_FILE ]; then
     CURRENT_PATH=$(pwd)
     cd $PROJECT_PATH
-    pipenv lock --requirements --keep-outdated > $RUN_DIR/requirements.txt
+    pipenv lock --requirements --keep-outdated > $REQUIREMENTS_FILE
     cd $CURRENT_PATH
-else
-    cp $PROJECT_PATH/requirements.txt $RUN_DIR/requirements.txt
 fi
 
 virtualenv -p python$PYTHON_VERSION snyk
 source snyk/bin/activate
 pip install -U -r $REQUIREMENTS_FILE
 
-PROJECT_PATH=$RUN_DIR
 bash docker-entrypoint.sh "$@"
+
+if [ -f $PIPENV_FILE ]; then
+    rm -f $REQUIREMENTS_FILE
+fi
