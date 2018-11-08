a4da0c580b4f179cde1e14c66b017af8be51683d
Michael Gokhman <michael.gokhman@yahoo.com>
2018-11-08 14:28:59 +0200

    chore: async/await in lib/snyk-test/run-test.js

--- src/lib/snyk-test/run-test.js
+++ src/lib/snyk-test/run-test.js
@@ -17,167 +17,153 @@ var snyk = require('../');
 var spinner = require('../spinner');
 var common = require('./common');
 
+async function runTest(packageManager, root, options) {
+  var policyLocations = [options['policy-path'] || root];
+  // TODO: why hasDevDependencies is always false?
+  var hasDevDependencies = false;
 
-function runTest(packageManager, root, options) {
-  return Promise.resolve().then(function () {
-    var policyLocations = [options['policy-path'] || root];
-    var hasDevDependencies = false;
-    var lbl = 'Querying vulnerabilities database...';
-    return assemblePayload(root, options, policyLocations)
-      .then(function (res) {
-        return spinner(lbl).then(function () {
-          return res;
-        });
-      })
-      .then(function (payload) {
-        var filesystemPolicy = payload.body && !!payload.body.policy;
-        return new Promise(function (resolve, reject) {
-          request(payload, function (error, res, body) {
-            if (error) {
-              return reject(error);
-            }
+  var spinnerLbl = 'Querying vulnerabilities database...';
 
-            if (res.statusCode !== 200) {
-              var err = new Error(body && body.error ?
-                body.error :
-                res.statusCode);
+  try {
+    var payload = await assemblePayload(root, options, policyLocations);
+    var filesystemPolicy = payload.body && !!payload.body.policy;
 
-              err.userMessage = body && body.userMessage;
-              // this is the case where a local module has been tested, but
-              // doesn't have any production deps, but we've noted that they
-              // have dep deps, so we'll error with a more useful message
-              if (res.statusCode === 404 && hasDevDependencies) {
-                err.code = 'NOT_FOUND_HAS_DEV_DEPS';
-              } else {
-                err.code = res.statusCode;
-              }
+    await spinner(spinnerLbl);
 
-              if (res.statusCode === 500) {
-                debug('Server error', body.stack);
-              }
+    var res = await sendRequest(payload, hasDevDependencies);
 
-              return reject(err);
-            }
+    analytics.add('vulns-pre-policy', res.vulnerabilities.length);
+    res.filesystemPolicy = filesystemPolicy;
+    if (!options['ignore-policy']) {
+      var policy = await snyk.policy.loadFromText(res.policy);
+      res = policy.filter(res, root);
+    }
+    analytics.add('vulns', res.vulnerabilities.length);
 
-            body.filesystemPolicy = filesystemPolicy;
+    // add the unique count of vulnerabilities found
+    res.uniqueCount = 0;
+    var seen = {};
+    res.uniqueCount = res.vulnerabilities.reduce(function (acc, curr) {
+      if (!seen[curr.id]) {
+        seen[curr.id] = true;
+        acc++;
+      }
+      return acc;
+    }, 0);
 
-            resolve(body);
-          });
-        });
-      }).then(function (res) {
-        analytics.add('vulns-pre-policy', res.vulnerabilities.length);
-        return Promise.resolve()
-          .then(function () {
-            if (options['ignore-policy']) {
-              return res;
-            }
+    return res;
+  } finally {
+    spinner.clear(spinnerLbl)();
+  }
+}
 
-            return snyk.policy.loadFromText(res.policy)
-              .then(function (policy) {
-                return policy.filter(res, root);
-              });
-          })
-          .then(function (res) {
-            analytics.add('vulns', res.vulnerabilities.length);
+function sendRequest(payload, hasDevDependencies) {
+  return new Promise(function (resolve, reject) {
+    request(payload, function (error, res, body) {
+      if (error) {
+        return reject(error);
+      }
 
-            // add the unique count of vulnerabilities found
-            res.uniqueCount = 0;
-            var seen = {};
-            res.uniqueCount = res.vulnerabilities.reduce(function (acc, curr) {
-              if (!seen[curr.id]) {
-                seen[curr.id] = true;
-                acc++;
-              }
-              return acc;
-            }, 0);
+      if (res.statusCode !== 200) {
+        var err = new Error(body && body.error ?
+          body.error :
+          res.statusCode);
 
-            return res;
-          });
-      })
-      // clear spinner in case of success or failure
-      .then(spinner.clear(lbl))
-      .catch(function (error) {
-        spinner.clear(lbl)();
-        throw error;
-      });
+        err.userMessage = body && body.userMessage;
+        // this is the case where a local module has been tested, but
+        // doesn't have any production deps, but we've noted that they
+        // have dep deps, so we'll error with a more useful message
+        if (res.statusCode === 404 && hasDevDependencies) {
+          err.code = 'NOT_FOUND_HAS_DEV_DEPS';
+        } else {
+          err.code = res.statusCode;
+        }
+
+        if (res.statusCode === 500) {
+          debug('Server error', body.stack);
+        }
+
+        return reject(err);
+      }
+
+      resolve(body);
+    });
   });
 }
 
 function assemblePayload(root, options, policyLocations) {
-  var local;
+  var isLocal;
   if (options.docker) {
-    local = true;
+    isLocal = true;
     policyLocations = policyLocations.filter(function (loc) {
       return loc !== root;
     });
   } else {
-    local = fs.existsSync(root);
+    isLocal = fs.existsSync(root);
   }
-  analytics.add('local', local);
-  return local ? assembleLocalPayload(root, options, policyLocations)
-    : assembleRemotePayload(root, options);
+  analytics.add('local', isLocal);
+  if (isLocal) {
+    return assembleLocalPayload(root, options, policyLocations);
+  }
+  return assembleRemotePayload(root, options);
 }
 
-function assembleLocalPayload(root, options, policyLocations) {
+async function assembleLocalPayload(root, options, policyLocations) {
   options.file = options.file || detect.detectPackageFile(root);
   var plugin = plugins.loadPlugin(options.packageManager, options);
   var moduleInfo = ModuleInfo(plugin, options.policy);
   var analysisType = options.docker ? 'docker' : options.packageManager;
-  var lbl = 'Analyzing ' + analysisType + ' dependencies for ' +
+  var spinnerLbl = 'Analyzing ' + analysisType + ' dependencies for ' +
     pathUtil.relative('.', pathUtil.join(root, options.file || ''));
-  return spinner(lbl)
-    .then(function () {
-      return moduleInfo.inspect(root, options.file, options);
-    })
-    // clear spinner in case of success or failure
-    .then(spinner.clear(lbl))
-    .catch(function (error) {
-      spinner.clear(lbl)();
-      throw error;
-    })
-    .then(function (info) {
-      var pkg = info.package;
-      if (_.get(info, 'plugin.packageManager')) {
-        options.packageManager = info.plugin.packageManager;
+
+  try {
+    await spinner(spinnerLbl);
+    var inspectRes = await moduleInfo.inspect(root, options.file, options);
+
+    var pkg = inspectRes.package;
+    if (_.get(inspectRes, 'plugin.packageManager')) {
+      options.packageManager = inspectRes.plugin.packageManager;
+    }
+    if (!_.get(pkg, 'docker.baseImage') && options['base-image']) {
+      pkg.docker = pkg.docker || {};
+      pkg.docker.baseImage = options['base-image'];
+    }
+    analytics.add('policies', policyLocations.length);
+    analytics.add('packageManager', options.packageManager);
+    analytics.add('packageName', pkg.name);
+    analytics.add('packageVersion', pkg.version);
+    analytics.add('package', pkg.name + '@' + pkg.version);
+    var payload = {
+      method: 'POST',
+      url: vulnUrl(options.packageManager),
+      json: true,
+      headers: {
+        'x-is-ci': isCI,
+        authorization: 'token ' + snyk.api,
+      },
+      body: pkg,
+    };
+    payload.qs = common.assembleQueryString(options);
+
+    if (policyLocations.length > 0) {
+      try {
+        var policy = await snyk.policy.load(policyLocations, options);
+        payload.body.policy = policy.toString();
+      } catch (err) {
+        // note: inline catch, to handle error from .load
+        //   if the .snyk file wasn't found, it is fine
+        if (err.code !== 'ENOENT') {
+          throw err;
+        }
       }
-      if (!_.get(pkg, 'docker.baseImage') && options['base-image']) {
-        pkg.docker = pkg.docker || {};
-        pkg.docker.baseImage = options['base-image'];
-      }
-      analytics.add('policies', policyLocations.length);
-      analytics.add('packageManager', options.packageManager);
-      analytics.add('packageName', pkg.name);
-      analytics.add('packageVersion', pkg.version);
-      analytics.add('package', pkg.name + '@' + pkg.version);
-      var payload = {
-        method: 'POST',
-        url: vulnUrl(options.packageManager),
-        json: true,
-        headers: {
-          'x-is-ci': isCI,
-          authorization: 'token ' + snyk.api,
-        },
-        body: pkg,
-      };
-      payload.qs = common.assembleQueryString(options);
-      if (policyLocations.length === 0) {
-        return payload;
-      }
-      return snyk.policy.load(policyLocations, options)
-        .then(function (policy) {
-          payload.body.policy = policy.toString();
-          return payload;
-        }, function (error) { // note: inline catch, to handle error from .load
-          // the .snyk file wasn't found, which is fine, so we'll return the payload
-          if (error.code === 'ENOENT') {
-            return payload;
-          }
-          throw error;
-        });
-    });
+    }
+    return payload;
+  } finally {
+    spinner.clear(spinnerLbl)();
+  }
 }
 
-function assembleRemotePayload(root, options) {
+async function assembleRemotePayload(root, options) {
   var pkg = moduleToObject(root);
   var encodedName = encodeURIComponent(pkg.name + '@' + pkg.version);
   debug('testing remote: %s', pkg.name + '@' + pkg.version);
@@ -195,7 +181,7 @@ function assembleRemotePayload(root, options) {
     },
   };
   payload.qs = common.assembleQueryString(options);
-  return Promise.resolve(payload);
+  return payload;
 }
 
 function vulnUrl(packageManager) {
