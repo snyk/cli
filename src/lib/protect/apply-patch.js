32a1e139202df62cc3e6c01aa48257ef0fff855d
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-01-18 23:47:59 +0900

    refactor: try jsdiff

--- src/lib/protect/apply-patch.js
+++ src/lib/protect/apply-patch.js
@@ -1,6 +1,7 @@
 module.exports = applyPatch;
 
 var debug = require('debug')('snyk');
+var diff = require('diff');
 var exec = require('child_process').exec;
 var path = require('path');
 var fs = require('fs');
@@ -10,10 +11,6 @@ function applyPatch(patch, vuln, live) {
   var cwd = vuln.source;
 
   return new Promise(function (resolve, reject) {
-
-    var cmd = 'patch -p1 --backup --verbose < ' + patch;
-    var test = ' --dry-run';
-
     if (!cwd) {
       cwd = process.cwd();
     }
@@ -29,48 +26,40 @@ function applyPatch(patch, vuln, live) {
       debug('Failed loading package.json of package about to be patched', err);
     }
 
-    // default is 200K, we increase it as we've seen
-    //  "stdout maxBuffer exceeded" erros in some user debug logs
-    var STDOUT_BUFFER_SIZE = 5 * 1024 * 1024;
-
-    // do a dry run first, otherwise the patch can "succeed" (exit 0) if it
-    // only manages to patch *some* of the chunks (and leave the file partly
-    // patched).
-    exec(cmd + test, {
-      cwd: cwd,
-      env: process.env,
-      maxBuffer: STDOUT_BUFFER_SIZE,
-    }, function (error, stdout) { // stderr is ignored
-      var out = stdout.trim();
-      if (error || out.indexOf('FAILED') !== -1) {
-        debug('patch command failed', relative, error, out);
-        return patchError(error, out, relative, vuln).catch(reject);
-      }
-      if (!live) {
-        return resolve();
-      }
-
-      // if it was okay, and it wasn't a dry-run, then let's do it for real
-      exec(cmd, {
-        cwd: cwd,
-        env: process.env,
-        maxBuffer: STDOUT_BUFFER_SIZE,
-      }, function (error, stdout) {
-        var out = stdout.trim();
-        if (error || out.indexOf('FAILED') !== -1) {
+    diff.applyPatches(patch, {
+      loadFile: function (index, callback) {
+        try {
+          var content = fs.readFileSync(path.resolve(relative, index.oldFileName), 'utf8');
+          callback(null, content);
+        } catch (err) {
+          callback(err);
+        }
+      },
+      patched: function (index, content, callback) {
+        if (live) {
+          try {
+            fs.writeFileSync(index.newFileName, content);
+            callback();
+          } catch (err) {
+            callback(err);
+          }
+        }
+      },
+      complete: function (error) {
+        if (error) {
           debug('patch command failed', relative, error, out);
-          return patchError(error, out, relative, vuln).catch(reject);
+          return patchError(error, relative, vuln).catch(reject);
         }
 
-        debug('patch succeed', out);
+        debug('patch succeed');
 
         resolve();
-      });
+      }
     });
   });
 }
 
-function patchError(error, stdout, dir, vuln) {
+function patchError(error, dir, vuln) {
   if (error && error.code === 'ENOENT') {
     error.message = 'Failed to patch: the target could not be found.';
     return Promise.reject(error);
@@ -79,13 +68,11 @@ function patchError(error, stdout, dir, vuln) {
   return new Promise(function (resolve, reject) {
     var id = vuln.id;
 
-    // sneaky trick to do two sys calls in one.
-    exec('npm -v && patch -v', {
+    exec('npm -v', {
       env: process.env,
     }, function (patchVError, versions) { // stderr is ignored
       var parts = versions.split('\n');
       var npmVersion = parts.shift();
-      var patchVersion = parts.shift();
 
       // post the raw error to help diagnose
       errorAnalytics({
@@ -97,8 +84,6 @@ function patchError(error, stdout, dir, vuln) {
           packageVersion: vuln.version,
           package: vuln.name + '@' + vuln.version,
           error: error,
-          stdout: stdout,
-          'patch-version': patchVersion,
           'npm-version': npmVersion,
         },
       });
