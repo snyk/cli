058de94faf91dd50f58590e9dd86b5756cab4bbf
Laura Woo <laura@snyk.io>
2019-10-08 15:57:16 +0100

    feat: add pipfile support to docker image

--- docker/docker-python-entrypoint.sh
+++ docker/docker-python-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+if [ -f "${PROJECT_PATH}/requirements.txt" ]; then
+    virtualenv -p python snyk
+    source snyk/bin/activate
+    pip install -U -r "${PROJECT_PATH}/requirements.txt"
+elif [ -f "${PROJECT_PATH}/Pipfile" ]; then
+    cd "${PROJECT_PATH}"
+    pipenv install Pipfile
+fi
 
-virtualenv -p python snyk
-source snyk/bin/activate
-pip install -U -r "${PROJECT_PATH}/requirements.txt"
+cd /$HOME/
 bash docker-entrypoint.sh "$@"
