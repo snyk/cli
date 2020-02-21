752a0321b9d678984d6729cc5dd7b10d3853d4ee
Daniel Kontorovskyi <dan.kontorovskyy@gmail.com>
2020-02-21 16:37:46 +0000

    fix: on merge PR use correct commit SHA

--- scripts/check_circleci_status.sh
+++ scripts/check_circleci_status.sh
@@ -4,6 +4,10 @@ workflow_name="test_and_release"
 declare -i max_retry=120
 declare -i i=0
 STATUS_ARR=("")
+COMMIT_TO_RUN=$([[ -z ${TRAVIS_PULL_REQUEST_SHA} ]] && echo ${TRAVIS_COMMIT} || echo ${TRAVIS_PULL_REQUEST_SHA})
+
+echo 'TRAVIS_COMMIT='${TRAVIS_COMMIT}
+echo 'TRAVIS_PULL_REQUEST_SHA='${TRAVIS_PULL_REQUEST_SHA}
 
 while [[ "${STATUS_ARR[0]}" != "completed" && "${i}" -lt "${max_retry}" ]]
 do
@@ -11,10 +15,10 @@ do
     curl_result=$(curl -s \
         -H 'Accept: application/vnd.github.antiope-preview+json' \
         -H 'Authorization: token '${GH_TOKEN} \
-        'https://api.github.com/repos/'${TRAVIS_REPO_SLUG}'/commits/'${TRAVIS_PULL_REQUEST_SHA}'/check-runs') # query github for CircleCI job status
+        'https://api.github.com/repos/'${TRAVIS_REPO_SLUG}'/commits/'${COMMIT_TO_RUN}'/check-runs') # query github for CircleCI job status
     result=$(echo ${curl_result} | jq -r '.check_runs[] | select(.name == "'${workflow_name}'") | "\(.status) \(.conclusion)"') # extract status and conclusion
     IFS=' ' read -ra STATUS_ARR <<< "$result"
-    echo $(date '+%F %T') 'Not completed yet. Finished '${i}' attempt out of '${max_retry}'. Current status: '${result}
+    echo $(date '+%F %T') 'Not completed for '${COMMIT_TO_RUN}'. Finished '${i}' attempt out of '${max_retry}'. Current status: '${result}
 
     sleep 15
 done
