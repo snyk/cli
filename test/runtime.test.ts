f6779fae942cc2b9d2642fe4ef9d03f1157e5771
Liliana Kastilio <lili@snyk.io>
2020-03-20 13:25:23 +0000

    feat: use 8 as min supported runtime message

--- test/runtime.test.ts
+++ test/runtime.test.ts
@@ -1,16 +1,16 @@
 import { test } from 'tap';
 import * as runtime from '../src/cli/runtime';
 
-test('nodejs runtime versions support ', (t) => {
+test('nodejs runtime versions support ', async (t) => {
   t.ok(
     runtime.isSupported(process.versions.node),
     'Current runtime is supported',
   );
-  t.ok(runtime.isSupported('6.16.0'), '6.16.0 is supported');
+  t.notOk(runtime.isSupported('6.16.0'), '6.16.0 is not supported');
+  t.ok(runtime.isSupported('8.0.0'), '8.0.0 is supported');
   t.ok(runtime.isSupported('11.0.0-pre'), 'pre-release is supported');
   t.notOk(runtime.isSupported('0.10.48'), '0.10 is not supported');
   t.notOk(runtime.isSupported('0.12.18'), '0.12 is not supported');
   t.notOk(runtime.isSupported('4.0.0'), '4.0.0 is not supported');
   t.notOk(runtime.isSupported('6.4.0'), '6.4.0 is not supported');
-  t.end();
 });
