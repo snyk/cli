640679fade3a994b9eeee7e18141eb31b7521af2
Dar Malovani <dar@snyk.io>
2020-07-23 16:37:08 +0300

    chore: refactor `abridgeErrorMessage`

--- src/lib/error-format.ts
+++ src/lib/error-format.ts
@@ -1,12 +1,13 @@
 export function abridgeErrorMessage(
   msg: string,
   maxLen: number,
-  ellipsis?: string,
+  ellipsis = ' ... ',
 ): string {
   if (msg.length <= maxLen) {
     return msg;
   }
-  const e = ellipsis || ' ... ';
-  const toKeep = (maxLen - e.length) / 2;
-  return msg.slice(0, toKeep) + e + msg.slice(msg.length - toKeep, msg.length);
+  const toKeep = Math.floor((maxLen - ellipsis.length) / 2);
+  return (
+    msg.slice(0, toKeep) + ellipsis + msg.slice(msg.length - toKeep, msg.length)
+  );
 }
