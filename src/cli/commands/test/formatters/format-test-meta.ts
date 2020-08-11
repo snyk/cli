Kingso28/snyk

feat:improve iac test json output

src/cli/commands/test/formatters/format-test-meta.ts
@@ -2,11 +2,11 @@ import chalk from 'chalk';
 import { rightPadWithSpaces } from '../../../../lib/right-pad';
 import { TestOptions, Options } from '../../../../lib/types';
 import { TestResult } from '../../../../lib/snyk-test/legacy';
-import { IacTestResult } from '../../../../lib/snyk-test/iac-test-result';
+import { IacTest } from '../../../../lib/snyk-test/iac-test-result';
 import { capitalizePackageManager } from '../iac-output';
 
 export function formatTestMeta(
-  res: TestResult | IacTestResult,
+  res: TestResult | IacTest,
   options: Options & TestOptions,
 ): string {
   const padToLength = 19; // chars to align

src/lib/snyk-test/run-iac-test.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as pathUtil from 'path';
 import { TestResult } from './legacy';
-import { IacTestResult } from './iac-test-result';
+import { IacTest } from './iac-test-result';
 import * as snyk from '..';
 import { isCI } from '../is-ci';
 import * as common from './common';
@@ -14,7 +14,7 @@ import { SEVERITY } from './legacy';
 import * as pathLib from 'path';
 
 export async function parseIacTestResult(
-  res: IacTestResult,
+  res: IacTest,
   targetFile: string | undefined,
   projectName: any,
   severityThreshold?: SEVERITY,

src/lib/snyk-test/run-test.ts
@@ -16,7 +16,7 @@ import {
   convertTestDepGraphResultToLegacy,
   LegacyVulnApiResult,
 } from './legacy';
-import { IacTestResult } from './iac-test-result';
+import { IacTest } from './iac-test-result';
 import {
   AuthFailedError,
   InternalServerError,
@@ -79,7 +79,7 @@ async function sendAndParseResults(
     if (options.iac) {
       const iacScan: IacScan = payload.body as IacScan;
       analytics.add('iac type', !!iacScan.type);
-      const res = (await sendTestPayload(payload)) as IacTestResult;
+      const res = (await sendTestPayload(payload)) as IacTest;
 
       const projectName =
         iacScan.projectNameOverride || iacScan.originalProjectName;
@@ -267,7 +267,7 @@ async function parseRes(
 
 function sendTestPayload(
   payload: Payload,
-): Promise<LegacyVulnApiResult | TestDepGraphResponse | IacTestResult> {
+): Promise<LegacyVulnApiResult | TestDepGraphResponse | IacTest> {
   const filesystemPolicy = payload.body && !!payload.body.policy;
   return new Promise((resolve, reject) => {
     request(payload, (error, res, body) => {

snyk/cli

src/cli/commands/test/formatters/format-test-meta.ts
@@ -2,11 +2,11 @@ import chalk from 'chalk';
 import { rightPadWithSpaces } from '../../../../lib/right-pad';
 import { TestOptions, Options } from '../../../../lib/types';
 import { TestResult } from '../../../../lib/snyk-test/legacy';
-import { IacTestResult } from '../../../../lib/snyk-test/iac-test-result';
+import { IacTestResponse } from '../../../../lib/snyk-test/iac-test-result';
 import { capitalizePackageManager } from '../iac-output';
 
 export function formatTestMeta(
-  res: TestResult | IacTestResult,
+  res: TestResult | IacTestResponse,
   options: Options & TestOptions,
 ): string {
   const padToLength = 19; // chars to align

src/lib/snyk-test/run-iac-test.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as pathUtil from 'path';
 import { TestResult } from './legacy';
-import { IacTestResult } from './iac-test-result';
+import { IacTestResponse } from './iac-test-result';
 import * as snyk from '..';
 import { isCI } from '../is-ci';
 import * as common from './common';
@@ -14,7 +14,7 @@ import { SEVERITY } from './legacy';
 import * as pathLib from 'path';
 
 export async function parseIacTestResult(
-  res: IacTestResult,
+  res: IacTestResponse,
   targetFile: string | undefined,
   projectName: any,
   severityThreshold?: SEVERITY,

src/lib/snyk-test/run-test.ts
@@ -16,7 +16,7 @@ import {
   convertTestDepGraphResultToLegacy,
   LegacyVulnApiResult,
 } from './legacy';
-import { IacTestResult } from './iac-test-result';
+import { IacTestResponse } from './iac-test-result';
 import {
   AuthFailedError,
   InternalServerError,
@@ -79,7 +79,7 @@ async function sendAndParseResults(
     if (options.iac) {
       const iacScan: IacScan = payload.body as IacScan;
       analytics.add('iac type', !!iacScan.type);
-      const res = (await sendTestPayload(payload)) as IacTestResult;
+      const res = (await sendTestPayload(payload)) as IacTestResponse;
 
       const projectName =
         iacScan.projectNameOverride || iacScan.originalProjectName;
@@ -267,7 +267,7 @@ async function parseRes(
 
 function sendTestPayload(
   payload: Payload,
-): Promise<LegacyVulnApiResult | TestDepGraphResponse | IacTestResult> {
+): Promise<LegacyVulnApiResult | TestDepGraphResponse | IacTestResponse> {
   const filesystemPolicy = payload.body && !!payload.body.policy;
   return new Promise((resolve, reject) => {
     request(payload, (error, res, body) => {