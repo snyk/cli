c470cec2caef7722061f45960dd6760e526f7c9c
Jakub Mikulas <jakub@snyk.io>
2020-09-01 23:48:28 +0200

    chore: add smoke test for monitor command

---
+++ test/smoke/spec/snyk_monitor_spec.sh
@@ -0,0 +1,47 @@
+#shellcheck shell=sh
+
+Describe "Snyk monitor command"
+  Before snyk_login
+  After snyk_logout
+
+  Describe "monitor npm project"
+    run_monitor_in_subfolder() {
+      cd ../fixtures/basic-npm || return
+      snyk monitor
+    }
+
+    It "finds vulns in a project in the same folder"
+      When run run_monitor_in_subfolder
+      The status should be success
+      The output should include "Explore this snapshot at https://app.snyk.io/org/"
+      The output should include "Notifications about newly disclosed issues related to these dependencies will be emailed to you."
+      The stderr should equal ""
+    End
+
+    It "finds vulns in a project when pointing to a folder"
+      When run snyk monitor ../fixtures/basic-npm
+      The status should be success
+      The output should include "Explore this snapshot at https://app.snyk.io/org/"
+      The output should include "Notifications about newly disclosed issues related to these dependencies will be emailed to you."
+      The stderr should equal ""
+    End
+
+    It "finds vulns in a project when pointing to a file"
+      When run snyk monitor --file=../fixtures/basic-npm/package.json
+      The status should be success
+      The output should include "Explore this snapshot at https://app.snyk.io/org/"
+      The output should include "Notifications about newly disclosed issues related to these dependencies will be emailed to you."
+      The stderr should equal ""
+    End
+  End
+
+  Describe "monitor npm project with JSON output"
+    It "outputs a valid JSON with vulns"
+      When run snyk monitor ../fixtures/basic-npm --json
+      The status should be success # issues found
+      The output should include '"ok": true,'
+      The stderr should equal ""
+      The result of function check_valid_json should be success
+    End
+  End
+End
