b6e602ba7df24c5f834f97db089dba756d5fd111
ghe <lili@snyk.io>
2020-09-03 11:19:47 +0100

    test: refactor & test exit codes

--- test/smoke/spec/snyk_test_spec.sh
+++ test/smoke/spec/snyk_test_spec.sh
@@ -10,9 +10,28 @@ Describe "Snyk test command"
       snyk test
     }
 
+    run_test_in_empty_subfolder() {
+      cd ../fixtures/empty || return
+      snyk test
+    }
+
+    It "throws error when file does not exist"
+      When run snyk test --file=non-existent/package.json
+      The status should equal 2
+      The output should include "Could not find the specified file"
+      The stderr should equal ""
+    End
+
+    It "throws error when no suppored manifests detected"
+      When run run_test_in_empty_subfolder
+      The status should equal 3
+      The output should include "Could not detect supported target files in"
+      The stderr should equal ""
+    End
+
     It "finds vulns in a project in the same folder"
       When run run_test_in_subfolder
-      The status should be failure # issues found
+      The status should equal 1
       The output should include "https://snyk.io/vuln/npm:minimatch:20160620"
       The stderr should equal ""
     End
