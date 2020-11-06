7012caa8c27c2ba9d12da0a1967a243e37c83f67
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2020-11-06 13:32:06 -0500

    fix: add json module with jsonStringifyLargeObject

---
+++ src/lib/json.ts
@@ -0,0 +1,20 @@
+const debug = require('debug')('snyk-json');
+
+/**
+ * Attempt to json-stringify an object which is potentially very large and might exceed the string limit.
+ * If it does exceed the string limit, try again without pretty-print to hopefully come out below the string limit.
+ * @param obj the object from which you want to get a JSON string
+ */
+export function jsonStringifyLargeObject(obj: any): string {
+  let res = '';
+  try {
+    // first try pretty-print
+    res = JSON.stringify(obj, null, 2);
+    return res;
+  } catch (err) {
+    // if that doesn't work, try non-pretty print
+    debug('JSON.stringify failed - trying again without pretty print', err);
+    res = JSON.stringify(obj);
+    return res;
+  }
+}
