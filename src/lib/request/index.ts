c25ab73a5c2fad9bc5a2c926bcc0010060663e68
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2020-12-13 09:25:15 -0500

    fix: add type annotation to request module

--- src/lib/request/index.ts
+++ src/lib/request/index.ts
@@ -1,12 +1,13 @@
 import request = require('./request');
 import alerts = require('../alerts');
 import { MetricsCollector } from '../metrics';
+import * as needle from 'needle';
 
 // A hybrid async function: both returns a promise and takes a callback
 export = async (
   payload: any,
   callback?: (err: Error | null, res?, body?) => void,
-) => {
+): Promise<void | { res: needle.NeedleResponse; body: any }> => {
   const totalNetworkTimeTimer = MetricsCollector.NETWORK_TIME.createInstance();
   totalNetworkTimeTimer.start();
   try {
