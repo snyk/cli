c9b214ec7daa7079e80b006a57bc107f97a8feb1
Jakub Mikulas <jakub@snyk.io>
2021-01-12 08:55:29 +0100

    chore(smoke): handle uppercase header in release downloader

--- test/smoke/install-snyk-binary-win.sh
+++ test/smoke/install-snyk-binary-win.sh
@@ -1,6 +1,6 @@
 echo "install-snyk-binary-win.sh"
 
-export latest_version=$(curl -Is "https://github.com/snyk/snyk/releases/latest" | grep location | sed s#.*tag/##g | tr -d "\r")
+export latest_version=$(curl -Is "https://github.com/snyk/snyk/releases/latest" | grep -i location | sed s#.*tag/##g | tr -d "\r")
 echo "latest_version: ${latest_version}"
 snyk_cli_dl="https://github.com/snyk/snyk/releases/download/${latest_version}/snyk-win.exe"
 echo "snyk_cli_dl: ${snyk_cli_dl}"
