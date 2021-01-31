99221970ed8bfcd6a2a60766a1a5efaf8f94d375
Avishag Israeli <avishag.israeli@snyk.io>
2021-01-31 11:53:40 +0200

    test: cat snyk_latest API call to debug docker bundle install
    test: add authorization to curl command
    
    to prevent API rate limit being exceeded
    
    test: add authorization to GH API calls
    
    test: don’t use authorization for windows binary
    
    test: add authorization to shell files
    
    test: add env variable at alpine test job

--- test/smoke/install-snyk-binary-win.sh
+++ test/smoke/install-snyk-binary-win.sh
@@ -1,5 +1,5 @@
 echo "install-snyk-binary-win.sh"
-snyk_cli_dl=$(curl https://api.github.com/repos/snyk/snyk/releases/latest | jq --raw-output '(.assets[])? | select(.name == "snyk-win.exe") | .browser_download_url')
+snyk_cli_dl=$(curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/snyk/snyk/releases/latest | jq --raw-output '(.assets[])? | select(.name == "snyk-win.exe") | .browser_download_url')
 echo "snyk_cli_dl: ${snyk_cli_dl}"
 curl -Lo ./snyk-cli.exe $snyk_cli_dl
 ./snyk-cli.exe --version
