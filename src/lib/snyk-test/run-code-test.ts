5020a9d9d91d06b67aca88ec95b81022b2d9412e
kotli <artur@snyk.io>
2021-02-16 14:15:26 +0200

    fix: add security rule filtering on code scaning
    * we want to filter our sarif response by security only

--- src/lib/snyk-test/run-code-test.ts
+++ src/lib/snyk-test/run-code-test.ts
@@ -1,5 +1,5 @@
 import { AnalysisSeverity, analyzeFolders } from '@snyk/code-client';
-import { Log } from 'sarif';
+import { Log, ReportingDescriptor, Result } from 'sarif';
 import { SEVERITY } from './legacy';
 import { api } from '../api-token';
 import * as config from '../config';
@@ -14,7 +14,8 @@ export async function getCodeAnalysisAndParseResults(
   await spinner.clear<void>(spinnerLbl)();
   await spinner(spinnerLbl);
 
-  return await getCodeAnalysis(root, options);
+  const codeAnalysis = await getCodeAnalysis(root, options);
+  return parseSecurityResults(codeAnalysis);
 }
 
 async function getCodeAnalysis(root: string, options: Options): Promise<Log> {
@@ -53,4 +54,58 @@ function severityToAnalysisSeverity(severity: SEVERITY): AnalysisSeverity {
     high: 3,
   };
   return severityLevel[severity];
+}
+
+function parseSecurityResults(codeAnalysis: Log): Log {
+  let securityRules;
+
+  const rules = codeAnalysis.runs[0].tool.driver.rules;
+  const results = codeAnalysis.runs[0].results;
+
+  if (rules) {
+    securityRules = getSecurityRulesMap(rules);
+    codeAnalysis.runs[0].tool.driver.rules = Object.values(securityRules);
+  }
+  if (results && securityRules) {
+    codeAnalysis.runs[0].results = getSecurityResults(
+      results,
+      Object.keys(securityRules),
+    );
+  }
+
+  return codeAnalysis;
+}
+
+function getSecurityRulesMap(
+  rules: ReportingDescriptor[],
+): { [ruleId: string]: ReportingDescriptor[] } {
+  const securityRules = rules.reduce((acc, rule) => {
+    const { id: ruleId, properties } = rule;
+    const isSecurityRule = properties?.tags?.some(
+      (tag) => tag.toLowerCase() === 'security',
+    );
+    if (isSecurityRule) {
+      acc[ruleId] = rule;
+    }
+    return acc;
+  }, {});
+
+  return securityRules;
+}
+
+function getSecurityResults(
+  results: Result[],
+  securityRules: string[],
+): Result[] {
+  const securityResults = results.reduce((acc: Result[], result: Result) => {
+    const isSecurityResult = securityRules.some(
+      (securityRule) => securityRule === result?.ruleId,
+    );
+    if (isSecurityResult) {
+      acc.push(result);
+    }
+    return acc;
+  }, []);
+
+  return securityResults;
 }
