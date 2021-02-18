903629897d3b7cdadc53b354cee16dd98f74c6e4
Ron Tal <ron.tal@snyk.io>
2021-02-18 16:56:19 +0200

    refactor: improve iac test error messages & remove irrelevant tests

--- test/smoke/spec/iac/snyk_test_directory_spec.sh
+++ test/smoke/spec/iac/snyk_test_directory_spec.sh
@@ -25,17 +25,11 @@ Describe "Snyk iac test command"
 
       The output should include "Tested sg_open_ssh.tf for known issues, found"
 
-      # Invalid File testing - Go Template
-      The output should include "Testing sg_open_ssh_invalid_go_templates.tf..."
-      The output should include "Illegal Terraform target file sg_open_ssh_invalid_go_templates.tf "
-      The output should include "Validation Error Reason: Go Template placeholders found in Terraform file."
-      The output should include "Please see our documentation for supported target files (currently we support Kubernetes files only): https://support.snyk.io/hc/en-us/articles/360006368877-Scan-and-fix-security-issues-in-your-Kubernetes-configuration-files and make sure you are in the right directory."
-
       # Invalid File testing - HCL2 Format
       The output should include "Testing sg_open_ssh_invalid_hcl2.tf..."
       The output should include "Illegal Terraform target file sg_open_ssh_invalid_hcl2.tf"
       The output should include "Validation Error Reason: Invalid HCL2 Format."
-      The output should include "Please see our documentation for supported target files (currently we support Kubernetes files only): https://support.snyk.io/hc/en-us/articles/360006368877-Scan-and-fix-security-issues-in-your-Kubernetes-configuration-files and make sure you are in the right directory."
+      The output should include "Please see our documentation for supported target files: https://support.snyk.io/hc/en-us/articles/360013723877-Test-your-Terraform-files-with-our-CLI-tool and make sure you are in the right directory."
     End
 
     It "filters out issues when using severity threshold"
@@ -55,17 +49,11 @@ Describe "Snyk iac test command"
 
       The output should include "Tested sg_open_ssh.tf for known issues, found"
 
-      # Invalid File testing - Go Template
-      The output should include "Testing sg_open_ssh_invalid_go_templates.tf..."
-      The output should include "Illegal Terraform target file sg_open_ssh_invalid_go_templates.tf "
-      The output should include "Validation Error Reason: Go Template placeholders found in Terraform file."
-      The output should include "Please see our documentation for supported target files (currently we support Kubernetes files only): https://support.snyk.io/hc/en-us/articles/360006368877-Scan-and-fix-security-issues-in-your-Kubernetes-configuration-files and make sure you are in the right directory."
-
       # Invalid File testing - HCL2 Format
       The output should include "Testing sg_open_ssh_invalid_hcl2.tf..."
       The output should include "Illegal Terraform target file sg_open_ssh_invalid_hcl2.tf"
       The output should include "Validation Error Reason: Invalid HCL2 Format."
-      The output should include "Please see our documentation for supported target files (currently we support Kubernetes files only): https://support.snyk.io/hc/en-us/articles/360006368877-Scan-and-fix-security-issues-in-your-Kubernetes-configuration-files and make sure you are in the right directory."
+      The output should include "Please see our documentation for supported target files: https://support.snyk.io/hc/en-us/articles/360013723877-Test-your-Terraform-files-with-our-CLI-tool and make sure you are in the right directory."
     End
 
     It "outputs the expected text when running with --sarif flag"
