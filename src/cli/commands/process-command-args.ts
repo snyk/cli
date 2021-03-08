7acd2aa53acad64d97843bb70a8c3181ea794844
ghe <lili@snyk.io>
2021-03-08 20:07:32 +0000

    feat: use processCommandArgs in monitor too

--- src/cli/commands/process-command-args.ts
+++ src/cli/commands/process-command-args.ts
@@ -8,10 +8,12 @@ export function processCommandArgs<CommandOptions>(
   if (typeof args[args.length - 1] === 'object') {
     options = (args.pop() as any) as Options & CommandOptions;
   }
+  args = args.filter(Boolean);
 
   // populate with default path (cwd) if no path given
   if (args.length === 0) {
     args.unshift(process.cwd());
   }
+
   return { options, paths: args };
 }
