442e37f22fd9a477e0f7066cefff2ca367145e0d
ghe <lili@snyk.io>
2021-03-11 18:03:51 +0000

    feat: delete vuln paths option once transformed
    Add tests for the setDefaultTestOptions()

--- src/cli/commands/test/set-default-test-options.ts
+++ src/cli/commands/test/set-default-test-options.ts
@@ -1,11 +1,12 @@
 import * as config from '../../../lib/config';
 import { Options, ShowVulnPaths, TestOptions } from '../../../lib/types';
 
-export function setDefaultTestOptions(
-  options: Options & TestOptions,
-): Options & TestOptions {
-  const svpSupplied = (options['show-vulnerable-paths'] || '').toLowerCase();
+export function setDefaultTestOptions(options: Options): Options & TestOptions {
+  const svpSupplied = (options['show-vulnerable-paths'] || '')
+    .toString()
+    .toLowerCase();
 
+  delete options['show-vulnerable-paths'];
   return {
     ...options,
     // org fallback to config unless specified
---
+++ test/set-default-test-options.spec.ts
@@ -0,0 +1,99 @@
+import { setDefaultTestOptions } from '../src/cli/commands/test/set-default-test-options';
+
+describe('setDefaultTestOptions', () => {
+  it('defaults to show-vulnerable-paths:some & org from config when no options passed in', () => {
+    const updated = setDefaultTestOptions({ path: '/' });
+    expect(updated).toEqual({
+      org: undefined,
+      path: '/',
+      showVulnPaths: 'some',
+    });
+  });
+
+  it('with show-vulnerable-paths set to `false` => `none`', () => {
+    const options = {
+      path: '/',
+      'show-vulnerable-paths': 'false',
+    };
+    const updated = setDefaultTestOptions(options);
+    expect(updated).toEqual({
+      org: undefined,
+      path: '/',
+      showVulnPaths: 'none',
+    });
+  });
+
+  it('with show-vulnerable-paths as boolean => `some`', () => {
+    const options = {
+      path: '/',
+      'show-vulnerable-paths': true,
+    };
+    const updated = setDefaultTestOptions(options as any);
+    expect(updated).toEqual({
+      org: undefined,
+      path: '/',
+      showVulnPaths: 'some',
+    });
+  });
+
+  it('with show-vulnerable-paths set to `none` => `none`', () => {
+    const options = {
+      path: '/',
+      'show-vulnerable-paths': 'none',
+    };
+    const updated = setDefaultTestOptions(options);
+    expect(updated).toEqual({
+      org: undefined,
+      path: '/',
+      showVulnPaths: 'none',
+    });
+  });
+
+  it('with show-vulnerable-paths set to `true` => `some`', () => {
+    const options = {
+      path: '/',
+      'show-vulnerable-paths': 'true',
+    };
+    const updated = setDefaultTestOptions(options);
+    expect(updated).toEqual({
+      org: undefined,
+      path: '/',
+      showVulnPaths: 'some',
+    });
+  });
+
+  it('with show-vulnerable-paths set to `some` => `some`', () => {
+    const options = {
+      path: '/',
+      'show-vulnerable-paths': 'some',
+    };
+    const updated = setDefaultTestOptions(options);
+    expect(updated).toEqual({
+      org: undefined,
+      path: '/',
+      showVulnPaths: 'some',
+    });
+  });
+
+  it('with show-vulnerable-paths set to `all` => `all`', () => {
+    const options = {
+      path: '/',
+      'show-vulnerable-paths': 'all',
+    };
+    const updated = setDefaultTestOptions(options);
+    expect(updated).toEqual({
+      org: undefined,
+      path: '/',
+      showVulnPaths: 'all',
+    });
+  });
+
+  it('with org set', () => {
+    const updated = setDefaultTestOptions({ path: '/', org: 'my-org' });
+    expect(updated).toEqual({
+      org: 'my-org',
+      path: '/',
+      showVulnPaths: 'some',
+    });
+  });
+});
