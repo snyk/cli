091f9e8a4605d72c3548cf04d0a21c348a90b624
ghe <lili@snyk.io>
2021-04-16 16:18:47 +0100

    feat: show debug mode tip if something failed

--- packages/snyk-fix/src/lib/errors/missing-file-name.ts
+++ packages/snyk-fix/src/lib/errors/missing-file-name.ts
@@ -3,7 +3,7 @@ import { CustomError, ERROR_CODES } from './custom-error';
 export class MissingFileNameError extends CustomError {
   public constructor() {
     super(
-      'Filename is missing from test result. Please contact support@snyk.io.',
+      'Filename is missing from test result',
       ERROR_CODES.MissingFileName,
     );
   }
