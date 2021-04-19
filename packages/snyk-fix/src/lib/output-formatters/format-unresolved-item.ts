5e52bc589636ce9c09bf7e86fd736993e57e5102
ghe <lili@snyk.io>
2021-04-19 10:47:08 +0100

    feat: errored items can show a tip

--- packages/snyk-fix/src/lib/output-formatters/format-unresolved-item.ts
+++ packages/snyk-fix/src/lib/output-formatters/format-unresolved-item.ts
@@ -5,11 +5,14 @@ import { PADDING_SPACE } from './show-results-summary';
 export function formatUnresolved(
   entity: EntityToFix,
   userMessage: string,
+  tip?: string,
 ): string {
   const name =
     entity.scanResult.identity.targetFile ||
     `${entity.scanResult.identity.type} project`;
-  return `${PADDING_SPACE}${name}\n${PADDING_SPACE}${chalk.red(
+  const tipMessage = tip ? `\n${PADDING_SPACE}Tip:     ${tip}` : '';
+  const errorMessage = `${PADDING_SPACE}${name}\n${PADDING_SPACE}${chalk.red(
     '✖',
   )} ${chalk.red(userMessage)}`;
+  return errorMessage + tipMessage;
 }
