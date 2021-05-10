dcb5b07477bcf518cd49d933b50c8a5c4c4cc320
Jahed Ahmed <jahed@snyk.io>
2021-05-10 15:19:12 +0000

    refactor: drop needle dependency
    Co-authored-by: Jeff McLean <maxjeffos@users.noreply.github.com>

--- src/lib/request/request.ts
+++ src/lib/request/request.ts
@@ -1,26 +1,26 @@
 import { debug as debugModule } from 'debug';
-import * as needle from 'needle';
-import { parse, format } from 'url';
-import * as querystring from 'querystring';
-import * as zlib from 'zlib';
-import * as config from '../config';
-import { getProxyForUrl } from 'proxy-from-env';
 import { bootstrap } from 'global-agent';
-import * as analytics from '../analytics';
-import { Global } from '../../cli/args';
-import { Payload } from './types';
-import { getVersion } from '../version';
-import * as https from 'https';
 import * as http from 'http';
+import * as https from 'https';
+import { RequestOptions } from 'https';
+import { getProxyForUrl } from 'proxy-from-env';
+import * as querystring from 'querystring';
+import { format } from 'url';
+import * as zlib from 'zlib';
+import { Global } from '../../cli/args';
+import * as analytics from '../analytics';
+import * as config from '../config';
+import { getVersion } from '../version';
+import { Payload } from './types';
 
 const debug = debugModule('snyk:req');
 const snykDebug = debugModule('snyk');
 
 declare const global: Global;
 
-export = function makeRequest(
+export = async function makeRequest(
   payload: Payload,
-): Promise<{ res: needle.NeedleResponse; body: any }> {
+): Promise<{ res: http.IncomingMessage; body: any }> {
   // This ensures we support lowercase http(s)_proxy values as well
   // The weird IF around it ensures we don't create an envvar with a value of undefined, which throws error when trying to use it as a proxy
   if (process.env.HTTP_PROXY || process.env.http_proxy) {
@@ -31,130 +31,146 @@ export = function makeRequest(
       process.env.HTTPS_PROXY || process.env.https_proxy;
   }
 
-  return getVersion().then(
-    (versionNumber) =>
-      new Promise((resolve, reject) => {
-        const body = payload.body;
-        let data;
+  const versionNumber = await getVersion();
 
-        delete payload.body;
+  const body = payload.body;
+  let data;
 
-        if (!payload.headers) {
-          payload.headers = {};
-        }
+  delete payload.body;
 
-        payload.headers['x-snyk-cli-version'] = versionNumber;
+  if (!payload.headers) {
+    payload.headers = {};
+  }
 
-        if (body) {
-          const json = JSON.stringify(body);
-          if (json.length < 1e4) {
-            debug(JSON.stringify(body, null, 2));
-          }
+  payload.headers['x-snyk-cli-version'] = versionNumber;
 
-          // always compress going upstream
-          data = zlib.gzipSync(json, { level: 9 });
+  if (body) {
+    const json = JSON.stringify(body);
+    if (json.length < 1e4) {
+      debug(JSON.stringify(body, null, 2));
+    }
 
-          snykDebug('sending request to:', payload.url);
-          snykDebug('request body size:', json.length);
-          snykDebug('gzipped request body size:', data.length);
+    // always compress going upstream
+    data = zlib.gzipSync(json, { level: 9 });
 
-          let callGraphLength: number | null = null;
-          if (body.callGraph) {
-            callGraphLength = JSON.stringify(body.callGraph).length;
-            snykDebug('call graph size:', callGraphLength);
-          }
+    snykDebug('sending request to:', payload.url);
+    snykDebug('request body size:', json.length);
+    snykDebug('gzipped request body size:', data.length);
 
-          if (!payload.url.endsWith('/analytics/cli')) {
-            analytics.add('payloadSize', json.length);
-            analytics.add('gzippedPayloadSize', data.length);
+    let callGraphLength: number | null = null;
+    if (body.callGraph) {
+      callGraphLength = JSON.stringify(body.callGraph).length;
+      snykDebug('call graph size:', callGraphLength);
+    }
 
-            if (callGraphLength) {
-              analytics.add('callGraphPayloadSize', callGraphLength);
-            }
-          }
+    if (!payload.url.endsWith('/analytics/cli')) {
+      analytics.add('payloadSize', json.length);
+      analytics.add('gzippedPayloadSize', data.length);
 
-          payload.headers['content-encoding'] = 'gzip';
-          payload.headers['content-length'] = data.length;
-        }
+      if (callGraphLength) {
+        analytics.add('callGraphPayloadSize', callGraphLength);
+      }
+    }
 
-        const parsedUrl = parse(payload.url);
+    payload.headers['content-encoding'] = 'gzip';
+    payload.headers['content-length'] = data.length;
+  }
 
-        if (
-          parsedUrl.protocol === 'http:' &&
-          parsedUrl.hostname !== 'localhost' &&
-          process.env.SNYK_HTTP_PROTOCOL_UPGRADE !== '0'
-        ) {
-          debug('forcing api request to https');
-          parsedUrl.protocol = 'https:';
-          payload.url = format(parsedUrl);
-        }
+  const parsedUrl = new URL(payload.url);
 
-        // prefer config timeout unless payload specified
-        if (!payload.hasOwnProperty('timeout')) {
-          payload.timeout = config.timeout * 1000; // s -> ms
-        }
+  if (
+    parsedUrl.protocol === 'http:' &&
+    parsedUrl.hostname !== 'localhost' &&
+    process.env.SNYK_HTTP_PROTOCOL_UPGRADE !== '0'
+  ) {
+    debug('forcing api request to https');
+    parsedUrl.protocol = 'https:';
+    payload.url = format(parsedUrl);
+  }
 
-        try {
-          debug('request payload: ', JSON.stringify(payload));
-        } catch (e) {
-          debug('request payload is too big to log', e);
-        }
+  // prefer config timeout unless payload specified
+  if (!payload.hasOwnProperty('timeout')) {
+    payload.timeout = config.timeout * 1000; // s -> ms
+  }
 
-        const method = (
-          payload.method || 'get'
-        ).toLowerCase() as needle.NeedleHttpVerbs;
-        let url = payload.url;
+  try {
+    debug('request payload: ', JSON.stringify(payload));
+  } catch (e) {
+    debug('request payload is too big to log', e);
+  }
 
-        if (payload.qs) {
-          // Parse the URL and append the search part - this will take care of adding the '/?' part if it's missing
-          const urlObject = new URL(url);
-          urlObject.search = querystring.stringify(payload.qs);
-          url = urlObject.toString();
-          delete payload.qs;
-        }
+  const method: Required<RequestOptions['method']> = (
+    payload.method || 'get'
+  ).toLowerCase();
+  let url = payload.url;
 
-        const agent =
-          parsedUrl.protocol === 'http:'
-            ? new http.Agent({ keepAlive: true })
-            : new https.Agent({ keepAlive: true });
-        const options: needle.NeedleOptions = {
-          json: payload.json,
-          headers: payload.headers,
-          timeout: payload.timeout,
-          // eslint-disable-next-line @typescript-eslint/camelcase
-          follow_max: 5,
-          family: payload.family,
-          agent,
-        };
+  if (payload.qs) {
+    // Parse the URL and append the search part - this will take care of adding the '/?' part if it's missing
+    const urlObject = new URL(url);
+    urlObject.search = querystring.stringify(payload.qs);
+    url = urlObject.toString();
+    delete payload.qs;
+  }
 
-        const proxyUri = getProxyForUrl(url);
-        if (proxyUri) {
-          snykDebug('using proxy:', proxyUri);
-          bootstrap({
-            environmentVariableNamespace: '',
-          });
-        } else {
-          snykDebug('not using proxy');
-        }
+  const options: RequestOptions = {
+    headers: payload.headers,
+    timeout: payload.timeout,
+    family: payload.family,
+  };
 
-        if (global.ignoreUnknownCA) {
-          debug('Using insecure mode (ignore unknown certificate authority)');
-          options.rejectUnauthorized = false;
-        }
+  const proxyUri = getProxyForUrl(url);
+  if (proxyUri) {
+    snykDebug('using proxy:', proxyUri);
+    bootstrap({
+      environmentVariableNamespace: '',
+    });
+  } else {
+    snykDebug('not using proxy');
+  }
 
-        needle.request(method, url, data, options, (err, res, respBody) => {
-          debug(err);
-          debug(
-            'response (%s): ',
-            (res || {}).statusCode,
-            JSON.stringify(respBody),
-          );
-          if (err) {
-            return reject(err);
-          }
+  if (global.ignoreUnknownCA) {
+    debug('Using insecure mode (ignore unknown certificate authority)');
+    options.rejectUnauthorized = false;
+  }
 
-          resolve({ res, body: respBody });
-        });
-      }),
-  );
+  try {
+    const response = await request(method, url, data, options);
+    debug(
+      'response (%s): ',
+      response.res.statusCode,
+      JSON.stringify(response.body),
+    );
+    return response;
+  } catch (err) {
+    debug(err);
+    throw err;
+  }
 };
+
+async function request(
+  method: RequestOptions['method'],
+  url: string,
+  data?: any,
+  options: RequestOptions = {},
+): Promise<{ res: http.IncomingMessage; body: string }> {
+  return new Promise((resolve, reject) => {
+    const client = new URL(url).protocol === 'https:' ? https : http;
+    const requestOptions = {
+      ...options,
+      method,
+      agent: new client.Agent({ keepAlive: true }),
+    };
+    const request = client.request(url, requestOptions, (response) => {
+      const body: any[] = [];
+      response.on('data', (chunk: any) => body.push(Buffer.from(chunk)));
+      response.on('end', () =>
+        resolve({ res: response, body: Buffer.concat(body).toString('utf-8') }),
+      );
+    });
+    request.on('error', reject);
+
+    if (data) {
+      request.write(data);
+    }
+  });
+}
