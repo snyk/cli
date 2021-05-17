07132ed24dd48ec780548781958c2ff622a3079b
Jahed Ahmed <jahed@snyk.io>
2021-05-17 17:56:46 +0000

    test(protect): add smoke test

---
+++ packages/snyk-protect/test/smoke/fix-pr-prepublish-hook.spec.ts
@@ -0,0 +1,106 @@
+import * as fse from 'fs-extra';
+import * as path from 'path';
+import * as uuid from 'uuid';
+import { spawn, SpawnOptionsWithoutStdio } from 'child_process';
+
+type TestProject = {
+  path: (filePath?: string) => string;
+  file: (filePath: string) => Promise<string>;
+};
+
+type RunCLIResult = {
+  code: number;
+  stdout: string;
+  stderr: string;
+};
+
+type RunCLIOptions = SpawnOptionsWithoutStdio;
+
+const runCommand = (
+  command: string,
+  args: string[],
+  options?: RunCLIOptions,
+): Promise<RunCLIResult> => {
+  return new Promise((resolve, reject) => {
+    const cli = spawn(command, args, options);
+    const stdout: Buffer[] = [];
+    const stderr: Buffer[] = [];
+
+    cli.on('error', (error) => {
+      reject(error);
+    });
+
+    cli.stdout.on('data', (chunk) => {
+      stdout.push(Buffer.from(chunk));
+    });
+
+    cli.stderr.on('data', (chunk) => {
+      stderr.push(Buffer.from(chunk));
+    });
+
+    cli.on('close', (code) => {
+      resolve({
+        code: code || 0,
+        stdout: Buffer.concat(stdout)
+          .toString('utf-8')
+          .trim(),
+        stderr: Buffer.concat(stderr)
+          .toString('utf-8')
+          .trim(),
+      });
+    });
+  });
+};
+
+const getPatchedLodash = (): Promise<string> => {
+  const patchedLodashPath = path.resolve(
+    __dirname,
+    '../fixtures/patchable-file-lodash/lodash-expected-patched.js',
+  );
+
+  return fse.readFile(patchedLodashPath, 'utf-8');
+};
+
+jest.setTimeout(1000 * 60);
+
+describe('Fix PR "prepublish" hook', () => {
+  let tempFolder: string;
+
+  const createProject = async (fixture: string): Promise<TestProject> => {
+    const fixturePath = path.resolve(__dirname, '../fixtures', fixture);
+    const projectPath = path.resolve(tempFolder, fixture);
+    await fse.copy(fixturePath, projectPath);
+    return {
+      path: (filePath = '') => path.resolve(projectPath, filePath),
+      file: (filePath: string) => {
+        const fullFilePath = path.resolve(projectPath, filePath);
+        return fse.readFile(fullFilePath, 'utf-8');
+      },
+    };
+  };
+
+  beforeEach(async () => {
+    tempFolder = path.resolve(__dirname, '__outputs__', uuid.v4());
+    await fse.ensureDir(tempFolder);
+  });
+
+  afterEach(async () => {
+    await fse.remove(tempFolder);
+    jest.restoreAllMocks();
+  });
+
+  test('patches vulnerable dependencies on install', async () => {
+    const project = await createProject('fix-pr-prepublish-hook');
+    const patchedLodash = await getPatchedLodash();
+    const lodashPath = 'node_modules/lodash/lodash.js';
+
+    const { code, stdout, stderr } = await runCommand('npm', ['install'], {
+      cwd: project.path(),
+    });
+
+    expect(stdout).toMatch(`patched ${project.path(lodashPath)}`);
+    expect(stderr).toEqual('');
+    expect(code).toEqual(0);
+    expect(project.file(lodashPath)).resolves.toEqual(patchedLodash);
+  });
+});
