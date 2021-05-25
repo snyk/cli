8e2699a2d477b5d99eb5dd518ed0b7c00730e879
Jahed Ahmed <jahed@snyk.io>
2021-05-25 12:33:19 +0000

    chore: upload test results to circlecI

--- check-dependencies.config.ts
+++ check-dependencies.config.ts
@@ -4,6 +4,8 @@ export const config: Options = {
   ignoreMatches: [
     'sarif', // we only use @types/sarif. https://github.com/depcheck/depcheck/issues/640
     '@types/jest', // jest is a global so impossible to detect usage of types
+    'jest-junit', // used in circleci
+    'tap-junit' // used in circleci
   ],
   ignoreDirs: ['node_modules', 'dist', 'fixtures', 'test-output'],
 };
--- jest.config.js
+++ jest.config.js
@@ -11,12 +11,12 @@ module.exports = {
     '<rootDir>/packages/**/test/**/*.spec.ts',
     '<rootDir>/test/jest/unit/**/*.spec.ts',
     '<rootDir>/test/jest/system/**/*.spec.ts',
-    '<rootDir>/test/jest/acceptance/**/*.spec.ts'
+    '<rootDir>/test/jest/acceptance/**/*.spec.ts',
   ],
   modulePathIgnorePatterns: [
     '<rootDir>/test/.*fixtures/*',
     '<rootDir>/packages/.+/test/.*fixtures/*',
     '<rootDir>/test/acceptance/*',
-    '<rootDir>/test/acceptance/workspaces/*' // to avoid `jest-haste-map: Haste module naming collision` errors
+    '<rootDir>/test/acceptance/workspaces/*', // to avoid `jest-haste-map: Haste module naming collision` errors
   ],
 };
