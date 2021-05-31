81fb7449c47a4801eb56823304166cfedd2a82b5
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2021-05-31 12:31:56 -0400

    chore: fix new tap test warning in DangerJS

--- dangerfile.js
+++ dangerfile.js
@@ -51,20 +51,19 @@ if (danger.github && danger.github.pr) {
     );
   }
 
+  // `.spec.ts` is always used for Jest tests
+  // `.test.ts` is normally used for Tap tests and but there are also `.spec.ts` files which are used be Tap tests in test/acceptance.
+  // either way, we should warn about new `.test.ts` or `.spec.ts` files being created outside the `/test/jest` folder
   const newTestFiles = danger.git.created_files.filter((f) => {
     const inTestFolder = f.startsWith('test/');
-    const inLegacyAcceptanceTestsFolder = f.includes('test/acceptance/');
-    const testFilenameLooksLikeJest = f.includes('.spec.ts');
-    return (
-      inTestFolder &&
-      !inLegacyAcceptanceTestsFolder &&
-      !testFilenameLooksLikeJest
-    );
+    const isATestFile = f.includes('.test.ts') || f.includes('.spec.ts');
+    const inJestFolder = f.startsWith('test/jest/');
+    return inTestFolder && isATestFile && !inJestFolder;
   });
 
   if (newTestFiles.length) {
     const joinedFileList = newTestFiles.map((f) => '- `' + f + '`').join('\n');
-    const msg = `Looks like you added a new Tap test. Consider making it a Jest test instead. See files like \`test/*.spec.ts\` for examples. Files found:\n${joinedFileList}`;
+    const msg = `Looks like you added a new Tap test. Consider making it a Jest test instead. See files in \`test/jest/(unit|system|acceptance)\` for examples. Files found:\n${joinedFileList}`;
     warn(msg);
   }
 
