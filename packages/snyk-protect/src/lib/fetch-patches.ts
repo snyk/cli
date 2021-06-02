2ad30ad11aa71647a92306fcc806d839deb485dd
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2021-06-02 13:35:48 -0400

    refactor: extract getApiBaseUrl to module for re-use

--- packages/snyk-protect/src/lib/fetch-patches.ts
+++ packages/snyk-protect/src/lib/fetch-patches.ts
@@ -1,22 +1,6 @@
 import { PatchInfo, Patch, VulnPatches, VulnIdAndPackageName } from './types';
 import { request } from './http';
-
-function getApiBaseUrl(): string {
-  let apiBaseUrl = 'https://snyk.io/api';
-  if (process.env.SNYK_API) {
-    if (process.env.SNYK_API.endsWith('/api')) {
-      apiBaseUrl = process.env.SNYK_API;
-    } else if (process.env.SNYK_API.endsWith('/api/v1')) {
-      // snyk CI environment - we use `.../api/v1` though the norm is just `.../api`
-      apiBaseUrl = process.env.SNYK_API.replace('/v1', '');
-    } else {
-      console.log(
-        'Malformed SNYK_API value. Using default: https://snyk.io/api',
-      );
-    }
-  }
-  return apiBaseUrl;
-}
+import { getApiBaseUrl } from './snyk-api';
 
 export async function fetchPatches(
   vulnId: string,
---
+++ packages/snyk-protect/src/lib/snyk-api.ts
@@ -0,0 +1,16 @@
+export function getApiBaseUrl(): string {
+  let apiBaseUrl = 'https://snyk.io/api';
+  if (process.env.SNYK_API) {
+    if (process.env.SNYK_API.endsWith('/api')) {
+      apiBaseUrl = process.env.SNYK_API;
+    } else if (process.env.SNYK_API.endsWith('/api/v1')) {
+      // snyk CI environment - we use `.../api/v1` though the norm is just `.../api`
+      apiBaseUrl = process.env.SNYK_API.replace('/v1', '');
+    } else {
+      console.log(
+        'Malformed SNYK_API value. Using default: https://snyk.io/api',
+      );
+    }
+  }
+  return apiBaseUrl;
+}
