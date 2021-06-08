d4268be387f3f552a182a73d036f1b95aed36349
Jahed Ahmed <jahed@snyk.io>
2021-06-08 15:15:47 +0000

    chore: remove unused babelrc

--- packages/snyk-protect/jest.config.js
+++ packages/snyk-protect/jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {}, // ignore .babelrc file
   collectCoverage: false, // not collecting coverage for now
   collectCoverageFrom: ['src/**/*.ts'],
   coverageReporters: ['text-summary', 'html'],
