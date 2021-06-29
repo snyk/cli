cc1835ccde744f04660d83baafcefb6afeabfb54
Mathias Fischer <mathias.fischer@snyk.io>
2021-06-29 18:08:52 +0200

    refactor: fix api token import.

--- src/lib/plugins/sast/checks.ts
+++ src/lib/plugins/sast/checks.ts
@@ -1,5 +1,5 @@
 import request = require('../../request');
-import snyk = require('../..');
+import { api as getApiToken } from '../../api-token';
 import * as config from '../../config';
 import { assembleQueryString } from '../../snyk-test/common';
 
@@ -18,7 +18,7 @@ export async function getSastSettingsForOrg(org): Promise<SastSettings> {
   const response = await request({
     method: 'GET',
     headers: {
-      Authorization: `token ${snyk.api}`,
+      Authorization: `token ${getApiToken()}`,
     },
     qs: assembleQueryString({ org }),
     url: `${config.API}/cli-config/settings/sast`,
@@ -33,7 +33,7 @@ export async function trackUsage(org): Promise<TrackUsageResponse> {
   const response = await request({
     method: 'POST',
     headers: {
-      Authorization: `token ${snyk.api}`,
+      Authorization: `token ${getApiToken()}`,
     },
     qs: assembleQueryString({ org }),
     url: `${config.API}/track-sast-usage/cli`,
