931b9fc7d372f5e60b486fcf457dea620ef708d3
Avishag Israeli <avishag.israeli@snyk.io>
2021-07-08 12:51:52 +0300

    test: migrate api-key-from-env test to jest
    test: merge api-key-from-env and api-key tests
    
    test: delete api-key and api-key-from-env tests

--- test/api-key-from-env.test.ts
+++
@@ -1,8 +0,0 @@
-import { test } from 'tap';
-const key = (process.env.SNYK_TOKEN = '123456');
-import * as snyk from '../src/lib';
-
-test('api token from env', (t) => {
-  t.equal(key, snyk.api, 'current api value env value');
-  t.end();
-});
