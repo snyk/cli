532d95d7f6ff63b6940a9107b8d87685a884b729
Jahed Ahmed <jahed@snyk.io>
2021-07-16 13:08:05 +0000

    test(protect): update to match fix pr setup

---
+++ packages/snyk-protect/test/acceptance/fix-pr.smoke.spec.ts
@@ -0,0 +1,28 @@
+import { createProject } from '../util/createProject';
+import { getPatchedLodash } from '../util/getPatchedLodash';
+import { RunCLIResult, runCommand } from '../util/runCommand';
+
+jest.setTimeout(1000 * 60);
+
+describe('Fix PR', () => {
+  test('patches vulnerable dependencies on install', async () => {
+    const project = await createProject('fix-pr');
+    const patchedLodash = await getPatchedLodash();
+
+    expect(
+      await runCommand('npm', ['install'], {
+        cwd: project.path(),
+      }),
+    ).toEqual(
+      expect.objectContaining<RunCLIResult>({
+        code: 0,
+        stdout: expect.stringContaining('Successfully applied Snyk patches'),
+        stderr: expect.any(String),
+      }),
+    );
+
+    await expect(
+      project.read('node_modules/lodash/lodash.js'),
+    ).resolves.toEqual(patchedLodash);
+  });
+});
