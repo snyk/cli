e974b9dbc32dffc5138282bf4bc85d037f75d1f2
Jahed Ahmed <jahed@snyk.io>
2021-07-23 15:49:13 +0000

    test: use binary from cdn for smoke tests

--- test/smoke/alpine/entrypoint.sh
+++ test/smoke/alpine/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-echo "install snyk with binary"
-snyk_cli_dl=$(curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/snyk/snyk/releases/latest | jq --raw-output '(.assets[])? | select(.name == "snyk-alpine") | .browser_download_url')
-echo "snyk_cli_dl: $snyk_cli_dl"
-curl -Lo ./snyk-cli "$snyk_cli_dl"
+curl -Lo ./snyk-cli 'https://static.snyk.io/cli/latest/snyk-alpine'
 chmod -R +x ./snyk-cli
 mv ./snyk-cli /usr/local/bin/snyk
 snyk --version
--- test/smoke/install-snyk-binary-win.sh
+++ test/smoke/install-snyk-binary-win.sh
@@ -1,7 +1,4 @@
-echo "install-snyk-binary-win.sh"
-snyk_cli_dl=$(curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/snyk/snyk/releases/latest | jq --raw-output '(.assets[])? | select(.name == "snyk-win.exe") | .browser_download_url')
-echo "snyk_cli_dl: ${snyk_cli_dl}"
-curl -Lo ./snyk-cli.exe $snyk_cli_dl
+curl -Lo ./snyk-cli.exe 'https://static.snyk.io/cli/latest/snyk-win.exe'
 ./snyk-cli.exe --version
 chmod -R +x ./snyk-cli
 mv ./snyk-cli.exe "/bin/snyk.exe"
