d49bfffa82de81e5efbb03aecfe75d5b1cc3c38e
ofir-eldar-snyk <ofir.eldar@snyk.io>
2021-08-23 12:36:34 +0300

    test: ansi escape codes

--- test/jest/unit/common.spec.ts
+++ test/jest/unit/common.spec.ts
@@ -2,6 +2,15 @@ import { colorTextBySeverity } from '../../../src/lib/snyk-test/common';
 import { color } from '../../../src/lib/theme';
 import { SEVERITY } from '../../../src/lib/snyk-test/common';
 
+const ansiColorMap = {
+  red: '\x1B[31m',
+  yellow: '\x1B[33m',
+  green: '\x1b[32m',
+  magenta: '\x1b[35m',
+  white: '\x1b[37m',
+  default: '\x1b[39m',
+};
+
 describe('colorTextBySeverity', () => {
   it('Returns a high severity colored text', () => {
     const severity = SEVERITY.HIGH;
@@ -39,5 +48,26 @@ describe('colorTextBySeverity', () => {
     expect(colorTextBySeverity(severity, 'Pls help me')).toEqual(
       color.severity[lowerCaseSeverity]('Pls help me'),
     );
+  });
+
+  describe("Pass if text for each severity is colorized to it's matching escape code color", () => {
+    it('Medium severity', () => {
+      const severity = SEVERITY.MEDIUM;
+      expect(colorTextBySeverity(severity, 'Pls help me')).toMatch(
+        ansiColorMap.yellow + 'Pls help me' + ansiColorMap.default,
+      );
+    });
+    it('High severity', () => {
+      const severity = SEVERITY.HIGH;
+      expect(colorTextBySeverity(severity, 'Pls help me')).toMatch(
+        ansiColorMap.red + 'Pls help me' + ansiColorMap.default,
+      );
+    });
+    it('Critical severity', () => {
+      const severity = SEVERITY.CRITICAL;
+      expect(colorTextBySeverity(severity, 'Pls help me')).toMatch(
+        ansiColorMap.magenta + 'Pls help me' + ansiColorMap.default,
+      );
+    });
   });
 });
