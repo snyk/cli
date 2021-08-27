7fc7ecf4ddbede148f1bc899a0116a3631e71af8
Jahed Ahmed <jahed@snyk.io>
2021-08-27 20:09:33 +0000

    refactor: replace callbacks with async/await

--- src/lib/monitor/index.ts
+++ src/lib/monitor/index.ts
@@ -1,6 +1,5 @@
 import * as Debug from 'debug';
 import * as path from 'path';
-
 import * as depGraphLib from '@snyk/dep-graph';
 import * as snyk from '..';
 import { apiOrOAuthTokenExists, getAuthHeader } from '../api-token';
@@ -20,7 +19,6 @@ import {
   Contributor,
 } from '../types';
 import * as projectMetadata from '../project-metadata';
-
 import {
   MonitorError,
   ConnectionTimeoutError,
@@ -54,8 +52,6 @@ import { abridgeErrorMessage } from '../error-format';
 const debug = Debug('snyk');
 
 const ANALYTICS_PAYLOAD_MAX_LENGTH = 1024;
-
-// TODO(kyegupov): clean up the type, move to snyk-cli-interface repository
 
 interface MonitorBody {
   meta: Meta;
@@ -187,8 +183,6 @@ async function monitorDepTree(
     root,
     meta.isDocker ? 'docker' : packageManager!,
     options,
-    // TODO: fix this and send only send when we used resolve-deps for node
-    // it should be a ExpandedPkgTree type instead
     depTree,
     targetFileDir,
   );
@@ -238,91 +232,74 @@ async function monitorDepTree(
     callGraphPayload = callGraph;
   }
 
-  // TODO(kyegupov): async/await
-  return new Promise((resolve, reject) => {
-    if (!depTree) {
-      debug(
-        'scannedProject is missing depGraph or depTree, cannot run test/monitor',
-      );
-      return reject(
-        new FailedToRunTestError(
-          'Your monitor request could not be completed. Please email support@snyk.io',
-        ),
-      );
-    }
-    makeRequest(
-      {
-        body: {
-          meta: {
-            method: meta.method,
-            hostname: os.hostname(),
-            id: snyk.id || depTree.name,
-            ci: isCI(),
-            pid: process.pid,
-            node: process.version,
-            master: snyk.config.isMaster,
-            name: getNameDepTree(scannedProject, depTree, meta),
-            version: depTree.version,
-            org: config.org ? decodeURIComponent(config.org) : undefined,
-            pluginName: pluginMeta.name,
-            pluginRuntime: pluginMeta.runtime,
-            missingDeps: treeMissingDeps,
-            dockerImageId: pluginMeta.dockerImageId,
-            dockerBaseImage: depTree.docker
-              ? depTree.docker.baseImage
-              : undefined,
-            dockerfileLayers: depTree.docker
-              ? depTree.docker.dockerfileLayers
-              : undefined,
-            projectName: getProjectName(scannedProject, meta),
-            prePruneDepCount, // undefined unless 'prune' is used,
-            monitorGraph: false,
-            versionBuildInfo: JSON.stringify(
-              scannedProject.meta?.versionBuildInfo,
-            ),
-            gradleProjectName: scannedProject.meta?.gradleProjectName,
-            platform: scannedProject.meta?.platform,
-          },
-          policy: policy ? policy.toString() : undefined,
-          package: depTree,
-          callGraph: callGraphPayload,
-          // we take the targetFile from the plugin,
-          // because we want to send it only for specific package-managers
-          target,
-          // WARNING: be careful changing this as it affects project uniqueness
-          targetFile: getTargetFile(scannedProject, pluginMeta),
-          targetFileRelativePath,
-          contributors,
-        } as MonitorBody,
-        gzip: true,
-        method: 'PUT',
-        headers: {
-          authorization: getAuthHeader(),
-          'content-encoding': 'gzip',
-        },
-        url: config.API + '/monitor/' + packageManager,
-        json: true,
-      },
-      (error, res, body) => {
-        if (error) {
-          return reject(error);
-        }
-
-        if (res.statusCode >= 200 && res.statusCode <= 299) {
-          resolve(body as MonitorResult);
-        } else {
-          let err;
-          const userMessage = body && body.userMessage;
-          if (!userMessage && res.statusCode === 504) {
-            err = new ConnectionTimeoutError();
-          } else {
-            err = new MonitorError(res.statusCode, userMessage);
-          }
-          reject(err);
-        }
-      },
+  if (!depTree) {
+    debug(
+      'scannedProject is missing depGraph or depTree, cannot run test/monitor',
     );
+    throw new FailedToRunTestError(
+      'Your monitor request could not be completed. Please email support@snyk.io',
+    );
+  }
+
+  const { res, body } = await makeRequest({
+    body: {
+      meta: {
+        method: meta.method,
+        hostname: os.hostname(),
+        id: snyk.id || depTree.name,
+        ci: isCI(),
+        pid: process.pid,
+        node: process.version,
+        master: snyk.config.isMaster,
+        name: getNameDepTree(scannedProject, depTree, meta),
+        version: depTree.version,
+        org: config.org ? decodeURIComponent(config.org) : undefined,
+        pluginName: pluginMeta.name,
+        pluginRuntime: pluginMeta.runtime,
+        missingDeps: treeMissingDeps,
+        dockerImageId: pluginMeta.dockerImageId,
+        dockerBaseImage: depTree.docker ? depTree.docker.baseImage : undefined,
+        dockerfileLayers: depTree.docker
+          ? depTree.docker.dockerfileLayers
+          : undefined,
+        projectName: getProjectName(scannedProject, meta),
+        prePruneDepCount, // undefined unless 'prune' is used,
+        monitorGraph: false,
+        versionBuildInfo: JSON.stringify(scannedProject.meta?.versionBuildInfo),
+        gradleProjectName: scannedProject.meta?.gradleProjectName,
+        platform: scannedProject.meta?.platform,
+      },
+      policy: policy ? policy.toString() : undefined,
+      package: depTree,
+      callGraph: callGraphPayload,
+      // we take the targetFile from the plugin,
+      // because we want to send it only for specific package-managers
+      target,
+      // WARNING: be careful changing this as it affects project uniqueness
+      targetFile: getTargetFile(scannedProject, pluginMeta),
+      targetFileRelativePath,
+      contributors,
+    } as MonitorBody,
+    gzip: true,
+    method: 'PUT',
+    headers: {
+      authorization: getAuthHeader(),
+      'content-encoding': 'gzip',
+    },
+    url: config.API + '/monitor/' + packageManager,
+    json: true,
   });
+
+  if (res.statusCode && res.statusCode >= 200 && res.statusCode <= 299) {
+    return body as MonitorResult;
+  } else {
+    const userMessage = body && body.userMessage;
+    if (!userMessage && res.statusCode === 504) {
+      throw new ConnectionTimeoutError();
+    } else {
+      throw new MonitorError(res.statusCode, userMessage);
+    }
+  }
 }
 
 export async function monitorDepGraph(
@@ -408,78 +385,65 @@ export async function monitorDepGraph(
     callGraphPayload = callGraph;
   }
 
-  return new Promise((resolve, reject) => {
-    if (!depGraph) {
-      debug(
-        'scannedProject is missing depGraph or depTree, cannot run test/monitor',
-      );
-      return reject(
-        new FailedToRunTestError(
-          'Your monitor request could not be completed. Please email support@snyk.io',
-        ),
-      );
-    }
-    makeRequest(
-      {
-        body: {
-          meta: {
-            method: meta.method,
-            hostname: os.hostname(),
-            id: snyk.id || depGraph.rootPkg.name,
-            ci: isCI(),
-            pid: process.pid,
-            node: process.version,
-            master: snyk.config.isMaster,
-            name: getNameDepGraph(scannedProject, depGraph, meta),
-            version: depGraph.rootPkg.version,
-            org: config.org ? decodeURIComponent(config.org) : undefined,
-            pluginName: pluginMeta.name,
-            pluginRuntime: pluginMeta.runtime,
-            projectName: getProjectName(scannedProject, meta),
-            monitorGraph: true,
-            versionBuildInfo: JSON.stringify(
-              scannedProject.meta?.versionBuildInfo,
-            ),
-            gradleProjectName: scannedProject.meta?.gradleProjectName,
-          },
-          policy: policy ? policy.toString() : undefined,
-          depGraphJSON: depGraph, // depGraph will be auto serialized to JSON on send
-          // we take the targetFile from the plugin,
-          // because we want to send it only for specific package-managers
-          target,
-          targetFile: getTargetFile(scannedProject, pluginMeta),
-          targetFileRelativePath,
-          contributors,
-          callGraph: callGraphPayload,
-        } as MonitorBody,
-        gzip: true,
-        method: 'PUT',
-        headers: {
-          authorization: getAuthHeader(),
-          'content-encoding': 'gzip',
-        },
-        url: `${config.API}/monitor/${packageManager}/graph`,
-        json: true,
-      },
-      (error, res, body) => {
-        if (error) {
-          return reject(error);
-        }
-        if (res.statusCode >= 200 && res.statusCode <= 299) {
-          resolve(body as MonitorResult);
-        } else {
-          let err;
-          const userMessage = body && body.userMessage;
-          if (!userMessage && res.statusCode === 504) {
-            err = new ConnectionTimeoutError();
-          } else {
-            err = new MonitorError(res.statusCode, userMessage);
-          }
-          reject(err);
-        }
-      },
+  if (!depGraph) {
+    debug(
+      'scannedProject is missing depGraph or depTree, cannot run test/monitor',
     );
+    throw new FailedToRunTestError(
+      'Your monitor request could not be completed. Please email support@snyk.io',
+    );
+  }
+
+  const { res, body } = await makeRequest({
+    body: {
+      meta: {
+        method: meta.method,
+        hostname: os.hostname(),
+        id: snyk.id || depGraph.rootPkg.name,
+        ci: isCI(),
+        pid: process.pid,
+        node: process.version,
+        master: snyk.config.isMaster,
+        name: getNameDepGraph(scannedProject, depGraph, meta),
+        version: depGraph.rootPkg.version,
+        org: config.org ? decodeURIComponent(config.org) : undefined,
+        pluginName: pluginMeta.name,
+        pluginRuntime: pluginMeta.runtime,
+        projectName: getProjectName(scannedProject, meta),
+        monitorGraph: true,
+        versionBuildInfo: JSON.stringify(scannedProject.meta?.versionBuildInfo),
+        gradleProjectName: scannedProject.meta?.gradleProjectName,
+      },
+      policy: policy ? policy.toString() : undefined,
+      depGraphJSON: depGraph, // depGraph will be auto serialized to JSON on send
+      // we take the targetFile from the plugin,
+      // because we want to send it only for specific package-managers
+      target,
+      targetFile: getTargetFile(scannedProject, pluginMeta),
+      targetFileRelativePath,
+      contributors,
+      callGraph: callGraphPayload,
+    } as MonitorBody,
+    gzip: true,
+    method: 'PUT',
+    headers: {
+      authorization: getAuthHeader(),
+      'content-encoding': 'gzip',
+    },
+    url: `${config.API}/monitor/${packageManager}/graph`,
+    json: true,
   });
+
+  if (res.statusCode && res.statusCode >= 200 && res.statusCode <= 299) {
+    return body as MonitorResult;
+  } else {
+    const userMessage = body && body.userMessage;
+    if (!userMessage && res.statusCode === 504) {
+      throw new ConnectionTimeoutError();
+    } else {
+      throw new MonitorError(res.statusCode, userMessage);
+    }
+  }
 }
 
 async function monitorDepGraphFromDepTree(
@@ -493,7 +457,7 @@ async function monitorDepGraphFromDepTree(
 ): Promise<MonitorResult> {
   const packageManager = meta.packageManager;
 
-  let treeMissingDeps: string[];
+  let treeMissingDeps: string[] | undefined;
   let depTree = scannedProject.depTree;
 
   if (!depTree) {
@@ -547,81 +511,66 @@ async function monitorDepGraphFromDepTree(
     prunedGraph = await pruneGraph(depGraph, packageManager, meta.prune);
   }
 
-  return new Promise((resolve, reject) => {
-    if (!depTree) {
-      debug(
-        'scannedProject is missing depGraph or depTree, cannot run test/monitor',
-      );
-      return reject(
-        new FailedToRunTestError(
-          'Your monitor request could not be completed. Please email support@snyk.io',
-        ),
-      );
-    }
-    makeRequest(
-      {
-        body: {
-          meta: {
-            method: meta.method,
-            hostname: os.hostname(),
-            id: snyk.id || depTree.name,
-            ci: isCI(),
-            pid: process.pid,
-            node: process.version,
-            master: snyk.config.isMaster,
-            name: getNameDepGraph(scannedProject, depGraph, meta),
-            version: depGraph.rootPkg.version,
-            org: config.org ? decodeURIComponent(config.org) : undefined,
-            pluginName: pluginMeta.name,
-            pluginRuntime: pluginMeta.runtime,
-            dockerImageId: pluginMeta.dockerImageId,
-            dockerBaseImage: depTree.docker
-              ? depTree.docker.baseImage
-              : undefined,
-            dockerfileLayers: depTree.docker
-              ? depTree.docker.dockerfileLayers
-              : undefined,
-            projectName: getProjectName(scannedProject, meta),
-            prePruneDepCount, // undefined unless 'prune' is used
-            missingDeps: treeMissingDeps,
-            monitorGraph: true,
-          },
-          policy: policy ? policy.toString() : undefined,
-          depGraphJSON: prunedGraph, // depGraph will be auto serialized to JSON on send
-          // we take the targetFile from the plugin,
-          // because we want to send it only for specific package-managers
-          target,
-          targetFile: getTargetFile(scannedProject, pluginMeta),
-          targetFileRelativePath,
-          contributors,
-        } as MonitorBody,
-        gzip: true,
-        method: 'PUT',
-        headers: {
-          authorization: getAuthHeader(),
-          'content-encoding': 'gzip',
-        },
-        url: `${config.API}/monitor/${packageManager}/graph`,
-        json: true,
-      },
-      (error, res, body) => {
-        if (error) {
-          return reject(error);
-        }
-
-        if (res.statusCode >= 200 && res.statusCode <= 299) {
-          resolve(body as MonitorResult);
-        } else {
-          let err;
-          const userMessage = body && body.userMessage;
-          if (!userMessage && res.statusCode === 504) {
-            err = new ConnectionTimeoutError();
-          } else {
-            err = new MonitorError(res.statusCode, userMessage);
-          }
-          reject(err);
-        }
-      },
+  if (!depTree) {
+    debug(
+      'scannedProject is missing depGraph or depTree, cannot run test/monitor',
     );
+    throw new FailedToRunTestError(
+      'Your monitor request could not be completed. Please email support@snyk.io',
+    );
+  }
+  const { res, body } = await makeRequest({
+    body: {
+      meta: {
+        method: meta.method,
+        hostname: os.hostname(),
+        id: snyk.id || depTree.name,
+        ci: isCI(),
+        pid: process.pid,
+        node: process.version,
+        master: snyk.config.isMaster,
+        name: getNameDepGraph(scannedProject, depGraph, meta),
+        version: depGraph.rootPkg.version,
+        org: config.org ? decodeURIComponent(config.org) : undefined,
+        pluginName: pluginMeta.name,
+        pluginRuntime: pluginMeta.runtime,
+        dockerImageId: pluginMeta.dockerImageId,
+        dockerBaseImage: depTree.docker ? depTree.docker.baseImage : undefined,
+        dockerfileLayers: depTree.docker
+          ? depTree.docker.dockerfileLayers
+          : undefined,
+        projectName: getProjectName(scannedProject, meta),
+        prePruneDepCount, // undefined unless 'prune' is used
+        missingDeps: treeMissingDeps,
+        monitorGraph: true,
+      },
+      policy: policy ? policy.toString() : undefined,
+      depGraphJSON: prunedGraph, // depGraph will be auto serialized to JSON on send
+      // we take the targetFile from the plugin,
+      // because we want to send it only for specific package-managers
+      target,
+      targetFile: getTargetFile(scannedProject, pluginMeta),
+      targetFileRelativePath,
+      contributors,
+    } as MonitorBody,
+    gzip: true,
+    method: 'PUT',
+    headers: {
+      authorization: getAuthHeader(),
+      'content-encoding': 'gzip',
+    },
+    url: `${config.API}/monitor/${packageManager}/graph`,
+    json: true,
   });
+
+  if (res.statusCode && res.statusCode >= 200 && res.statusCode <= 299) {
+    return body as MonitorResult;
+  } else {
+    const userMessage = body && body.userMessage;
+    if (!userMessage && res.statusCode === 504) {
+      throw new ConnectionTimeoutError();
+    } else {
+      throw new MonitorError(res.statusCode, userMessage);
+    }
+  }
 }
