a1750e3a5d402c436d7600f0780a02ba45133bc5
Jakub Mikulas <jakub@snyk.io>
2021-09-03 13:51:21 +0200

    refactor(esmodule): monitor

--- src/cli/commands/monitor/index.ts
+++ src/cli/commands/monitor/index.ts
@@ -1,5 +1,3 @@
-export = monitor;
-
 import chalk from 'chalk';
 import * as fs from 'fs';
 import * as Debug from 'debug';
@@ -61,7 +59,7 @@ async function promiseOrCleanup<T>(
 
 // Returns an array of Registry responses (one per every sub-project scanned), a single response,
 // or an error message.
-async function monitor(...args0: MethodArgs): Promise<any> {
+export default async function monitor(...args0: MethodArgs): Promise<any> {
   const { options, paths } = processCommandArgs(...args0);
   const results: Array<GoodResult | BadResult> = [];
 
