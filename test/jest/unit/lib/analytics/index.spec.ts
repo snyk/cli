e94979f107f541a8029365700d345ae454f9019d
Jahed Ahmed <jahed@snyk.io>
2021-11-08 15:18:10 +0000

    fix: ignore analytics request failures

--- test/jest/unit/lib/analytics/index.spec.ts
+++ test/jest/unit/lib/analytics/index.spec.ts
@@ -4,18 +4,36 @@ import { argsFrom } from './utils';
 import * as apiTokenModule from '../../../../../src/lib/api-token';
 
 describe('analytics module', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it('sends anaytics with no token set', async () => {
     analytics.add('k1', 'v1');
     const requestSpy = jest.spyOn(request, 'makeRequest');
+    requestSpy.mockResolvedValue();
+
     const someTokenExistsSpy = jest.spyOn(apiTokenModule, 'someTokenExists');
     someTokenExistsSpy.mockReturnValue(false);
-    requestSpy.mockImplementation(jest.fn());
+
     await analytics.addDataAndSend({
       args: argsFrom({}),
     });
+
     expect(requestSpy).toBeCalledTimes(1);
     expect(requestSpy.mock.calls[0][0]).not.toHaveProperty(
       'headers.authorization',
     );
+  });
+
+  it('ignores analytics request failures', async () => {
+    const requestSpy = jest.spyOn(request, 'makeRequest');
+    requestSpy.mockRejectedValue(new Error('this should be ignored'));
+
+    const result = analytics.addDataAndSend({
+      args: argsFrom({}),
+    });
+
+    await expect(result).resolves.toBeUndefined();
   });
 });
