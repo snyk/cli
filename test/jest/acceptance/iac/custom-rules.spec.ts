7c99334b822021ec55fb32805f5d61b2b26e5f18
Teodora Sandu <teodora.sandu@snyk.io>
2021-11-23 10:05:30 +0000

    chore: add gcr acceptance test

--- test/jest/acceptance/iac/custom-rules.spec.ts
+++ test/jest/acceptance/iac/custom-rules.spec.ts
@@ -139,6 +139,12 @@ describe('custom rules pull from a remote OCI registry', () => {
       process.env.OCI_GITHUB_REGISTRY_USERNAME,
       process.env.OCI_GITHUB_REGISTRY_PASSWORD,
     ],
+    [
+      'GCR',
+      process.env.OCI_GCR_REGISTRY_URL,
+      process.env.OCI_GCR_REGISTRY_USERNAME,
+      process.env.OCI_GCR_REGISTRY_PASSWORD,
+    ],
   ];
   test.each(cases)(
     'given %p as a registry and correct credentials, it returns a success exit code',
