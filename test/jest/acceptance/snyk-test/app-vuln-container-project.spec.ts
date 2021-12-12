9598904f87f9259d6972e8745e71f9ac164a54f2
Sharon Naftaly <sharon.naftaly@snyk.io>
2021-12-12 12:42:54 +0200

    feat: Allow json output for app vulns with --experimental

--- test/jest/acceptance/snyk-test/app-vuln-container-project.spec.ts
+++ test/jest/acceptance/snyk-test/app-vuln-container-project.spec.ts
@@ -7,6 +7,8 @@ import {
   SupportedProjectTypes,
   TestOptions,
 } from '../../../../src/lib/types';
+import { fakeServer } from '../../../acceptance/fake-server';
+import { runSnykCLI } from '../../util/runSnykCLI';
 
 describe('container test projects behavior with --app-vulns, --file and --exclude-base-image-vulns flags', () => {
   const fixturePath = path.normalize('test/fixtures/container-projects');
@@ -56,5 +58,65 @@ describe('container test projects behavior with --app-vulns, --file and --exclud
     );
     expect(result.vulnerabilities).toHaveLength(10);
     expect(result.ok).toEqual(false);
+  });
+});
+
+describe('container test projects behavior with --app-vulns, --json flags', () => {
+  let server;
+  let env: Record<string, string>;
+
+  beforeAll((done) => {
+    const port = process.env.PORT || process.env.SNYK_PORT || '12345';
+    const baseApi = '/api/v1';
+    env = {
+      ...process.env,
+      SNYK_API: 'http://localhost:' + port + baseApi,
+      SNYK_HOST: 'http://localhost:' + port,
+      SNYK_TOKEN: '123456789',
+      SNYK_INTEGRATION_NAME: 'JENKINS',
+      SNYK_INTEGRATION_VERSION: '1.2.3',
+    };
+    server = fakeServer(baseApi, env.SNYK_TOKEN);
+    server.listen(port, () => {
+      done();
+    });
+  });
+
+  afterEach(() => {
+    server.restore();
+  });
+
+  afterAll((done) => {
+    server.close(() => {
+      done();
+    });
+  });
+
+  it('returns a json with the --experimental flags', async () => {
+    const { code, stdout } = await runSnykCLI(
+      `container test snykgoof/os-app:node-snykin --app-vulns --json --experimental`,
+      {
+        env,
+      },
+    );
+
+    const jsonOutput = JSON.parse(stdout);
+    expect(Array.isArray(jsonOutput)).toBeTruthy();
+    expect(jsonOutput).toHaveLength(3);
+    expect(code).toEqual(0);
+  });
+
+  it('returns an error without the --experimental flags', async () => {
+    const { code, stdout } = await runSnykCLI(
+      `container test snykgoof/os-app:node-snykin --app-vulns --json`,
+      {
+        env,
+      },
+    );
+
+    expect(stdout).toContain(
+      'Application vulnerabilities is currently not supported with JSON output',
+    );
+    expect(code).toEqual(2);
   });
 });
