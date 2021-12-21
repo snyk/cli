e18adef5a0a72ad2874239f46650156e52091bd5
Teodora Sandu <teodora.sandu@snyk.io>
2021-12-21 14:44:06 +0000

    fix: custom rules output for sarif

--- src/lib/formatters/iac-output.ts
+++ src/lib/formatters/iac-output.ts
@@ -192,20 +192,43 @@ export function extractReportingDescriptor(
     if (tool[issue.id]) {
       return;
     }
+    // custom rules may not have some of these fields so we check them first
+    const fullDescriptionText = issue.subType
+      ? `${upperFirst(issue.severity)} severity - ${issue.subType}`
+      : `${upperFirst(issue.severity)} severity`;
+    const issueText = issue.iacDescription.issue
+      ? `The issue is... \n${issue.iacDescription.issue}\n\n`
+      : '';
+    const issueMarkdown = issue.iacDescription.issue
+      ? `**The issue is...** \n${issue.iacDescription.issue}\n\n`
+      : '';
+    const impactText = issue.iacDescription.impact
+      ? ` The impact of this is... \n ${issue.iacDescription.impact}\n\n`
+      : '';
+    const impactMarkdown = issue.iacDescription.impact
+      ? ` **The impact of this is...** \n ${issue.iacDescription.impact}\n\n`
+      : '';
+    const resolveText = issue.iacDescription.resolve
+      ? ` You can resolve this by... \n${issue.iacDescription.resolve}`
+      : '';
+    const resolveMarkdown = issue.iacDescription.resolve
+      ? ` **You can resolve this by...** \n${issue.iacDescription.resolve}`
+      : '';
+    const tags = ['security'];
+    if (issue.subType) {
+      tags.push(issue.subType);
+    }
     tool[issue.id] = {
       id: issue.id,
       shortDescription: {
         text: `${upperFirst(issue.severity)} severity - ${issue.title}`,
       },
       fullDescription: {
-        text: `${upperFirst(issue.severity)} severity - ${issue.subType}`,
+        text: fullDescriptionText,
       },
       help: {
-        text: `The issue is... \n${issue.iacDescription.issue}\n\n The impact of this is... \n ${issue.iacDescription.impact}\n\n You can resolve this by... \n${issue.iacDescription.resolve}`.replace(
-          /^\s+/g,
-          '',
-        ),
-        markdown: `**The issue is...** \n${issue.iacDescription.issue}\n\n **The impact of this is...** \n ${issue.iacDescription.impact}\n\n **You can resolve this by...** \n${issue.iacDescription.resolve}`.replace(
+        text: `${issueText}${impactText}${resolveText}`.replace(/^\s+/g, ''),
+        markdown: `${issueMarkdown}${impactMarkdown}${resolveMarkdown}`.replace(
           /^\s+/g,
           '',
         ),
@@ -214,7 +237,7 @@ export function extractReportingDescriptor(
         level: getIssueLevel(issue.severity),
       },
       properties: {
-        tags: ['security', `${issue.subType}`],
+        tags,
         documentation: issue.documentation,
       },
     };
@@ -228,10 +251,14 @@ export function mapIacTestResponseToSarifResults(
 ): sarif.Result[] {
   return issues.map(({ targetPath, issue }) => {
     const hasLineNumber = issue.lineNumber && issue.lineNumber >= 0;
+    // custom rules may not have some of these fields so we check them first
+    const affectingText = issue.subType
+      ? ` affecting the ${issue.subType}`
+      : '';
     return {
       ruleId: issue.id,
       message: {
-        text: `This line contains a potential ${issue.severity} severity misconfiguration affecting the ${issue.subType}`,
+        text: `This line contains a potential ${issue.severity} severity misconfiguration${affectingText}`,
       },
       locations: [
         {
