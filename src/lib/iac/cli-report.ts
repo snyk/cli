21d3efaca9b1ff614df563844681d69a95b78f00
Teodora Sandu <teodora.sandu@snyk.io>
2022-01-31 16:07:18 +0000

    add CD

--- src/lib/iac/cli-report.ts
+++ src/lib/iac/cli-report.ts
@@ -2,8 +2,15 @@ import config from '../config';
 import { makeRequest } from '../request';
 import { getAuthHeader } from '../api-token';
 import { FormattedResult } from '../../cli/commands/test/iac-local-execution/types';
+import {Contributor} from "../types";
+import * as analytics from "../analytics";
+import {getContributors} from "../monitor/dev-count-analysis";
 
-export function sendReport(issues: FormattedResult[]) {
+export async function sendReport(issues: FormattedResult[]) {
+  // TODO: check if allowed
+  const contributors = await getContributors();
+  console.log(contributors)
+
   makeRequest({
     method: 'POST',
     url: `${config.API}/iac-cli-report`,
@@ -13,6 +20,7 @@ export function sendReport(issues: FormattedResult[]) {
     },
     body: {
       issues,
+      contributors,
     },
   });
 }
