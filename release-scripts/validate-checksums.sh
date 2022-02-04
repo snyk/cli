32399f0df4ee6c72f5a559607d494e5ae55b6589
Jahed Ahmed <jahed@snyk.io>
2022-02-04 14:13:17 +0000

    chore(ci): move all artifacts into a single directory
    So that they're easier to keep track of.

--- release-scripts/validate-checksums.sh
+++ release-scripts/validate-checksums.sh
@@ -9,9 +9,6 @@ shasum -a 256 -c snyk-win.exe.sha256
 shasum -a 256 -c snyk-for-docker-desktop-darwin-x64.tar.gz.sha256
 shasum -a 256 -c snyk-for-docker-desktop-darwin-arm64.tar.gz.sha256
 shasum -a 256 -c docker-mac-signed-bundle.tar.gz.sha256
-popd
-
-pushd dist-pack
 shasum -a 256 -c snyk-fix.tgz.sha256
 shasum -a 256 -c snyk-protect.tgz.sha256
 shasum -a 256 -c snyk.tgz.sha256
