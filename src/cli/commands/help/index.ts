21cf5ceb536e091ecbde98e5b57f5b22eb2ad973
Avishagp <avishag.israeli@snyk.io>
2022-02-22 12:19:15 +0200

    docs: allow documenting sub-commands in --help

--- src/cli/commands/help/index.ts
+++ src/cli/commands/help/index.ts
@@ -1,36 +1,27 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { MethodArgs } from '../../args';
 import { renderMarkdown } from './markdown-renderer';
 
-const DEFAULT_HELP = 'README';
-
-function readHelpFile(filename: string): string {
-  const file = fs.readFileSync(filename, 'utf8');
-  return renderMarkdown(file);
+export function findHelpFile(
+  helpArgs: string[],
+  helpFolderPath = '../../help/cli-commands', // this is a relative path from the webpack dist directory,
+): string {
+  while (helpArgs.length > 0) {
+    // cleanse the filename to only contain letters
+    // aka: /\W/g but figured this was easier to read
+    const file = `${helpArgs.join('-').replace(/[^a-z0-9-]/gi, '')}.md`;
+    const testHelpAbsolutePath = path.resolve(__dirname, helpFolderPath, file);
+    if (fs.existsSync(testHelpAbsolutePath)) {
+      return testHelpAbsolutePath;
+    }
+    helpArgs = helpArgs.slice(0, -1);
+  }
+  return path.resolve(__dirname, helpFolderPath, `README.md`); // Default help file
 }
 
-export default async function help(item?: string | boolean): Promise<string> {
-  if (!item || item === true || typeof item !== 'string' || item === 'help') {
-    item = DEFAULT_HELP;
-  }
-
-  // cleanse the filename to only contain letters
-  // aka: /\W/g but figured this was easier to read
-  item = item.replace(/[^a-z0-9-]/gi, '');
-
-  try {
-    const filename = path.resolve(
-      __dirname,
-      '../../help/cli-commands', // this is a relative path from the webpack dist directory
-      item === DEFAULT_HELP ? `${DEFAULT_HELP}.md` : `${item}.md`,
-    );
-    return readHelpFile(filename);
-  } catch (error) {
-    const filename = path.resolve(
-      __dirname,
-      '../../help/cli-commands',
-      `${DEFAULT_HELP}.md`,
-    );
-    return readHelpFile(filename);
-  }
+export default async function help(...args: MethodArgs): Promise<string> {
+  const helpArgs = args.filter((arg): arg is string => typeof arg === 'string');
+  const helpFileAbsolutePath = findHelpFile(helpArgs);
+  return renderMarkdown(fs.readFileSync(helpFileAbsolutePath, 'utf8'));
 }
---
+++ test/jest/unit/cli/commands/help/help.spec.ts
@@ -0,0 +1,33 @@
+import * as help from '../../../../../../src/cli/commands/help';
+
+describe('findHelpFile', () => {
+  it('returns default help README path with no arguments', () => {
+    expect(help.findHelpFile([], '../../../../help/cli-commands')).toContain(
+      'README.md',
+    );
+  });
+
+  it('returns default help README path with non-existing command', () => {
+    expect(
+      help.findHelpFile(['rainmaker'], '../../../../help/cli-commands'),
+    ).toContain('README.md');
+  });
+
+  it('returns correct help markdown path with a `test` command', () => {
+    expect(
+      help.findHelpFile(['test'], '../../../../help/cli-commands'),
+    ).toContain('test.md');
+  });
+
+  it('returns correct help markdown path with a `container` command and a non-documented subcommand', () => {
+    expect(
+      help.findHelpFile(['container', 'test'], '../../../../help/cli-commands'),
+    ).toContain('container.md');
+  });
+
+  it('returns correct help markdown path for a documented subcommand with `iac drift`', () => {
+    expect(
+      help.findHelpFile(['iac', 'drift'], '../../../../help/cli-commands'),
+    ).toContain('iac-drift.md');
+  });
+});
