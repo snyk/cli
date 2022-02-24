cloudgeek7/snyk

feat: IaC CLI Share Results

src/cli/commands/test/iac-local-execution/share-results.ts
@@ -1,0 +1,22 @@
+import { isFeatureFlagSupportedForOrg } from '../../../../lib/feature-flags';
+import { shareResults } from '../../../../lib/iac/cli-share-results';
+import { FlagError } from './assert-iac-options-flag';
+import { formatShareResults } from './share-results-formatter';
+
+export async function formatAndShareResults(
+  results,
+  options,
+  orgPublicId,
+): Promise<Record<string, string>> {
+  const isCliReportEnabled = await isFeatureFlagSupportedForOrg(
+    'iacCliShareResults',
+    orgPublicId,
+  );
+  if (!isCliReportEnabled.ok) {
+    throw new FlagError('report', 'iacCliShareResults');
+  }
+
+  const formattedResults = formatShareResults(results, options);
+
+  return await shareResults(formattedResults);
+}

src/lib/iac/envelope-formatters.ts
@@ -1,0 +1,25 @@
+import {
+  IacShareResultsFormat,
+  PolicyMetadata,
+} from '../../cli/commands/test/iac-local-execution/types';
+import { ScanResult } from '../ecosystems/types';
+
+export function convertIacResultToScanResult(
+  iacResult: IacShareResultsFormat,
+): ScanResult {
+  return {
+    identity: {
+      type: iacResult.projectType,
+      targetFile: iacResult.targetFile,
+    },
+    facts: [],
+    findings: iacResult.violatedPolicies.map((policy: PolicyMetadata) => {
+      return {
+        data: { metadata: policy, docId: policy.docId },
+        type: 'iacIssues',
+      };
+    }),
+    name: iacResult.projectName,
+    target: {},
+  };
+}

test/jest/unit/iac/cli-share-results.fixtures.ts
@@ -1,0 +1,163 @@
+import {
+  IacShareResultsFormat,
+  PolicyMetadata,
+} from '../../../../src/cli/commands/test/iac-local-execution/types';
+import { IacProjectType } from '../../../../src/lib/iac/constants';
+import { SEVERITY } from '../../../../src/lib/snyk-test/common';
+
+const policyStub: PolicyMetadata = {
+  id: '1',
+  description: '',
+  impact:
+    'Compromised container could potentially modify the underlying host’s kernel by loading unauthorized modules (i.e. drivers).',
+  issue: 'Container is running in privileged mode',
+  msg: 'input.spec.containers[whatever].securityContext.privileged',
+  publicId: 'SNYK-CC-K8S-1',
+  references: [
+    'CIS Kubernetes Benchmark 1.6.0 - 5.2.1 Minimize the admission of privileged containers',
+    'https://kubernetes.io/docs/concepts/policy/pod-security-policy/#privileged',
+    'https://kubernetes.io/blog/2016/08/security-best-practices-kubernetes-deployment/',
+  ],
+  resolve:
+    'Remove `securityContext.privileged` attribute, or set value to `false`',
+  severity: 'medium' as SEVERITY,
+  subType: 'Deployment',
+  title: 'Container is running in privileged mode',
+  type: 'k8s',
+  docId: 0,
+};
+
+const anotherPolicyStub: PolicyMetadata = {
+  ...policyStub,
+  severity: 'high' as SEVERITY,
+  id: '2',
+  publicId: 'SNYK-CC-K8S-2',
+  docId: 1,
+};
+
+export function generateScanResults(): IacShareResultsFormat[] {
+  return [
+    {
+      projectName: 'projectA',
+      targetFile: 'file.yaml',
+      filePath: '/some/path/to/file.yaml',
+      fileType: 'yaml',
+      projectType: IacProjectType.K8S,
+      violatedPolicies: [{ ...policyStub }, { ...anotherPolicyStub }],
+    },
+    {
+      projectName: 'projectB',
+      targetFile: 'file.yaml',
+      filePath: '/some/path/to/file.yaml',
+      fileType: 'yaml',
+      projectType: IacProjectType.K8S,
+      violatedPolicies: [{ ...policyStub }],
+    },
+  ];
+}
+
+export const expectedEnvelopeFormatterResults = [
+  {
+    identity: {
+      type: 'k8sconfig',
+      targetFile: 'file.yaml',
+    },
+    facts: [],
+    findings: [
+      {
+        data: {
+          metadata: {
+            id: '1',
+            description: '',
+            impact:
+              'Compromised container could potentially modify the underlying host’s kernel by loading unauthorized modules (i.e. drivers).',
+            issue: 'Container is running in privileged mode',
+            msg: 'input.spec.containers[whatever].securityContext.privileged',
+            publicId: 'SNYK-CC-K8S-1',
+            references: [
+              'CIS Kubernetes Benchmark 1.6.0 - 5.2.1 Minimize the admission of privileged containers',
+              'https://kubernetes.io/docs/concepts/policy/pod-security-policy/#privileged',
+              'https://kubernetes.io/blog/2016/08/security-best-practices-kubernetes-deployment/',
+            ],
+            resolve:
+              'Remove `securityContext.privileged` attribute, or set value to `false`',
+            severity: 'medium',
+            subType: 'Deployment',
+            title: 'Container is running in privileged mode',
+            type: 'k8s',
+            docId: 0,
+          },
+          docId: 0,
+        },
+        type: 'iacIssues',
+      },
+      {
+        data: {
+          metadata: {
+            id: '2',
+            description: '',
+            impact:
+              'Compromised container could potentially modify the underlying host’s kernel by loading unauthorized modules (i.e. drivers).',
+            issue: 'Container is running in privileged mode',
+            msg: 'input.spec.containers[whatever].securityContext.privileged',
+            publicId: 'SNYK-CC-K8S-2',
+            references: [
+              'CIS Kubernetes Benchmark 1.6.0 - 5.2.1 Minimize the admission of privileged containers',
+              'https://kubernetes.io/docs/concepts/policy/pod-security-policy/#privileged',
+              'https://kubernetes.io/blog/2016/08/security-best-practices-kubernetes-deployment/',
+            ],
+            resolve:
+              'Remove `securityContext.privileged` attribute, or set value to `false`',
+            severity: 'high',
+            subType: 'Deployment',
+            title: 'Container is running in privileged mode',
+            type: 'k8s',
+            docId: 1,
+          },
+          docId: 1,
+        },
+        type: 'iacIssues',
+      },
+    ],
+    name: 'projectA',
+    target: {},
+  },
+  {
+    identity: {
+      type: 'k8sconfig',
+      targetFile: 'file.yaml',
+    },
+    facts: [],
+    findings: [
+      {
+        data: {
+          metadata: {
+            id: '1',
+            description: '',
+            impact:
+              'Compromised container could potentially modify the underlying host’s kernel by loading unauthorized modules (i.e. drivers).',
+            issue: 'Container is running in privileged mode',
+            msg: 'input.spec.containers[whatever].securityContext.privileged',
+            publicId: 'SNYK-CC-K8S-1',
+            references: [
+              'CIS Kubernetes Benchmark 1.6.0 - 5.2.1 Minimize the admission of privileged containers',
+              'https://kubernetes.io/docs/concepts/policy/pod-security-policy/#privileged',
+              'https://kubernetes.io/blog/2016/08/security-best-practices-kubernetes-deployment/',
+            ],
+            resolve:
+              'Remove `securityContext.privileged` attribute, or set value to `false`',
+            severity: 'medium',
+            subType: 'Deployment',
+            title: 'Container is running in privileged mode',
+            type: 'k8s',
+            docId: 0,
+          },
+          docId: 0,
+        },
+        type: 'iacIssues',
+      },
+    ],
+    name: 'projectB',
+    target: {},
+  },
+];

snyk/cli

src/cli/commands/test/iac-local-execution/share-results.ts
@@ -1,0 +1,22 @@
+import { isFeatureFlagSupportedForOrg } from '../../../../lib/feature-flags';
+import { shareResults } from '../../../../lib/iac/cli-share-results';
+import { FeatureFlagError } from './assert-iac-options-flag';
+import { formatShareResults } from './share-results-formatter';
+
+export async function formatAndShareResults(
+  results,
+  options,
+  orgPublicId,
+): Promise<Record<string, string>> {
+  const isCliReportEnabled = await isFeatureFlagSupportedForOrg(
+    'iacCliShareResults',
+    orgPublicId,
+  );
+  if (!isCliReportEnabled.ok) {
+    throw new FeatureFlagError('report', 'iacCliShareResults');
+  }
+
+  const formattedResults = formatShareResults(results, options);
+
+  return await shareResults(formattedResults);
+}

src/lib/iac/envelope-formatters.ts
@@ -1,0 +1,25 @@
+import {
+  IacShareResultsFormat,
+  PolicyMetadata,
+} from '../../cli/commands/test/iac-local-execution/types';
+import { ScanResult } from '../ecosystems/types';
+
+export function convertIacResultToScanResult(
+  iacResult: IacShareResultsFormat,
+): ScanResult {
+  return {
+    identity: {
+      type: iacResult.projectType,
+      targetFile: iacResult.targetFile,
+    },
+    facts: [],
+    findings: iacResult.violatedPolicies.map((policy: PolicyMetadata) => {
+      return {
+        data: { metadata: policy, docId: policy.docId },
+        type: 'iacIssue',
+      };
+    }),
+    name: iacResult.projectName,
+    target: { name: iacResult.projectName },
+  };
+}

test/jest/unit/iac/cli-share-results.fixtures.ts
@@ -1,0 +1,163 @@
+import {
+  IacShareResultsFormat,
+  PolicyMetadata,
+} from '../../../../src/cli/commands/test/iac-local-execution/types';
+import { IacProjectType } from '../../../../src/lib/iac/constants';
+import { SEVERITY } from '../../../../src/lib/snyk-test/common';
+
+const policyStub: PolicyMetadata = {
+  id: '1',
+  description: '',
+  impact:
+    'Compromised container could potentially modify the underlying host’s kernel by loading unauthorized modules (i.e. drivers).',
+  issue: 'Container is running in privileged mode',
+  msg: 'input.spec.containers[whatever].securityContext.privileged',
+  publicId: 'SNYK-CC-K8S-1',
+  references: [
+    'CIS Kubernetes Benchmark 1.6.0 - 5.2.1 Minimize the admission of privileged containers',
+    'https://kubernetes.io/docs/concepts/policy/pod-security-policy/#privileged',
+    'https://kubernetes.io/blog/2016/08/security-best-practices-kubernetes-deployment/',
+  ],
+  resolve:
+    'Remove `securityContext.privileged` attribute, or set value to `false`',
+  severity: 'medium' as SEVERITY,
+  subType: 'Deployment',
+  title: 'Container is running in privileged mode',
+  type: 'k8s',
+  docId: 0,
+};
+
+const anotherPolicyStub: PolicyMetadata = {
+  ...policyStub,
+  severity: 'high' as SEVERITY,
+  id: '2',
+  publicId: 'SNYK-CC-K8S-2',
+  docId: 1,
+};
+
+export function generateScanResults(): IacShareResultsFormat[] {
+  return [
+    {
+      projectName: 'projectA',
+      targetFile: 'file.yaml',
+      filePath: '/some/path/to/file.yaml',
+      fileType: 'yaml',
+      projectType: IacProjectType.K8S,
+      violatedPolicies: [{ ...policyStub }, { ...anotherPolicyStub }],
+    },
+    {
+      projectName: 'projectB',
+      targetFile: 'file.yaml',
+      filePath: '/some/path/to/file.yaml',
+      fileType: 'yaml',
+      projectType: IacProjectType.K8S,
+      violatedPolicies: [{ ...policyStub }],
+    },
+  ];
+}
+
+export const expectedEnvelopeFormatterResults = [
+  {
+    identity: {
+      type: 'k8sconfig',
+      targetFile: 'file.yaml',
+    },
+    facts: [],
+    findings: [
+      {
+        data: {
+          metadata: {
+            id: '1',
+            description: '',
+            impact:
+              'Compromised container could potentially modify the underlying host’s kernel by loading unauthorized modules (i.e. drivers).',
+            issue: 'Container is running in privileged mode',
+            msg: 'input.spec.containers[whatever].securityContext.privileged',
+            publicId: 'SNYK-CC-K8S-1',
+            references: [
+              'CIS Kubernetes Benchmark 1.6.0 - 5.2.1 Minimize the admission of privileged containers',
+              'https://kubernetes.io/docs/concepts/policy/pod-security-policy/#privileged',
+              'https://kubernetes.io/blog/2016/08/security-best-practices-kubernetes-deployment/',
+            ],
+            resolve:
+              'Remove `securityContext.privileged` attribute, or set value to `false`',
+            severity: 'medium',
+            subType: 'Deployment',
+            title: 'Container is running in privileged mode',
+            type: 'k8s',
+            docId: 0,
+          },
+          docId: 0,
+        },
+        type: 'iacIssue',
+      },
+      {
+        data: {
+          metadata: {
+            id: '2',
+            description: '',
+            impact:
+              'Compromised container could potentially modify the underlying host’s kernel by loading unauthorized modules (i.e. drivers).',
+            issue: 'Container is running in privileged mode',
+            msg: 'input.spec.containers[whatever].securityContext.privileged',
+            publicId: 'SNYK-CC-K8S-2',
+            references: [
+              'CIS Kubernetes Benchmark 1.6.0 - 5.2.1 Minimize the admission of privileged containers',
+              'https://kubernetes.io/docs/concepts/policy/pod-security-policy/#privileged',
+              'https://kubernetes.io/blog/2016/08/security-best-practices-kubernetes-deployment/',
+            ],
+            resolve:
+              'Remove `securityContext.privileged` attribute, or set value to `false`',
+            severity: 'high',
+            subType: 'Deployment',
+            title: 'Container is running in privileged mode',
+            type: 'k8s',
+            docId: 1,
+          },
+          docId: 1,
+        },
+        type: 'iacIssue',
+      },
+    ],
+    name: 'projectA',
+    target: { name: 'projectA' },
+  },
+  {
+    identity: {
+      type: 'k8sconfig',
+      targetFile: 'file.yaml',
+    },
+    facts: [],
+    findings: [
+      {
+        data: {
+          metadata: {
+            id: '1',
+            description: '',
+            impact:
+              'Compromised container could potentially modify the underlying host’s kernel by loading unauthorized modules (i.e. drivers).',
+            issue: 'Container is running in privileged mode',
+            msg: 'input.spec.containers[whatever].securityContext.privileged',
+            publicId: 'SNYK-CC-K8S-1',
+            references: [
+              'CIS Kubernetes Benchmark 1.6.0 - 5.2.1 Minimize the admission of privileged containers',
+              'https://kubernetes.io/docs/concepts/policy/pod-security-policy/#privileged',
+              'https://kubernetes.io/blog/2016/08/security-best-practices-kubernetes-deployment/',
+            ],
+            resolve:
+              'Remove `securityContext.privileged` attribute, or set value to `false`',
+            severity: 'medium',
+            subType: 'Deployment',
+            title: 'Container is running in privileged mode',
+            type: 'k8s',
+            docId: 0,
+          },
+          docId: 0,
+        },
+        type: 'iacIssue',
+      },
+    ],
+    name: 'projectB',
+    target: { name: 'projectB' },
+  },
+];