da7efe5d0358162b39aecf6b2cd3fdc732f2b109
Avishagp <avishag.israeli@snyk.io>
2022-03-22 11:35:40 +0200

    chore: support sub folders in help command

--- src/cli/commands/help/index.ts
+++ src/cli/commands/help/index.ts
@@ -7,14 +7,32 @@ export function findHelpFile(
   helpArgs: string[],
   helpFolderPath = '../../help/cli-commands', // this is a relative path from the webpack dist directory,
 ): string {
+  let helpArgsSubFolders = helpArgs.slice();
+  let subFolder = '';
   while (helpArgs.length > 0) {
     // cleanse the filename to only contain letters
     // aka: /\W/g but figured this was easier to read
     const file = `${helpArgs.join('-').replace(/[^a-z0-9-]/gi, '')}.md`;
     const testHelpAbsolutePath = path.resolve(__dirname, helpFolderPath, file);
+
+    const fileInSubFolders = `${helpArgsSubFolders
+      .join('-')
+      .replace(/[^a-z0-9-]/gi, '')}.md`;
+    const testHelpAbsolutePathInSubFolders = path.resolve(
+      __dirname,
+      helpFolderPath,
+      subFolder,
+      fileInSubFolders,
+    );
+
     if (fs.existsSync(testHelpAbsolutePath)) {
       return testHelpAbsolutePath;
+    } else if (fs.existsSync(testHelpAbsolutePathInSubFolders)) {
+      return testHelpAbsolutePathInSubFolders;
     }
+
+    subFolder = subFolder + helpArgsSubFolders[0] + '/';
+    helpArgsSubFolders = helpArgsSubFolders.slice(1);
     helpArgs = helpArgs.slice(0, -1);
   }
   return path.resolve(__dirname, helpFolderPath, `README.md`); // Default help file
