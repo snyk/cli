d18e295fd0593a22b7e4212654b354986db0b16f
Jahed Ahmed <jahed@snyk.io>
2022-04-07 10:52:14 +0000

    test: use async framework for interactive tests
    Previously interactive tests where using timeouts to predict when to
    send input. This is brittle and caused flakey tests as the CLI may not
    be ready to take input.
    
    Instead, we should wait for specific output (a question) before
    sending input (an answer). This makes tests less naive, more flexible
    to minor delays, and more expressive by allowing some narration around
    the scenario being tested.

---
+++ test/jest/util/constants.ts
@@ -0,0 +1,3 @@
+import * as path from 'path';
+
+export const CLI_BIN_PATH = path.resolve(__dirname, '../../../bin/snyk');
--- test/jest/util/runCommand.ts
+++ test/jest/util/runCommand.ts
@@ -7,9 +7,7 @@ type RunCommandResult = {
   stderr: string;
 };
 
-interface RunCommandOptions extends SpawnOptionsWithoutStdio {
-  delay?: number;
-}
+type RunCommandOptions = SpawnOptionsWithoutStdio;
 
 const runCommand = (
   command: string,
@@ -43,62 +41,4 @@ const runCommand = (
   });
 };
 
-function runCommandsWithUserInputs(
-  command: string,
-  args: string[] = [],
-  inputs: string[] = [],
-  options?: RunCommandOptions,
-): Promise<any> {
-  const timeout = options?.delay ?? 100;
-  const childProcess = spawn(command, args, options);
-
-  // Creates a loop to feed user inputs to the child process
-  // in order to get results from the tool
-  // This code is heavily inspired (if not blantantly copied)
-  // from inquirer-test package
-  const loop = (inputs) => {
-    if (!inputs.length) {
-      childProcess.stdin.end();
-      return;
-    }
-
-    setTimeout(() => {
-      childProcess.stdin.write(inputs[0]);
-      loop(inputs.slice(1));
-    }, timeout);
-  };
-
-  return new Promise((resolve, reject) => {
-    const stdout: Buffer[] = [];
-    const stderr: Buffer[] = [];
-
-    childProcess.on('error', (error) => {
-      reject(error);
-    });
-
-    childProcess.stdout.on('data', (chunk) => {
-      stdout.push(Buffer.from(chunk));
-    });
-
-    childProcess.stderr.on('data', (chunk) => {
-      stderr.push(Buffer.from(chunk));
-    });
-
-    childProcess.on('close', (code) => {
-      resolve({
-        code: code || 0,
-        stdout: Buffer.concat(stdout).toString('utf-8'),
-        stderr: Buffer.concat(stderr).toString('utf-8'),
-      });
-    });
-
-    loop(inputs);
-  });
-}
-
-export {
-  runCommand,
-  runCommandsWithUserInputs,
-  RunCommandResult,
-  RunCommandOptions,
-};
+export { runCommand, RunCommandResult, RunCommandOptions };
