3f00da91b38e0423b429e60d5d9978e874abf791
Francesco Mari <francesco.mari@snyk.io>
2022-04-12 14:05:05 +0200

    fix: Support scanning files with BOMs

--- test/jest/unit/iac/file-loader.spec.ts
+++ test/jest/unit/iac/file-loader.spec.ts
@@ -1,4 +1,4 @@
-const mockFs = require('mock-fs');
+import * as mockFs from 'mock-fs';
 import {
   FailedToLoadFileError,
   loadContentForFiles,
@@ -41,6 +41,8 @@ describe('loadContentForFiles', () => {
   });
 
   describe('errors', () => {
+    afterEach(jest.restoreAllMocks);
+
     it('throws an error when an error occurs when loading files', async () => {
       jest.spyOn(fileLoader, 'tryLoadFileData').mockImplementation(() => {
         throw FailedToLoadFileError;
@@ -50,5 +52,31 @@ describe('loadContentForFiles', () => {
 
       await expect(loadFilesFn).rejects.toThrow(FailedToLoadFileError);
     });
+  });
+});
+
+describe('tryLoadFileData', () => {
+  // In UTF-8, a BOM is useless because there is no need to signal a specific
+  // byte ordering. Some operating systems and editors, though, still prepend a
+  // BOM to files encoded in UTF-8. The BOM for UTF-8 is encoded by the bytes
+  // contained in the following buffer. When reading a file encoded as UTF-8
+  // with BOM, Node.js generates a string encoded in UTF-16 with BOM. The BOM
+  // for UTF-16 is encoded by the unicode code point U+FEFF. For more details,
+  // see https://en.wikipedia.org/wiki/Byte_order_mark.
+
+  const UTF8_BOM = Buffer.from([0xef, 0xbb, 0xbf]);
+
+  afterEach(mockFs.restore);
+
+  it('should load a file without BOM', async () => {
+    mockFs({ file: Buffer.from('data') });
+    const result = await fileLoader.tryLoadFileData('file');
+    expect(result.fileContent).toBe('data');
+  });
+
+  it('should load a file with BOM', async () => {
+    mockFs({ file: Buffer.concat([UTF8_BOM, Buffer.from('data')]) });
+    const result = await fileLoader.tryLoadFileData('file');
+    expect(result.fileContent).toBe('data');
   });
 });
