0a1493b9ac23412b2c18e920f90fa68e72aeb02b
Francesco Mari <francesco.mari@snyk.io>
2022-04-21 16:41:33 +0200

    refactor: remove inactive code related to ecosystems and container

--- src/cli/commands/test/iac/index.ts
+++ src/cli/commands/test/iac/index.ts
@@ -1,12 +1,10 @@
 import { EOL } from 'os';
-import { MissingArgError } from '../../../../lib/errors';
 import { MethodArgs } from '../../../args';
 import { TestCommandResult } from '../../../commands/types';
 import {
   initalUserMessageOutput,
   shouldPrintIacInitialMessage,
 } from '../../../../lib/formatters/iac-output';
-import { getEcosystemForTest, testEcosystem } from '../../../../lib/ecosystems';
 import { validateCredentials } from '../validate-credentials';
 import { validateTestOptions } from '../validate-test-options';
 import { setDefaultTestOptions } from '../set-default-test-options';
@@ -31,26 +29,6 @@ export default async function (
   const options = setDefaultTestOptions(originalOptions);
   validateTestOptions(options);
   validateCredentials(options);
-
-  // Handles no image arg provided to the container command until
-  // a validation interface is implemented in the docker plugin.
-  if (options.docker && paths.length === 0) {
-    throw new MissingArgError();
-  }
-
-  const ecosystem = getEcosystemForTest(options);
-  if (ecosystem) {
-    try {
-      const commandResult = await testEcosystem(ecosystem, paths, options);
-      return commandResult;
-    } catch (error) {
-      if (error instanceof Error) {
-        throw error;
-      } else {
-        throw new Error(String(error));
-      }
-    }
-  }
 
   const isNewIacOutputSupported = await hasFeatureFlag('iacCliOutput', options);
 
