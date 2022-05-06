947b6a5a67bf9d71b86cfcd85c245d7eb947f75e
Francesco Mari <francesco.mari@snyk.io>
2022-05-06 10:43:45 +0200

    refactor: move the origninal computePaths() close to its point of use

--- src/cli/commands/test/iac/local-execution/process-results/results-formatter.ts
+++ src/cli/commands/test/iac/local-execution/process-results/results-formatter.ts
@@ -17,7 +17,8 @@ import {
   getTrees,
   parsePath,
 } from '@snyk/cloud-config-parser';
-import { computePaths } from './utils';
+import * as path from 'path';
+import { isLocalFolder } from '../../../../../../lib/detect';
 
 const severitiesArray = SEVERITIES.map((s) => s.verboseName);
 
@@ -163,4 +164,36 @@ export class FailedToFormatResults extends CustomError {
     this.userMessage =
       'We failed printing the results, please contact support@snyk.io';
   }
+}
+
+function computePaths(
+  filePath: string,
+  pathArg = '.',
+): { targetFilePath: string; projectName: string; targetFile: string } {
+  const targetFilePath = path.resolve(filePath, '.');
+
+  // the absolute path is needed to compute the full project path
+  const cmdPath = path.resolve(pathArg);
+
+  let projectPath: string;
+  let targetFile: string;
+  if (!isLocalFolder(cmdPath)) {
+    // if the provided path points to a file, then the project starts at the parent folder of that file
+    // and the target file was provided as the path argument
+    projectPath = path.dirname(cmdPath);
+    targetFile = path.isAbsolute(pathArg)
+      ? path.relative(process.cwd(), pathArg)
+      : pathArg;
+  } else {
+    // otherwise, the project starts at the provided path
+    // and the target file must be the relative path from the project path to the path of the scanned file
+    projectPath = cmdPath;
+    targetFile = path.relative(projectPath, targetFilePath);
+  }
+
+  return {
+    targetFilePath,
+    projectName: path.basename(projectPath),
+    targetFile,
+  };
 }
