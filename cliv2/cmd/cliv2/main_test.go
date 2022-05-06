1032185701977e4f7dd960524ba7c66fcf6c37ce
Jahed Ahmed <jahed@snyk.io>
2022-05-06 11:47:35 +0000

    test(v2): upstreamProxy no longer defined
    Should have been removed in #3184 but tests weren't running.

--- cliv2/cmd/cliv2/main_test.go
+++ cliv2/cmd/cliv2/main_test.go
@@ -8,12 +8,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_getEnvVariables(t *testing.T) {
-	upstreamProxy := ""
+func Test_MainWithErrorCode(t *testing.T) {
 	cacheDirectory := ""
 
 	variables := main.EnvironmentVariables{
-		UpstreamProxy: upstreamProxy,
 		CacheDirectory: cacheDirectory,
 	}
 
@@ -21,30 +19,15 @@ func Test_getEnvVariables(t *testing.T) {
 	assert.Equal(t, err, 0)
 }
 
-func Test_getEnvVariables_no_proxy (t *testing.T) {
-	upstreamProxy := "MADE_UP_NAME"
-	cacheDirectory := ""
-
-	variables := main.EnvironmentVariables{
-		UpstreamProxy: upstreamProxy,
-		CacheDirectory: cacheDirectory,
-	}
-
-	err := main.MainWithErrorCode(variables, os.Args[1:])
-	assert.Equal(t, err, 2)
-}
-
-func Test_getEnvVariables_no_cache(t *testing.T) {
-	upstreamProxy := ""
+func Test_MainWithErrorCode_no_cache(t *testing.T) {
 	cacheDirectory := "MADE_UP_NAME"
 
 	variables := main.EnvironmentVariables{
-		UpstreamProxy: upstreamProxy,
 		CacheDirectory: cacheDirectory,
 	}
 
 	mainErr := main.MainWithErrorCode(variables, os.Args[1:])
-	
+
 	assert.Equal(t, mainErr, 0)
 	assert.DirExists(t, cacheDirectory)
 }
