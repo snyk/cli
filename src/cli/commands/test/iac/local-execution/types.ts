8c9c0747251f56f233ad1e2f819d7bafca16b82a
Francesco Mari <francesco.mari@snyk.io>
2022-05-30 10:02:36 +0200

    test: remove unused types

--- src/cli/commands/test/iac/local-execution/types.ts
+++ src/cli/commands/test/iac/local-execution/types.ts
@@ -356,34 +356,6 @@ export interface TestReturnValue {
   ignoreCount: number;
 }
 
-// https://github.com/opencontainers/image-spec/blob/main/manifest.md#image-manifest
-export interface ImageManifest {
-  schemaVersion: number;
-  mediaType: string;
-  config: ManifestConfig;
-  layers: ManifestConfig[];
-}
-
-export interface ManifestConfig {
-  mediaType: string;
-  size: number;
-  digest: string; // unique content identifier
-}
-
-export interface Layer {
-  config: ManifestConfig;
-  blob: Buffer;
-}
-
-export interface OCIPullOptions {
-  username?: string;
-  password?: string;
-  // weak typing on the client
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  reqOptions?: { accept?: string; indexContentType?: string };
-  imageSavePath?: string;
-}
-
 export interface OCIRegistryURLComponents {
   registryBase: string;
   repo: string;
