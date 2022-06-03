c58b5afc73d023b379a87f1262580da07af8f7f3
Elie <elie.charra@snyk.io>
2022-06-03 10:35:07 +0200

    feat: improve error message for iac describe
    - Display message in red
    - Hide drift alias from the list of options
    - Add more details for each required parameter usage

--- src/lib/errors/describe-required-argument-error.ts
+++ src/lib/errors/describe-required-argument-error.ts
@@ -1,11 +1,15 @@
 import { CustomError } from './custom-error';
-import { DescribeRequiredArgs } from '../iac/drift';
+import chalk from 'chalk';
 
 export class DescribeRequiredArgumentError extends CustomError {
   constructor() {
     super(
-      'Describe command require one of these arguments: ' +
-        DescribeRequiredArgs.join(', '),
+      chalk.red(
+        'Describe command require one of these arguments:\n' +
+          '    --only-unmanaged: Report resources not under Terraform control\n' +
+          '    --only-managed: Report resources from Terraform states that changed (may take a while)\n' +
+          '    --all: Scan for managed and unmanaged resources (may take a while)',
+      ),
     );
   }
 }
