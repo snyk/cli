De30/Snyk-CLI

chore: Send results to API

src/cli/commands/test/iac/v2/index.ts
@@ -59,5 +59,6 @@ async function prepareTestConfig(
     userRulesBundlePath: config.IAC_BUNDLE_PATH,
     userPolicyEnginePath: config.IAC_POLICY_ENGINE_PATH,
     severityThreshold: options.severityThreshold,
+    shouldShareResults: !!options.report,
   };
 }

src/lib/iac/test/v2/scan/index.ts
@@ -45,6 +45,10 @@ function scanWithConfig(
     args.push('-severity-threshold', options.severityThreshold);
   }
 
+  if (options.shouldShareResults) {
+    args.push('-report');
+  }
+
   args.push(...options.paths);
 
   const process = childProcess.spawnSync(policyEnginePath, args, {

src/lib/iac/test/v2/types.ts
@@ -8,5 +8,6 @@ export interface TestConfig {
   userPolicyEnginePath?: string;
   projectName: string;
   orgSettings: IacOrgSettings;
+  shouldShareResults: boolean;
   severityThreshold?: SEVERITY;
 }

snyk/cli

src/cli/commands/test/iac/v2/index.ts
@@ -63,6 +63,7 @@ async function prepareTestConfig(
     userPolicyEnginePath: config.IAC_POLICY_ENGINE_PATH,
     severityThreshold: options.severityThreshold,
     attributes,
+    report: !!options.report,
   };
 }
 

src/lib/iac/test/v2/scan/index.ts
@@ -98,6 +98,10 @@ function processFlags(
     flags.push('-project-lifecycle', options.attributes.lifecycle.join(','));
   }
 
+  if (options.report) {
+    flags.push('-report');
+  }
+
   return flags;
 }
 

src/lib/iac/test/v2/types.ts
@@ -9,6 +9,7 @@ export interface TestConfig {
   userPolicyEnginePath?: string;
   projectName: string;
   orgSettings: IacOrgSettings;
+  report: boolean;
   severityThreshold?: SEVERITY;
   attributes?: ProjectAttributes;
 }