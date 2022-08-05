b61d9cafe817f1e88666a76f7b8b4583c1060704
Jakub Mikulas <jakub@snyk.io>
2022-08-05 14:30:23 +0200

    chore(webpack): make license plugin the default
    And manually fill in slice-ansi license text
    
    Signed-off-by: Jakub Mikulas <jakub@snyk.io>

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,7 +1,6 @@
 import { merge } from 'webpack-merge';
 import common from './webpack.common';
 import { Configuration } from 'webpack';
-const LicensePlugin = require('webpack-license-plugin');
 
 export default merge(common as Configuration, {
   mode: 'production',
@@ -9,15 +8,4 @@ export default merge(common as Configuration, {
   optimization: {
     minimize: false,
   },
-  plugins: [
-    new LicensePlugin({
-      outputFilename: 'thirdPartyNotice.json',
-      licenseOverrides: {
-        '@arcanis/slice-ansi@1.0.2': 'MIT',
-      },
-      excludedPackageTest: (packageName) => {
-        return packageName.startsWith('@snyk/');
-      },
-    }),
-  ],
 });
