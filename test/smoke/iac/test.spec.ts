8890fb36c9831fe182f6d3c15eb7a8af172717c3
Ofek Atar <ofek.atar@snyk.io>
2022-09-13 18:56:01 +0300

    chore: Create IaC smoke tests for experimental test

---
+++ test/smoke/iac/test.spec.ts
@@ -0,0 +1,28 @@
+import { run } from '../../jest/acceptance/iac/helpers';
+
+jest.setTimeout(1_000 * 90);
+
+describe('snyk iac test --experimental', () => {
+  beforeAll(async () => {
+    await login();
+  });
+
+  it('runs successfully and resolves with a non-error exit code', async () => {
+    // Arrange
+    const filePath = 'iac/depth_detection/root.tf';
+
+    // Act
+    const { stderr, stdout, exitCode } = await run(
+      `snyk iac test --experimental ${filePath}`,
+    );
+
+    // Assert
+    expect(stdout).toContain('Infrastructure as Code');
+    expect(stderr).toBe('');
+    expect(exitCode).toBeLessThan(2);
+  });
+
+  async function login() {
+    await run(`snyk auth ${process.env.IAC_SMOKE_TESTS_SNYK_TOKEN}`);
+  }
+});
