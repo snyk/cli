c188dc90146ea584aac454a66ca537209bf55e3b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-22 21:21:59 -0400

    spelling: recursively
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/snyk-protect/src/lib/explore-node-modules.ts
+++ packages/snyk-protect/src/lib/explore-node-modules.ts
@@ -37,7 +37,7 @@ function checkPhysicalModule(
 }
 
 /**
- * Explore the node_modules of a project, starting with the given path and recusively exploring deeper,
+ * Explore the node_modules of a project, starting with the given path and recursively exploring deeper,
  * looking for modules which match the package names we are considering for patching.
  * @param pathToCheck the path to look for a matching module.
  * @param packageNamesOfInterest - a list of package names we are considering for patching.
