2586e0bc058420b967ad2abb6599904df75aa891
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-22 21:21:42 -0400

    spelling: containing
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/snyk-protect/src/lib/explore-node-modules.ts
+++ packages/snyk-protect/src/lib/explore-node-modules.ts
@@ -4,7 +4,7 @@ import { FoundPhysicalPackage } from './types';
 
 /**
  * Check if a physical module (given by folderPath) is one of the package names we are interested in for patching.
- * If it is, capture it as a FoundPhysicalPackage containg the name, version, and path
+ * If it is, capture it as a FoundPhysicalPackage containing the name, version, and path
  * @param folderPath the physical path of the module under consideration.
  * @param packageNamesOfInterest a list of package names we are considering for patching.
  * @returns a FoundPhysicalPackage if the module at the given path has a package name which we are considering for patching.
