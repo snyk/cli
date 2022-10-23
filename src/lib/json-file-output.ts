bcffad3854334045067a3cd3ede0a0837f1efd52
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-22 21:21:45 -0400

    spelling: directory
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/lib/json-file-output.ts
+++ src/lib/json-file-output.ts
@@ -6,7 +6,7 @@ export const MIN_VERSION_FOR_MKDIR_RECURSIVE = '10.12.0';
 
 /**
  * Attempts to create a directory and fails quietly if it cannot. Rather than throwing errors it logs them to stderr and returns false.
- * It will attempt to recursively nested direcotry (ex `mkdir -p` style) if it needs to but will fail to do so with Node < 10 LTS.
+ * It will attempt to recursively nested directory (ex `mkdir -p` style) if it needs to but will fail to do so with Node < 10 LTS.
  * @param newDirectoryFullPath the full path to a directory to create
  * @returns true if either the directory already exists or it is successful in creating one or false if it fails to create it.
  */
