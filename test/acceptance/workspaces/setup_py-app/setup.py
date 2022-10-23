c3a74945e3c1a71885b3223e135625d92284f335
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-22 21:22:04 -0400

    spelling: testtools
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test/acceptance/workspaces/setup_py-app/setup.py
+++ test/acceptance/workspaces/setup_py-app/setup.py
@@ -11,6 +11,6 @@ setup(
         "python-etcd==0.4.5",
         "Django-Select2==6.0.1",  # this version installs with lowercase so it catches a previous bug in pip_resolve.py
         "irc==16.2",  # this has a cyclic dependency (internal jaraco.text <==> jaraco.collections)
-        "testtools==2.3.0",  # this has a cycle (fixtures ==> testtols)
+        "testtools==2.3.0",  # this has a cycle (fixtures ==> testtools)
     ],
 )
