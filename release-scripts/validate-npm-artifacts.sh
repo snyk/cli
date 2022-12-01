3ef13a720648013117e224707f9cfe44fd1327e7
michelkaporin <michel@snyk.io>
2022-12-01 10:01:03 +0100

    fix: path

--- release-scripts/validate-npm-artifacts.sh
+++ release-scripts/validate-npm-artifacts.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo 'Running "npm install"'
-npm install binary-releases/snyk.tgz
+echo 'Running "npm install binary-releases/snyk.tgz"'
+npm install ./binary-releases/snyk.tgz
 
 echo 'Running "snyk"'
 ./node_modules/snyk/bin/snyk
