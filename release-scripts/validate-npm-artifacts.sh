823acc5c2befa4a8f4d87b1c87dfe8608bb92d9b
michelkaporin <michel@snyk.io>
2022-12-01 13:17:47 +0100

    fix: sandbox npm install

--- release-scripts/validate-npm-artifacts.sh
+++ release-scripts/validate-npm-artifacts.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo 'Running "npm install binary-releases/snyk.tgz"'
-npm install ./binary-releases/snyk.tgz
+releaseTar=$(pwd)/binary-releases/snyk.tgz
 
-echo 'Running "snyk"'
+echo 'Creating temp directory for sandbox validation...'
+pushd $(mktemp -d)
+
+echo 'Running "npm install binary-releases/snyk.tgz"...'
+npm install $releaseTar
+
+echo 'Verifying "snyk" command succeeds...'
 ./node_modules/snyk/bin/snyk
+
+popd
