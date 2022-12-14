a984389b47e24e0ce582f80a0ab6c3b4d7f440c1
Toni <toni.devine@snyk.io>
2022-12-14 16:41:33 +0000

    feat: add support for swift and install the swiftpm plugin

---
+++ test/fixtures/basic-swift/Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "swift-app-project",
+
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", exact: "1.0.0"),
+
+    ]
+)
