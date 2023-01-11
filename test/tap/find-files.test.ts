3ebd4a66210a46850616ac3ecbadd768de3caab4
Doron Eli Rachman <doron.elirachman@snyk.io>
2023-01-11 15:32:13 +0200

    fix: naming

--- test/tap/find-files.test.ts
+++ test/tap/find-files.test.ts
@@ -108,7 +108,7 @@ test('find package.json file in test fixture ignoring node_modules', async (t) =
   t.same(result.sort(), expected.sort(), 'should return expected file');
 });
 
-test('find package.json file in test fixture ignoring .build folder', async (t) => {
+test('find Package.swift file in test fixture ignoring .build folder', async (t) => {
   // six levels deep to ensure .build is tested
   const nodeModulesPath = path.join(testFixture, '.build');
   const { files: result } = await find(
