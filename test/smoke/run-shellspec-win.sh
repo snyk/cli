84446aac3993d652e9b7bec276c3a4405b030741
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2023-02-09 13:32:01 +0100

    feat: Release of the Extensible CLI via npm

--- test/smoke/run-shellspec-win.sh
+++ test/smoke/run-shellspec-win.sh
@@ -1,3 +1,3 @@
 echo "run-shellscript-win.sh"
 
-/c/Users/runneradmin/.local/bin/shellspec -f d --skip-message quiet
+/c/Users/runneradmin/.local/bin/shellspec -f d --skip-message quiet --no-warning-as-failure
