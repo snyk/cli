a7648b27a6393f8165754911d8f5bcf1ed8cc223
Jason Luong <jason.luong@snyk.io>
2023-02-16 16:29:07 +0000

    fix: ts-binary-wrapper tests

--- ts-binary-wrapper/test/acceptance/basic.spec.ts
+++ ts-binary-wrapper/test/acceptance/basic.spec.ts
@@ -1,4 +1,6 @@
-import { prepareEnvironment } from '../util/prepareEnvironment';
+import {
+  prepareEnvironment,
+} from '../util/prepareEnvironment';
 import * as bootstrap from '../../src/bootstrap';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -8,6 +10,7 @@ jest.setTimeout(60 * 1000);
 
 describe('Basic acceptance test', () => {
   it('Bootstrap binary & execute a command', async () => {
+    // setup
     const cliVersionForTesting = '1.1080.0';
     const envInfo = await prepareEnvironment(cliVersionForTesting);
     const executable = bootstrap.executable.replace(
@@ -49,6 +52,50 @@ describe('Basic acceptance test', () => {
     expect(resultIndex.output.toString()).toContain(cliVersionForTesting);
 
     fs.unlinkSync(executable);
+
+    try {
+      fs.rmSync(envInfo.outputfolder, { recursive: true });
+    } catch {
+      // to support nodejs 12, which doesn't know rmSync()
+      fs.rmdirSync(envInfo.outputfolder, { recursive: true });
+    }
+  });
+
+  it('Bootstrap binary fails when proxy is used but not allowed', async () => {
+    // only run when PROXY_TESTS is set
+    if (!process.env.PROXY_TESTS) {
+      console.debug('Skipping test because PROXY_TEST is not set');
+      return;
+    }
+
+    // setup
+    const cliVersionForTesting = '1.1080.0';
+    const envInfo = await prepareEnvironment(cliVersionForTesting);
+    const executable = bootstrap.executable.replace(
+      envInfo.inputfolder,
+      envInfo.outputfolder,
+    );
+
+    try {
+      fs.unlinkSync(executable);
+    } catch {
+      //
+    }
+
+    expect(fs.existsSync(executable)).toBeFalsy();
+
+    const bootstrapScript = path.join(envInfo.outputfolder, 'bootstrap.js');
+
+    // set NO_PROXY for snyk.io
+    process.env.NO_PROXY = '*.snyk.io';
+
+    // run system under test: index
+    const resultBootstrap = child_process.spawnSync(
+      'node ' + bootstrapScript + ' exec',
+      { shell: true, env: { ...process.env } },
+    );
+    console.debug(resultBootstrap.stdout.toString());
+    expect(resultBootstrap.status).toEqual(1);
 
     try {
       fs.rmSync(envInfo.outputfolder, { recursive: true });
