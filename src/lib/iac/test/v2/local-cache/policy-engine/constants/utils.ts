b195a62d6f35aed126d85583e989ec0b347526bc
Jason Lynch <jason.lynch@snyk.io>
2023-02-21 10:57:29 -0500

    feat: upgrade snyk-iac-test to 0.40.3

--- src/lib/iac/test/v2/local-cache/policy-engine/constants/utils.ts
+++ src/lib/iac/test/v2/local-cache/policy-engine/constants/utils.ts
@@ -1,12 +1,12 @@
 import * as os from 'os';
 
 const policyEngineChecksums = `
-0e505b9284ff31465a50656006a9fbbca943988e3dc1a2860ee0ab3e6b2573bc  snyk-iac-test_0.39.0_Windows_arm64.exe
-566d8587057312ecbe8298ad0acf712904689e332652c43c6bd0a499d677a44c  snyk-iac-test_0.39.0_Darwin_arm64
-7258a935fc08634a47f0c111400664cb9ff176f1cda67131891421f78bce115c  snyk-iac-test_0.39.0_Linux_arm64
-b172acbb0a84f40a9917f196e1f7b4d1aa21feddc1b2f1670ed6ef9dc71fb2e4  snyk-iac-test_0.39.0_Linux_x86_64
-c3f38885ba20087ecf20587dd83ebc4f9df27438cd9d9281700cd670877f3144  snyk-iac-test_0.39.0_Windows_x86_64.exe
-e1286f48b4bd297c05a52d256ddc4487d7c10a0e48298a177c43f3825ef83944  snyk-iac-test_0.39.0_Darwin_x86_64
+2cc88f4f928d47ec914d6ab40c773ce3efef18a879db1c702df0500e250666fa  snyk-iac-test_0.40.3_Darwin_arm64
+987a9bb850739c727dff6f47f0daa673489ca2bf0ae87ca253c7a4c66ce81285  snyk-iac-test_0.40.3_Darwin_x86_64
+ff9e2dfaca90f0efe270cffc843a25252dec266d8ba3307babf70af4d28509e6  snyk-iac-test_0.40.3_Linux_arm64
+0b516c5a25aa23950646b314d182236106ea984322837aa17e928dd9f24a0103  snyk-iac-test_0.40.3_Linux_x86_64
+505f9371d0a5cc874ae979127b8d5690bd4ddc6c38046dcb43d091ce67c4978e  snyk-iac-test_0.40.3_Windows_arm64.exe
+62dbb08941b6d520ba68a1ab1aa804b1797133dc4f91560a4762e9510d14292b  snyk-iac-test_0.40.3_Windows_x86_64.exe
 `;
 
 export const policyEngineVersion = getPolicyEngineVersion();
