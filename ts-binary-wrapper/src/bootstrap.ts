19f8721dacdaa57773815cd1cb59a808958668e9
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2023-03-03 14:45:29 +0100

    feat: Revert the Release of the Extensible CLI via npm
    This reverts commit 8002be9de6a3f426c8fb5ca237f8271905bcc916.

--- ts-binary-wrapper/src/bootstrap.ts
+++ ts-binary-wrapper/src/bootstrap.ts
@@ -1,27 +1,18 @@
 import * as common from './common';
 import * as process from 'process';
 
-const errorContextMessage = 'Download Error';
+const config = common.getCurrentConfiguration();
+export const executable = config.getLocalLocation();
 
-(async () => {
-  try {
-    const config = common.getCurrentConfiguration();
-    const executable = config.getLocalLocation();
+if (process.argv.includes('exec')) {
+  const filenameShasum = config.getShasumFile();
+  const downloadUrl = config.getDownloadLocation();
 
-    if (process.argv.includes('exec')) {
-      const filenameShasum = config.getShasumFile();
-      const downloadUrl = config.getDownloadLocation();
-
-      const downloadError = await common.downloadExecutable(
-        downloadUrl,
-        executable,
-        filenameShasum,
-      );
-      if (downloadError !== undefined) {
-        throw downloadError;
-      }
-    }
-  } catch (err) {
-    await common.logError(errorContextMessage, err);
-  }
-})();
+  common
+    .downloadExecutable(downloadUrl, executable, filenameShasum)
+    .then(process.exit)
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+}
