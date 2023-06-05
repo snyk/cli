780ee7f0b068b13771a56825ab9ff4962f65c7d7
Bastian Doetsch <bastian.doetsch@snyk.io>
2023-06-05 13:16:32 +0200

    fix: create directories if not already created in proxy

--- cliv2/internal/utils/directories.go
+++ cliv2/internal/utils/directories.go
@@ -12,7 +12,7 @@ const CACHEDIR_PERMISSION = 0755
 // The directory structure used to cache things into
 // - Base cache directory (user definable, default depends on OS, exmple:  /Users/username/Library/Caches/snyk/)
 // |- Version cache directory (example: /Users/username/Library/Caches/snyk/snyk-cli/1.1075.0/)
-//  |- Temp directory (example: /Users/username/Library/Caches/snyk/1.1075.0/tmp/)
+// |- Temp directory (example: /Users/username/Library/Caches/snyk/snyk-cli/1.1075.0/tmp/)
 
 func GetTemporaryDirectory(baseCacheDirectory string, versionNumber string) string {
 	return path.Join(GetVersionCacheDirectory(baseCacheDirectory, versionNumber), "tmp")
