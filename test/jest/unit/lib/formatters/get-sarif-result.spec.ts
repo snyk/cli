6b3e0468061be8ed6098253691f45990d4731b92
Robert Mutuleanu <robert.mutuleanu@snyk.io>
2023-06-19 16:28:43 +0300

    fix: container sarif replace colon within location uri
    When using sarif output and the Dockerfile is not specified, the path is used as the sarif results location uri. But the path can contain colon characters (e.g. between repo and tag) and GitHub Code Scanning seems to return an error in this case. This fix replaces the colon characters from the sarif results location uri with underscore characters.
    
    Issue: LUM-257

--- test/jest/unit/lib/formatters/get-sarif-result.spec.ts
+++ test/jest/unit/lib/formatters/get-sarif-result.spec.ts
@@ -2,12 +2,47 @@ import { getResults } from '../../../../../src/lib/formatters/get-sarif-result';
 import { SEVERITY, TestResult } from '../../../../../src/lib/snyk-test/legacy';
 
 describe('Retrieving sarif result', () => {
-  it('should use the test results path as the location uri when target file is not present', () => {
-    let result = getResults(
+  const cases: Array<[
+    string,
+    { path: string; displayTargetFile?: string },
+    { resultLocationUri: string },
+  ]> = [
+    [
+      'should return the path given there is no target file present',
+      { path: 'alpine' },
+      { resultLocationUri: 'alpine' },
+    ],
+    [
+      'should return the path without colon characters given there is no target file present and the path contains a tag',
+      { path: 'alpine:3.18.0' },
+      { resultLocationUri: 'alpine_3.18.0' },
+    ],
+    [
+      'should return the path without colon characters given there is no target file present and the path contains a digest',
+      {
+        path:
+          'alpine@sha256:c0669ef34cdc14332c0f1ab0c2c01acb91d96014b172f1a76f3a39e63d1f0bda',
+      },
+      {
+        resultLocationUri:
+          'alpine@sha256_c0669ef34cdc14332c0f1ab0c2c01acb91d96014b172f1a76f3a39e63d1f0bda',
+      },
+    ],
+    [
+      'should return the target file given there is a target file present',
+      { path: 'alpine', displayTargetFile: 'Dockerfile.test' },
+      { resultLocationUri: 'Dockerfile.test' },
+    ],
+  ];
+
+  it.each(cases)('%s', (_, input, want) => {
+    const result = getResults(
       getTestResult({
-        path: 'alpine:3.18.0',
+        displayTargetFile: input.displayTargetFile,
+        path: input.path,
       }),
     );
+
     expect(result).toEqual([
       {
         ruleId: 'SNYK-LINUX-EXPAT-450908',
@@ -19,83 +54,7 @@ describe('Retrieving sarif result', () => {
         locations: [
           {
             physicalLocation: {
-              artifactLocation: { uri: 'alpine:3.18.0' },
-              region: { startLine: 1 },
-            },
-          },
-        ],
-      },
-    ]);
-
-    result = getResults(
-      getTestResult({
-        path: 'alpine:3.18.0',
-        displayTargetFile: undefined,
-      }),
-    );
-    expect(result).toEqual([
-      {
-        ruleId: 'SNYK-LINUX-EXPAT-450908',
-        level: 'error',
-        message: {
-          text:
-            'This file introduces a vulnerable expat package with a critical severity vulnerability.',
-        },
-        locations: [
-          {
-            physicalLocation: {
-              artifactLocation: { uri: 'alpine:3.18.0' },
-              region: { startLine: 1 },
-            },
-          },
-        ],
-      },
-    ]);
-
-    result = getResults(
-      getTestResult({
-        path: 'alpine:3.18.0',
-        displayTargetFile: null,
-      }),
-    );
-    expect(result).toEqual([
-      {
-        ruleId: 'SNYK-LINUX-EXPAT-450908',
-        level: 'error',
-        message: {
-          text:
-            'This file introduces a vulnerable expat package with a critical severity vulnerability.',
-        },
-        locations: [
-          {
-            physicalLocation: {
-              artifactLocation: { uri: 'alpine:3.18.0' },
-              region: { startLine: 1 },
-            },
-          },
-        ],
-      },
-    ]);
-  });
-
-  it('should use the target file as the location uri when target file is present', () => {
-    const actualResult = getResults(
-      getTestResult({
-        displayTargetFile: 'Dockerfile.test',
-      }),
-    );
-    expect(actualResult).toEqual([
-      {
-        ruleId: 'SNYK-LINUX-EXPAT-450908',
-        level: 'error',
-        message: {
-          text:
-            'This file introduces a vulnerable expat package with a critical severity vulnerability.',
-        },
-        locations: [
-          {
-            physicalLocation: {
-              artifactLocation: { uri: 'Dockerfile.test' },
+              artifactLocation: { uri: want.resultLocationUri },
               region: { startLine: 1 },
             },
           },
