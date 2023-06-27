3756304052b6f6f02c657e1e8416954e41289e52
Yair Zohar <yair.zohar@snyk.io>
2023-06-27 14:49:44 +0300

    feat: new snyk-iac-test version

--- src/lib/iac/test/v2/local-cache/policy-engine/constants/utils.ts
+++ src/lib/iac/test/v2/local-cache/policy-engine/constants/utils.ts
@@ -2,11 +2,11 @@ import * as os from 'os';
 
 // TODO: update!
 const policyEngineChecksums = `
-2a4abd57e799bc083ca6dfbbee46bfba0dda3b0f998add03292cf3ded7fa7393  snyk-iac-test_0.47.1_Windows_x86_64.exe
-2b7bbaae7c7220ae8aac141aae4c7833b978398505ad33c5bfab8ecacfd66940  snyk-iac-test_0.47.1_Linux_x86_64
-8468636a35b6b40cbc1c5dfc39f31a811394b96833d98aef7aa3705ac817448f  snyk-iac-test_0.47.1_Darwin_x86_64
-95625d1664427d4947ec34be0d48fb4d7442ec481187d5ba32ea1f2ddc40753b  snyk-iac-test_0.47.1_Darwin_arm64
-a252789d8111b5f1dc280ef77f70aa6e1fbfd2fdc8149312ffac4948faf36b12  snyk-iac-test_0.47.1_Linux_arm64
+1577b258cbdd083b232f5a4d6e579f57678a42bf17cf5cec812e50aa9f400f35  snyk-iac-test_0.47.2_Darwin_arm64
+4e76a831c087e1e9dacf278a4b4c3b01f4ffeb49c15ce1192fd36c580ece30fc  snyk-iac-test_0.47.2_Linux_x86_64
+a420d4655be8fde67652461d1c311c912f1fa99a07989f752a1e9fd54303b5b9  snyk-iac-test_0.47.2_Darwin_x86_64
+b4e68106d88b7e28d089289b9ee73abe6025df0307e189397f17d88002029d9e  snyk-iac-test_0.47.2_Windows_x86_64.exe
+c1ca5fbf2fb09e99b95bef456e0f360fbe5a5068d9b5a6dcfd4efea16add6681  snyk-iac-test_0.47.2_Linux_arm64
 `;
 
 export const policyEngineVersion = getPolicyEngineVersion();
