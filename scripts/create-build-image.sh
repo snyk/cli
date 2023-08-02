97b6d0dd7f7cfb907ea7701c69d2de2508dee9eb
Bastian Doetsch <bastian.doetsch@snyk.io>
2023-08-02 08:48:06 +0200

    fix: pipeline

--- scripts/create-build-image.sh
+++ scripts/create-build-image.sh
@@ -12,13 +12,13 @@ pushd "$SCRIPT_DIR/.."
   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 
   BASE_IMG_NAME=$DOCKER_USERNAME/cli-build
-  docker buildx build --no-cache --build-arg NODEVERSION="$NODEVERSION" --build-arg ARCH="x86_64" --platform linux/amd64 -t "$BASE_IMG_NAME":latest -f .circleci/Dockerfile .
+  docker buildx build --build-arg NODEVERSION="$NODEVERSION" --build-arg ARCH="x86_64" --platform linux/amd64 -t "$BASE_IMG_NAME":latest -f .circleci/Dockerfile .
   docker tag "$BASE_IMG_NAME":latest "$BASE_IMG_NAME":"$NOW"
   docker push "$BASE_IMG_NAME":latest
   docker push "$BASE_IMG_NAME:$NOW"
 
   BASE_IMG_NAME=$DOCKER_USERNAME/cli-build-arm64
-  docker buildx build --no-cache --build-arg NODEVERSION="$NODEVERSION" --build-arg ARCH="aarch64" --platform linux/arm64 -t "$BASE_IMG_NAME":latest -f .circleci/Dockerfile .
+  docker buildx build --build-arg NODEVERSION="$NODEVERSION" --build-arg ARCH="aarch64" --platform linux/arm64 -t "$BASE_IMG_NAME":latest -f .circleci/Dockerfile .
   docker tag "$BASE_IMG_NAME":latest "$BASE_IMG_NAME":"$NOW"
   docker push "$BASE_IMG_NAME":latest
   docker push "$BASE_IMG_NAME:$NOW"
