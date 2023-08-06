d422252096ccfea68bbcb45ba992bdf2f9c4cd97
Kaspar Lyngsie <kaspar.moss@snyk.io>
2023-08-06 09:05:33 +0300

    fix: moved to build image Dockerfile

--- scripts/create-build-image.sh
+++ scripts/create-build-image.sh
@@ -12,14 +12,23 @@ pushd "$SCRIPT_DIR/.."
   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 
   BASE_IMG_NAME=$DOCKER_USERNAME/cli-build
-  docker buildx build --build-arg NODEVERSION="$NODEVERSION" --build-arg ARCH="x86_64" --platform linux/amd64 -t "$BASE_IMG_NAME":latest -f .circleci/Dockerfile .
-  docker tag "$BASE_IMG_NAME:latest" "$BASE_IMG_NAME:$NOW"
-  docker push "$BASE_IMG_NAME:latest"
-  docker push "$BASE_IMG_NAME:$NOW"
+  docker buildx build \
+    --build-arg NODEVERSION="$NODEVERSION" \
+    --build-arg ARCH="x86_64" \
+    --platform linux/amd64 \
+    --tag "$BASE_IMG_NAME":$NOW \
+    --tag "$BASE_IMG_NAME":latest \
+    --push \
+    --file .circleci/Dockerfile .
 
   BASE_IMG_NAME=$DOCKER_USERNAME/cli-build-arm64
-  docker buildx build --build-arg NODEVERSION="$NODEVERSION" --build-arg ARCH="aarch64" --platform linux/arm64 -t "$BASE_IMG_NAME":latest -f .circleci/Dockerfile .
-  docker tag "$BASE_IMG_NAME:latest" "$BASE_IMG_NAME:$NOW"
-  docker push "$BASE_IMG_NAME:latest"
-  docker push "$BASE_IMG_NAME:$NOW"
+  docker buildx build \
+    --build-arg NODEVERSION="$NODEVERSION" \
+    --build-arg ARCH="aarch64" \
+    --platform linux/arm64 \
+    --tag "$BASE_IMG_NAME":$NOW \
+    --tag "$BASE_IMG_NAME":latest \
+    --push \
+    --file .circleci/Dockerfile .
+
 popd
