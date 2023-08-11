946b6703bb2f61581dd2be5a9d06386b09f3de0e
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2023-08-11 20:05:45 +0200

    fix: dont_skip_test

--- test/jest/util/isDontSkipTestsEnabled.ts
+++ test/jest/util/isDontSkipTestsEnabled.ts
@@ -1,4 +1,7 @@
 export const isDontSkipTestsEnabled = (): boolean => {
-  const dontSkip = !!process.env.TEST_SNYK_DONT_SKIP_ANYTHING;
+  const dontSkip = !(
+    process.env.TEST_SNYK_DONT_SKIP_ANYTHING === undefined ||
+    process.env.TEST_SNYK_DONT_SKIP_ANYTHING == '0'
+  );
   return dontSkip;
 };
