1a5a4ad28d1565f4e2c2776061cdf1ec51380d0c
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2023-09-19 12:32:43 +0200

    chore: remove unncessary install

--- cliv2/scripts/prepare_licenses.sh
+++ cliv2/scripts/prepare_licenses.sh
@@ -7,5 +7,4 @@ if python3 -c 'print("python3")' > /dev/null 2>&1; then
     PYTHON_VERSION="3"
 fi
 
-PIP_BREAK_SYSTEM_PACKAGES=1 pip$PYTHON_VERSION install requests
 python$PYTHON_VERSION $BASEDIR/prepare_licenses.py
