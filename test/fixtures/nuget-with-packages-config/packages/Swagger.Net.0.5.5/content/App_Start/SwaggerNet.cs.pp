6c57d98f531b094b2623100374b9915c9cb623d4
Lucian Rosu <lucian.rosu@snyk.io>
2023-09-22 17:56:14 +0300

    feat: extra sbom npm,nuget,unmanaged args

---
+++ test/fixtures/nuget-with-packages-config/packages/Swagger.Net.0.5.5/content/App_Start/SwaggerNet.cs.pp
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Dispatcher;
+using System.Web.Routing;
+using Swagger.Net;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.SwaggerNet), "PreStart")]
+[assembly: WebActivator.PostApplicationStartMethod(typeof($rootnamespace$.App_Start.SwaggerNet), "PostStart")]
+namespace $rootnamespace$.App_Start 
+{
+    public static class SwaggerNet 
+    {
+        public static void PreStart() 
+        {
+            RouteTable.Routes.MapHttpRoute(
+                name: "SwaggerApi",
+                routeTemplate: "api/docs/{controller}",
+                defaults: new { swagger = true }
+            );            
+        }
+        
+        public static void PostStart() 
+        {
+            var config = GlobalConfiguration.Configuration;
+
+            config.Filters.Add(new SwaggerActionFilter());
+            
+            try
+            {
+                config.Services.Replace(typeof(IDocumentationProvider),
+                    new XmlCommentDocumentationProvider(HttpContext.Current.Server.MapPath("~/bin/$rootnamespace$.XML")));
+            }
+            catch (FileNotFoundException)
+            {
+                throw new Exception("Please enable \"XML documentation file\" in project properties with default (bin\\$rootnamespace$.XML) value or edit value in App_Start\\SwaggerNet.cs");
+            }
+        }
+    }
+}
