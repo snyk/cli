66b77c1e4a8b97b4a67088fc231545ffff1e9c40
Jason Luong <jason.luong@snyk.io>
2023-09-27 09:56:36 +0100

    revert: remove Dockerfiles for deprecated images (#4762)
    This reverts commit 48da04e3e22eb5601f3cb62f001aa85602a7f408.

---
+++ docker/README.md
@@ -0,0 +1,5 @@
+# Deprecation Notice
+
+Please note, these Docker images are deprecated and may be removed in the future. They remain available only to provide continuity for legacy Snyk integrations. **Snyk does not recommend their use.**
+
+Instead, please use the [`snyk/snyk`](https://hub.docker.com/r/snyk/snyk) Docker images. These images wrap the Snyk CLI and depending on the Tag come with a relevant tooling for different projects. [See the snyk/images on GitHub for more details and examples](https://github.com/snyk/snyk-images).
---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+OUTPUT_FILE=snyk-result.json
+MONITOR_OUTPUT_FILE=snyk-monitor-result.json
+ERROR_FILE=snyk-error.log
+HTML_FILE=snyk_report.html
+SNYK_COMMAND="$1"
+SNYK_PARAMS="${@:2}"
+ADDITIONAL_ENV=""
+
+if [ -z "${USER_ID}" ]; then
+  USER_ID=$(id -u)
+fi
+
+USER_NAME=$(getent passwd "${USER_ID}" | awk -F ':' '{print $1}')
+
+if [ "${USER_NAME}" != "" ] && [ "${USER_NAME}" != "root" ]; then
+  usermod -d /home/node "${USER_NAME}"
+fi
+
+useradd -o -m -u "${USER_ID}" -d /home/node docker-user 2>/dev/null
+
+runCmdAsDockerUser() {
+  su docker-user -m -c "$1"
+
+  return $?
+}
+
+exitWithMsg() {
+  echo "Failed to run the process ..."
+
+  if [ -f "$1" ]; then
+    cat "$1"
+  else
+    echo "$1"
+  fi
+
+  exit "$2"
+}
+
+TEST_SETTINGS=""
+PROJECT_SUBDIR=""
+
+if [ -n "${TARGET_FILE}" ]; then
+  if [ ! -f "${PROJECT_PATH}/${PROJECT_FOLDER}/${TARGET_FILE}" ]; then
+    exitWithMsg "\"${PROJECT_PATH}/${PROJECT_FOLDER}/${TARGET_FILE}\" does not exist" 2
+  fi
+
+  PROJECT_SUBDIR=$(dirname "${TARGET_FILE}")
+  MANIFEST_NAME=$(basename "${TARGET_FILE}")
+  TEST_SETTINGS="--file=${MANIFEST_NAME} "
+fi
+
+if [ -n "${ORGANIZATION}" ]; then
+  TEST_SETTINGS="${TEST_SETTINGS} --org=${ORGANIZATION}"
+fi
+
+SNYK_PARAMS="${SNYK_PARAMS} ${TEST_SETTINGS}"
+
+if [ -z "${SNYK_TOKEN}" ]; then
+  exitWithMsg "Missing \${SNYK_TOKEN}" 2
+fi
+
+if [ -n "${ENV_FLAGS}" ]; then
+  ADDITIONAL_ENV="-- ${ENV_FLAGS}"
+fi
+
+cd "${PROJECT_PATH}/${PROJECT_FOLDER}/${PROJECT_SUBDIR}" ||
+  exitWithMsg "Can't cd to ${PROJECT_PATH}/${PROJECT_FOLDER}/${PROJECT_SUBDIR}" 2
+
+runCmdAsDockerUser "PATH=${PATH} snyk ${SNYK_COMMAND} --json ${SNYK_PARAMS} \
+${ADDITIONAL_ENV} > \"${OUTPUT_FILE}\" 2>\"${ERROR_FILE}\""
+
+RC=$?
+
+if [ "$RC" -ne "0" ] && [ "$RC" -ne "1" ]; then
+  exitWithMsg "${OUTPUT_FILE}" "$RC"
+fi
+
+runCmdAsDockerUser "touch \"${PROJECT_PATH}/${PROJECT_FOLDER}/${HTML_FILE}\""
+
+if [ -n "$MONITOR" ]; then
+  echo "Monitoring & generating report ..."
+  runCmdAsDockerUser "PATH=$PATH snyk monitor --json ${SNYK_PARAMS} ${ADDITIONAL_ENV} > ${MONITOR_OUTPUT_FILE} 2>$ERROR_FILE"
+fi
+
+runCmdAsDockerUser "cat \"${OUTPUT_FILE}\" | snyk-to-html >> \"${PROJECT_PATH}/${PROJECT_FOLDER}/${HTML_FILE}\""
+
+if [ $RC -ne "0" ]; then
+  exitWithMsg "${OUTPUT_FILE}" "$RC"
+fi
+
+cat "${OUTPUT_FILE}"
+
+exit "$RC"
