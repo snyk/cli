c5bba91be3a9f2166c868337dc2f72c05ba1994a
JSON <jason.luong@snyk.io>
2023-09-28 10:11:17 +0100

    revert: remove Dockerfiles for deprecated images (#4762) (#4880)
    This reverts commit 48da04e3e22eb5601f3cb62f001aa85602a7f408.

---
+++ docker/docker-python-entrypoint.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+virtualenv -p python snyk
+source snyk/bin/activate
+
+exitWithMsg() {
+    echo "Failed to run the process ..."
+
+    if [ -f "$1" ]; then
+        cat "$1"
+    else
+        echo "$1"
+    fi
+
+    exit "$2"
+}
+
+installRequirementsTxtDeps() {
+    echo "Installing dependencies from requirements file"
+    pip install -U -r "$1"
+}
+
+installPipfileDeps() {
+    pushd "${PROJECT_PATH}/"
+    echo "Found Pipfile"
+    pipenv install --system --deploy
+    popd
+}
+
+PROJECT_SUBDIR=""
+echo "Project path = ${PROJECT_PATH}"
+if [ -n "${TARGET_FILE}" ]; then
+    if [ ! -f "${PROJECT_PATH}/${PROJECT_FOLDER}/${TARGET_FILE}" ]; then
+        exitWithMsg "\"${PROJECT_PATH}/${PROJECT_FOLDER}/${TARGET_FILE}\" does not exist" 2
+    fi
+
+    PROJECT_SUBDIR=$(dirname "${TARGET_FILE}")
+    MANIFEST_NAME=$(basename "${TARGET_FILE}")
+    TEST_SETTINGS="--file=${MANIFEST_NAME} "
+
+    echo "Target file = ${TARGET_FILE}"
+
+    case $MANIFEST_NAME in
+    *req*.txt)
+        echo "Installing dependencies from requirements file"
+        installRequirementsTxtDeps "${PROJECT_PATH}/$MANIFEST_NAME"
+        ;;
+    *setup.py)
+        echo "Installing dependencies from setup.py"
+        pip install -U -e "${PROJECT_PATH}"
+        ;;
+    *Pipfile)
+        echo "Installing dependencies from Pipfile"
+        installPipfileDeps
+        ;;
+    *)
+        exitWithMsg "\"${PROJECT_PATH}/${TARGET_FILE}\" is not supported" 3
+        ;;
+    esac
+fi
+
+if [ -z "${TARGET_FILE}" ]; then
+    if [ -f "${PROJECT_PATH}/requirements.txt" ]; then
+        echo "Found requirement.txt"
+        installRequirementsTxtDeps "${PROJECT_PATH}/requirements.txt"
+    elif [ -f "${PROJECT_PATH}/setup.py" ]; then
+        echo "Found setup.py"
+        pip install -U -e "${PROJECT_PATH}"
+    elif [ -f "${PROJECT_PATH}/Pipfile" ]; then
+        echo "Found Pipfile"
+        installPipfileDeps
+    fi
+fi
+
+bash docker-entrypoint.sh "$@"
