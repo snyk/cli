92d88b640a593116265fe20afdaa9b0a9f12a9c8
Matthew Rogers <matt.rogers@snyk.io>
2023-11-21 17:14:42 +0000

    fix: allow http requests towards code local engine (#4934)

--- src/lib/plugins/sast/localCodeEngine.ts
+++ src/lib/plugins/sast/localCodeEngine.ts
@@ -1,11 +1,8 @@
 import * as debugLib from 'debug';
 import chalk from 'chalk';
-
 import { makeRequest } from '../../request';
-import { Global } from '../../../cli/args';
 import { SastSettings } from './types';
 
-declare const global: Global;
 const debug = debugLib('snyk-code');
 
 export function isLocalCodeEngine(sastSettings: SastSettings): boolean {
@@ -14,23 +11,21 @@ export function isLocalCodeEngine(sastSettings: SastSettings): boolean {
   return sastEnabled && localCodeEngine.enabled;
 }
 
-export async function logLocalCodeEngineVersion(lceUrl = ''): Promise<void> {
-  const lceBaseUrl = lceUrl.replace('/api', '');
-  const isNonSecureHttp = lceBaseUrl.match(/^http:/);
-  let ignoreUnknownCAoriginalValue;
-  if (isNonSecureHttp) {
-    ignoreUnknownCAoriginalValue = global.ignoreUnknownCA;
-    // `makeRequest` function converts `http` calls to `https`. In some cases, SCLE might be running on http.
-    // This problem is fixed by setting `options.rejectUnauthorized = true`.
-    // Setting `global.ignoreUnknownCA` to true adds rejectUnauthorized=true as an option in `makeRequest`.
-    global.ignoreUnknownCA = true;
+export async function logLocalCodeEngineVersion(
+  localEngineUrl = '',
+): Promise<void> {
+  const parsedUrl = new URL(localEngineUrl);
+  const localEngineBaseUrl = parsedUrl.origin;
+  const isHttp = parsedUrl.protocol.match('http:');
+  const originalProtocolUpgrade = process.env['SNYK_HTTP_PROTOCOL_UPGRADE'];
+  if (isHttp) {
+    process.env.SNYK_HTTP_PROTOCOL_UPGRADE = '0';
   }
-
   try {
     const {
       res: { body, statusCode },
     } = await makeRequest({
-      url: `${lceBaseUrl}/status`,
+      url: `${localEngineBaseUrl}/status`,
       method: 'get',
     });
     if (body?.ok && body?.version) {
@@ -58,9 +53,6 @@ export async function logLocalCodeEngineVersion(lceUrl = ''): Promise<void> {
   } catch (err) {
     debug('Snyk Code Local Engine health check failed.', err);
   } finally {
-    if (isNonSecureHttp) {
-      // Resetting `global.ignoreUnknownCA` to whatever value it had before I changed it above.
-      global.ignoreUnknownCA = ignoreUnknownCAoriginalValue;
-    }
+    process.env.SNYK_HTTP_PROTOCOL_UPGRADE = originalProtocolUpgrade;
   }
 }
