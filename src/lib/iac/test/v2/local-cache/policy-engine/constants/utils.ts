76a251c6843475e6356398256659911aba92fc5b
Francesco Mari <francesco.mari@snyk.io>
2023-12-04 12:08:15 +0100

    feat: upgrade snyk-iac-test to v0.50.4

--- src/lib/iac/test/v2/local-cache/policy-engine/constants/utils.ts
+++ src/lib/iac/test/v2/local-cache/policy-engine/constants/utils.ts
@@ -1,11 +1,11 @@
 import * as os from 'os';
 
 const policyEngineChecksums = `
-0678a0e62acdc567bd5fcee5f6ce59420cf8c70f3082c98cec653d36d86a6c0e  snyk-iac-test_0.50.3_Darwin_x86_64
-69bab03c97735f01c833135c71861dec2afcc2e42fe95a75fe7d7f41399a4144  snyk-iac-test_0.50.3_Windows_x86_64.exe
-764eb0f5e722f9ef5a9951e1e2e314c9b49a77cb7a57aa2e03c0c5799825d8cf  snyk-iac-test_0.50.3_Linux_arm64
-c10e9971285a33736377d8c85890e24a728bbaf1caeccc4bc49d633c25f5e61c  snyk-iac-test_0.50.3_Darwin_arm64
-c60fec5fcfc876378ccf217b0aae1e6564b8e93ac35efcde9d7f61e9f587c87d  snyk-iac-test_0.50.3_Linux_x86_64
+5152d4bc7a8f5f1a7bfdd198b6bb5b82a1d2c66555fdc657f62069a00cc993fe  snyk-iac-test_0.50.4_Linux_x86_64
+59ea910aac234e7ce563078b54ef721adf795a9bc6348183028374662ab35ec9  snyk-iac-test_0.50.4_Windows_x86_64.exe
+5a7136b0dc8b8ee40bf61ab101fb5689d18cf9476521a6a16ed939ad62a6d695  snyk-iac-test_0.50.4_Darwin_x86_64
+87657782a31f66897dd568303b38862d3587589bc12601adfbbdcbd688818814  snyk-iac-test_0.50.4_Linux_arm64
+a7150de35ab607163343ce287eac7cfe8b9a904843837ea643c188d0406a3e2f  snyk-iac-test_0.50.4_Darwin_arm64
 `;
 
 export const policyEngineVersion = getPolicyEngineVersion();
