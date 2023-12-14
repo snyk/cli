5a06079d9bd43ac6c97cfdf70dcaaa4444ead5f6
Mathias Fischer <mathias.fischer@snyk.io>
2023-12-14 10:18:50 +0100

    fix: disable proxy in needle, global-agent will be responsible to proxy requests

--- src/lib/request/request.ts
+++ src/lib/request/request.ts
@@ -106,7 +106,10 @@ function setupRequest(payload: Payload) {
     parsedUrl.protocol === 'http:'
       ? new http.Agent({ keepAlive: true })
       : new https.Agent({ keepAlive: true });
-  const options: needle.NeedleOptions = {
+  const options: needle.NeedleOptions & {
+    use_proxy_from_env_var: boolean | undefined; // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/67880
+  } = {
+    use_proxy_from_env_var: false,
     json: payload.json,
     parse: payload.parse,
     headers: payload.headers,
