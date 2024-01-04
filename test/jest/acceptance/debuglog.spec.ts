8940b97c991bda8ba62def397ef49413fb72e461
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2024-01-04 16:59:30 +0100

    chore: run formatter

--- test/jest/acceptance/debuglog.spec.ts
+++ test/jest/acceptance/debuglog.spec.ts
@@ -1,15 +1,12 @@
 import { runSnykCLI } from '../util/runSnykCLI';
 import { createProjectFromWorkspace } from '../util/createProject';
 
-
 jest.setTimeout(1000 * 60);
 
 describe('debug log', () => {
-
   it('', async () => {
     const project = await createProjectFromWorkspace('cocoapods-app');
-    const token = "mytoken"
-
+    const token = 'mytoken';
 
     const { code, stderr } = await runSnykCLI('test -d', {
       cwd: project.path(),
@@ -18,12 +15,12 @@ describe('debug log', () => {
         SNYK_DISABLE_ANALYTICS: '1',
         DEBUG: '*',
         SNYK_LOG_LEVEL: 'trace',
-        SNYK_TOKEN: token
+        SNYK_TOKEN: token,
       },
     });
 
-    console.debug(stderr)
-    expect(stderr).not.toContain(token)
+    console.debug(stderr);
+    expect(stderr).not.toContain(token);
     expect(code).toEqual(2);
   });
 });
