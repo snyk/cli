319918ee02240bab8a255fb8a455aea30d999922
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2024-01-04 11:04:24 +0100

    test: add failing token redact test

---
+++ test/jest/acceptance/debuglog.spec.ts
@@ -0,0 +1,28 @@
+import { runSnykCLI } from '../util/runSnykCLI';
+import { createProjectFromWorkspace } from '../util/createProject';
+
+
+jest.setTimeout(1000 * 60);
+
+describe('debug log', () => {
+
+  it('', async () => {
+    const project = await createProjectFromWorkspace('cocoapods-app');
+    const token = "mytoken"
+
+
+    const { code, stderr } = await runSnykCLI('test -d', {
+      cwd: project.path(),
+      env: {
+        ...process.env,
+        SNYK_DISABLE_ANALYTICS: '1',
+        DEBUG: '*',
+        SNYK_TOKEN: token
+      },
+    });
+
+    console.debug(stderr)
+    expect(stderr).not.toContain(token)
+    expect(code).toEqual(2);
+  });
+});
