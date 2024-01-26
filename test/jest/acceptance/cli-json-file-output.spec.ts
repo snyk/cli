304356c7e8dce910f6a5eaccc3c8d41c7ea92dbd
Jason Luong <jason.luong@snyk.io>
2024-01-26 14:43:24 +0000

    chore: move common fn to utils

--- test/jest/acceptance/cli-json-file-output.spec.ts
+++ test/jest/acceptance/cli-json-file-output.spec.ts
@@ -2,6 +2,7 @@ import { fakeServer } from '../../acceptance/fake-server';
 import * as fs from 'fs';
 import { createProjectFromWorkspace } from '../util/createProject';
 import { runSnykCLI } from '../util/runSnykCLI';
+import { humanFileSize } from '../../utils';
 
 jest.setTimeout(1000 * 60);
 
@@ -146,37 +147,3 @@ describe('test --json-file-output', () => {
     expect(fileSize).toBeGreaterThan(500000000); // ~0.5GB
   }, 120000);
 });
-
-/**
- * Format bytes as human-readable text.
- *
- * @param bytes Number of bytes.
- * @param si True to use metric (SI) units, aka powers of 1000. False to use
- *           binary (IEC), aka powers of 1024.
- * @param dp Number of decimal places to display.
- *
- * @return Formatted string.
- */
-function humanFileSize(bytes, si = false, dp = 1) {
-  const thresh = si ? 1000 : 1024;
-
-  if (Math.abs(bytes) < thresh) {
-    return bytes + ' B';
-  }
-
-  const units = si
-    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-    : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-  let u = -1;
-  const r = 10 ** dp;
-
-  do {
-    bytes /= thresh;
-    ++u;
-  } while (
-    Math.round(Math.abs(bytes) * r) / r >= thresh &&
-    u < units.length - 1
-  );
-
-  return bytes.toFixed(dp) + ' ' + units[u];
-}
--- test/jest/unit/json-file-output.spec.ts
+++ test/jest/unit/json-file-output.spec.ts
@@ -7,6 +7,7 @@ import {
   writeContentsToFileSwallowingErrors,
 } from '../../../src/lib/json-file-output';
 import * as os from 'os';
+import { humanFileSize } from '../../utils';
 
 const isWindows = os.platform().indexOf('win') === 0;
 
@@ -71,36 +72,3 @@ describe('saveObjectToFileCreatingDirectoryIfRequired', () => {
   });
 });
 
-/**
- * Format bytes as human-readable text.
- *
- * @param bytes Number of bytes.
- * @param si True to use metric (SI) units, aka powers of 1000. False to use
- *           binary (IEC), aka powers of 1024.
- * @param dp Number of decimal places to display.
- *
- * @return Formatted string.
- */
-function humanFileSize(bytes, si = false, dp = 1) {
-  const thresh = si ? 1000 : 1024;
-
-  if (Math.abs(bytes) < thresh) {
-    return bytes + ' B';
-  }
-
-  const units = si
-    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-    : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-  let u = -1;
-  const r = 10 ** dp;
-
-  do {
-    bytes /= thresh;
-    ++u;
-  } while (
-    Math.round(Math.abs(bytes) * r) / r >= thresh &&
-    u < units.length - 1
-  );
-
-  return bytes.toFixed(dp) + ' ' + units[u];
-}
