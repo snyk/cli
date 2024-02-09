01187da980616120117b33df5a3cc285ed8142af
Luke Watts <luke@snyk.io>
2024-02-09 11:38:52 +0100

    docs: introduce comments to create-build-script

--- scripts/create-build-image.sh
+++ scripts/create-build-image.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-set -exuo pipefail
-# determine script directory
+set -exuo pipefail # ensures that the script exits on any error, and that all commands are printed before they are executed
+
+# This script is used for building Docker images which in turn build the CLI.
+# It sets up the environment, logs into Docker, and builds images for different architectures.
+
+# Before running the script, ensure DOCKER_USERNAME and DOCKER_PASSWORD environment variables are set.
+# Example usage:
+#   export DOCKER_USERNAME=<your-docker-hub-username>
+#   export DOCKER_PASSWORD=<a-docker-hub-personal-access-token>
+#   ./scripts/create-build-image.sh
+
+# Determine the directory where the script is located.
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 NOW=$(date "+%Y%m%d-%H%M%S")
 
