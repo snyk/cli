63b3a91db21b39281f53d8351f6f28a006181d83
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2024-02-14 16:29:57 +0100

    fix: remove v prefix for version

--- release-scripts/next-version.sh
+++ release-scripts/next-version.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 # Only output the next version to stdout. All other output should go to stderr.
 
 RELEASE_BRANCH="master"
-NEXT_VERSION="v$(convco version --bump)"
+NEXT_VERSION="$(convco version --bump)"
+CURRENT_TAG="$(git describe --tags `git rev-list --tags --max-count=1`)"
 
 if [ "${CIRCLE_BRANCH:-}" != "${RELEASE_BRANCH}" ]; then
     NEXT_VERSION="${NEXT_VERSION}-dev.$(git rev-parse HEAD)"
 fi
 
-echo "Current version: $(git describe --tags `git rev-list --tags --max-count=1`)" 1>&2
+echo "Current version: ${CURRENT_TAG/v/}" 1>&2
 echo "Next version:    ${NEXT_VERSION}" 1>&2
 
 echo "${NEXT_VERSION}"
