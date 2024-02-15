c0af11196e6cfe06b073e4f2d5f622b34772c467
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2024-02-15 12:08:05 +0100

    chore: renaming master to main

--- release-scripts/next-version.sh
+++ release-scripts/next-version.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 # Checks the latest version of Snyk CLI on npm and decides the next version.
 # Only output the next version to stdout. All other output should go to stderr.
 
-RELEASE_BRANCH="master"
+RELEASE_BRANCH="main"
 NEXT_VERSION="$(convco version --bump)"
 CURRENT_TAG="$(git describe --tags `git rev-list --tags --max-count=1`)"
 
