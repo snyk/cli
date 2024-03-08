14f31dd5de649033cf456cb8042778e38e8e60b7
PeterSchafer <101886095+PeterSchafer@users.noreply.github.com>
2024-03-08 16:48:39 +0100

    chore: support additional version postfixes (#5096)
    * chore: support additional version postfixes
    
    * chore: change default version
    
    * chore: add comment

---
+++ release-scripts/determine-release-channel.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This script determines the appropriate release channel for a project based on
+# the current Git branch. The script should always return a string value that
+# matches one of the supported channels.
+#
+# The following release channels are supported:
+# - stable
+# - rc
+# - preview
+# - dev
+
+
+# enable support for stable, preview and other release channels
+ENABLE_STABLE_CHANNELS=false
+
+CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [ $ENABLE_STABLE_CHANNELS == true ]; then # support for stable, preview and release candidate
+  if [ "$CURRENT_BRANCH" == "main" ]; then
+    echo "preview"
+  elif [ "$CURRENT_BRANCH" == "release-candidate" ]; then
+    echo "rc"
+  elif [[ "$CURRENT_BRANCH" == release/* ]]; then
+    echo "stable"
+  else
+    echo "dev"
+  fi
+else # legacy release channels
+  if [ "$CURRENT_BRANCH" == "main" ]; then
+    echo "stable"
+  else
+    echo "dev"
+  fi
+fi
+
