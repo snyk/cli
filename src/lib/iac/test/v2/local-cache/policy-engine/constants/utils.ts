58ed65ac887246994754196302730e1644d9ad91
Jasper Van der Jeugt <jasper.vanderjeugt@snyk.io>
2024-03-20 15:01:51 +0100

    feat: upgrade snyk-iac-test to v0.51.1
    Upgrade snyk-iac-test to v0.51.1.  Changes:
    
    - Print scan URL to debug output after uploading report.
    - Fix panic on invalid arm input

--- src/lib/iac/test/v2/local-cache/policy-engine/constants/utils.ts
+++ src/lib/iac/test/v2/local-cache/policy-engine/constants/utils.ts
@@ -1,11 +1,11 @@
 import * as os from 'os';
 
 const policyEngineChecksums = `
-5152d4bc7a8f5f1a7bfdd198b6bb5b82a1d2c66555fdc657f62069a00cc993fe  snyk-iac-test_0.50.4_Linux_x86_64
-59ea910aac234e7ce563078b54ef721adf795a9bc6348183028374662ab35ec9  snyk-iac-test_0.50.4_Windows_x86_64.exe
-5a7136b0dc8b8ee40bf61ab101fb5689d18cf9476521a6a16ed939ad62a6d695  snyk-iac-test_0.50.4_Darwin_x86_64
-87657782a31f66897dd568303b38862d3587589bc12601adfbbdcbd688818814  snyk-iac-test_0.50.4_Linux_arm64
-a7150de35ab607163343ce287eac7cfe8b9a904843837ea643c188d0406a3e2f  snyk-iac-test_0.50.4_Darwin_arm64
+1bc0af9da49216a52e8cef41f48b4544471930c3d9fb9cc549d2ff75a55589a1  snyk-iac-test_0.51.2_Linux_x86_64
+410ee514e20bc796d070afb23b9fe66fdeca97358f965d92fb2d6359829e5ac8  snyk-iac-test_0.51.2_Windows_x86_64.exe
+846b3d154a7ddd50f7aa64a8bba9b5436672acdd6efdca1a014ae239649a240f  snyk-iac-test_0.51.2_Linux_arm64
+a67afe8f0ddd8456b2845253ba4ab0ad1aceb8bd04629b4947844c1db921ac19  snyk-iac-test_0.51.2_Darwin_x86_64
+c3c90bb71e78978c73e68a04b8603f9d2a90e067cb0affd1b89d68090c6c74b2  snyk-iac-test_0.51.2_Darwin_arm64
 `;
 
 export const policyEngineVersion = getPolicyEngineVersion();
