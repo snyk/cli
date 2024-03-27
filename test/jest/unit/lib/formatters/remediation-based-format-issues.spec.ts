675cc5a42da2ce5f96733c7b78c99a0461e2e7fd
Luke Watts <luke@snyk.io>
2024-03-27 13:37:19 +0100

    test: refactor to group test cases around method under test

--- test/jest/unit/lib/formatters/remediation-based-format-issues.spec.ts
+++ test/jest/unit/lib/formatters/remediation-based-format-issues.spec.ts
@@ -6,105 +6,108 @@ import { formatIssuesWithRemediation } from '../../../../../src/lib/formatters/r
 import { getFixturePath } from '../../../util/getFixturePath';
 import { getWorkspacePath } from '../../../util/getWorkspacePath';
 
-it('with pins & unfixable & showVulnsPaths = all', () => {
-  const withRemediation = JSON.parse(
-    fs.readFileSync(
-      getFixturePath('pip-app-with-remediation/test-graph-results.json'),
-      'utf8',
-    ),
-  );
-  const groupedVulns = groupVulnerabilities(withRemediation.vulnerabilities);
-
-  const sortedGroupedVulns = orderBy(
-    groupedVulns,
-    ['metadata.severityValue', 'metadata.name'],
-    ['asc', 'desc'],
-  );
-
-  const res = formatIssuesWithRemediation(
-    sortedGroupedVulns,
-    withRemediation.remediation,
-    { showVulnPaths: 'all' },
-  );
-  expect(
-    stripAnsi(res.join('\n').replace(/\[http.*\]/g, '[URL]')),
-  ).toMatchSnapshot();
-});
-
-it('with showVulnPaths = some', () => {
-  const withRemediation = JSON.parse(
-    fs.readFileSync(
-      getFixturePath('pip-app-with-remediation/test-graph-results.json'),
-      'utf8',
-    ),
-  );
-  const groupedVulns = groupVulnerabilities(withRemediation.vulnerabilities);
-
-  const sortedGroupedVulns = orderBy(
-    groupedVulns,
-    ['metadata.severityValue', 'metadata.name'],
-    ['asc', 'desc'],
-  );
-
-  const res = formatIssuesWithRemediation(
-    sortedGroupedVulns,
-    withRemediation.remediation,
-    { showVulnPaths: 'some' },
-  );
-  expect(
-    stripAnsi(res.join('\n').replace(/\[http.*\]/g, '[URL]')),
-  ).toMatchSnapshot();
-});
-it('with upgrades & patches', () => {
-  const withRemediation = JSON.parse(
-    fs.readFileSync(
-      getFixturePath(
-        'npm-package-with-severity-override/test-graph-result-patches.json',
+describe('formatIssuesWithRemediation', () => {
+  it('with pins & unfixable & showVulnsPaths = all', () => {
+    const withRemediation = JSON.parse(
+      fs.readFileSync(
+        getFixturePath('pip-app-with-remediation/test-graph-results.json'),
+        'utf8',
       ),
-      'utf8',
-    ),
-  );
-  const groupedVulns = groupVulnerabilities(withRemediation.vulnerabilities);
+    );
+    const groupedVulns = groupVulnerabilities(withRemediation.vulnerabilities);
 
-  const sortedGroupedVulns = orderBy(
-    groupedVulns,
-    ['metadata.severityValue', 'metadata.name'],
-    ['asc', 'desc'],
-  );
+    const sortedGroupedVulns = orderBy(
+      groupedVulns,
+      ['metadata.severityValue', 'metadata.name'],
+      ['asc', 'desc'],
+    );
 
-  const res = formatIssuesWithRemediation(
-    sortedGroupedVulns,
-    withRemediation.remediation,
-    { showVulnPaths: 'all' },
-  );
-  expect(
-    stripAnsi(res.join('\n').replace(/\[http.*\]/g, '[URL]')),
-  ).toMatchSnapshot();
-});
+    const res = formatIssuesWithRemediation(
+      sortedGroupedVulns,
+      withRemediation.remediation,
+      { showVulnPaths: 'all' },
+    );
+    expect(
+      stripAnsi(res.join('\n').replace(/\[http.*\]/g, '[URL]')),
+    ).toMatchSnapshot();
+  });
 
-it('with license issues', () => {
-  const withRemediation = JSON.parse(
-    fs.readFileSync(
-      getWorkspacePath(
-        'ruby-app/test-graph-response-with-legal-instruction.json',
+  it('with showVulnPaths = some', () => {
+    const withRemediation = JSON.parse(
+      fs.readFileSync(
+        getFixturePath('pip-app-with-remediation/test-graph-results.json'),
+        'utf8',
       ),
-      'utf8',
-    ),
-  );
-  const groupedVulns = groupVulnerabilities(withRemediation.vulnerabilities);
+    );
+    const groupedVulns = groupVulnerabilities(withRemediation.vulnerabilities);
 
-  const sortedGroupedVulns = orderBy(
-    groupedVulns,
-    ['metadata.severityValue', 'metadata.name'],
-    ['asc', 'desc'],
-  );
+    const sortedGroupedVulns = orderBy(
+      groupedVulns,
+      ['metadata.severityValue', 'metadata.name'],
+      ['asc', 'desc'],
+    );
 
-  const res = formatIssuesWithRemediation(
-    sortedGroupedVulns,
-    withRemediation.remediation,
-    { showVulnPaths: 'all' },
-  );
-  expect(
-    stripAnsi(res.join('\n').replace(/\[http.*\]/g, '[URL]')),
-  ).toMatchSnapshot();
+    const res = formatIssuesWithRemediation(
+      sortedGroupedVulns,
+      withRemediation.remediation,
+      { showVulnPaths: 'some' },
+    );
+    expect(
+      stripAnsi(res.join('\n').replace(/\[http.*\]/g, '[URL]')),
+    ).toMatchSnapshot();
+  });
+  
+  it('with upgrades & patches', () => {
+    const withRemediation = JSON.parse(
+      fs.readFileSync(
+        getFixturePath(
+          'npm-package-with-severity-override/test-graph-result-patches.json',
+        ),
+        'utf8',
+      ),
+    );
+    const groupedVulns = groupVulnerabilities(withRemediation.vulnerabilities);
+
+    const sortedGroupedVulns = orderBy(
+      groupedVulns,
+      ['metadata.severityValue', 'metadata.name'],
+      ['asc', 'desc'],
+    );
+
+    const res = formatIssuesWithRemediation(
+      sortedGroupedVulns,
+      withRemediation.remediation,
+      { showVulnPaths: 'all' },
+    );
+    expect(
+      stripAnsi(res.join('\n').replace(/\[http.*\]/g, '[URL]')),
+    ).toMatchSnapshot();
+  });
+
+  it('with license issues', () => {
+    const withRemediation = JSON.parse(
+      fs.readFileSync(
+        getWorkspacePath(
+          'ruby-app/test-graph-response-with-legal-instruction.json',
+        ),
+        'utf8',
+      ),
+    );
+    const groupedVulns = groupVulnerabilities(withRemediation.vulnerabilities);
+
+    const sortedGroupedVulns = orderBy(
+      groupedVulns,
+      ['metadata.severityValue', 'metadata.name'],
+      ['asc', 'desc'],
+    );
+
+    const res = formatIssuesWithRemediation(
+      sortedGroupedVulns,
+      withRemediation.remediation,
+      { showVulnPaths: 'all' },
+    );
+    expect(
+      stripAnsi(res.join('\n').replace(/\[http.*\]/g, '[URL]')),
+    ).toMatchSnapshot();
+  });
 });
