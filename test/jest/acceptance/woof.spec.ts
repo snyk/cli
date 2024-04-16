493abe4c02d518c25901766884b79b0a078e004a
Marian N. Gyorke <marian.gyorke@snyk.io>
2024-04-16 10:28:18 +0300

    feat: snyk woof ro language support and tests

--- test/jest/acceptance/woof.spec.ts
+++ test/jest/acceptance/woof.spec.ts
@@ -1,29 +1,46 @@
 import { runSnykCLI } from '../util/runSnykCLI';
 
 describe('woof', () => {
+  // supported languages
+  const languages = [
+    { langCode: 'en', expectedWoof: 'Woof!' },
+    { langCode: 'he', expectedWoof: 'בה! ' },
+    { langCode: 'ru', expectedWoof: 'Гав!' },
+    { langCode: 'es', expectedWoof: 'Guau!' },
+    { langCode: 'cs', expectedWoof: 'Haf!' },
+    { langCode: 'uk', expectedWoof: 'Гав!' },
+    { langCode: 'de', expectedWoof: 'Wuff!' },
+    { langCode: 'ro', expectedWoof: 'Ham!' },
+    { langCode: 'cat', expectedWoof: 'Meow?' },
+  ];
+
+  // test default
   it('Woofs in English by default', async () => {
     const { stdout, code, stderr } = await runSnykCLI(`woof`);
-
     expect(stdout).toContain('Woof!');
     expect(code).toBe(0);
     expect(stderr).toBe('');
   });
 
+  // test unsuported
   it('Woofs in English when passed unsupported language', async () => {
     const { stdout, stderr, code } = await runSnykCLI(
       `woof --language=blalbla`,
     );
-
     expect(stdout).toContain('Woof!');
     expect(code).toBe(0);
     expect(stderr).toBe('');
   });
 
-  it('Woofs in Czech when passed "cs"', async () => {
-    const { stdout, code, stderr } = await runSnykCLI(`woof --language=cs`);
-
-    expect(stdout).toContain('Haf!');
-    expect(code).toBe(0);
-    expect(stderr).toBe('');
+  // test each supported language code
+  languages.forEach(({ langCode, expectedWoof }) => {
+    it(`Woofs in ${langCode}`, async () => {
+      const { stdout, code, stderr } = await runSnykCLI(
+        `woof --language=${langCode}`,
+      );
+      expect(stdout).toContain(expectedWoof);
+      expect(code).toBe(0);
+      expect(stderr).toBe('');
+    });
   });
 });
