19c1a2472f5b83c39ddafda92204ecc2c61461b4
Marian N. Gyorke <marian.gyorke@snyk.io>
2024-04-16 15:27:51 +0300

    chore: handle python dependencies in install-dev-dependencies.sh
    The installation script creates a local .venv environment that
    needs to be activated before starting a build.

--- scripts/install-dev-dependencies.sh
+++ scripts/install-dev-dependencies.sh
@@ -3,3 +3,11 @@ set -exuo pipefail
 
 # requires https://brew.sh/
 brew bundle --file=$(dirname "$0")/Brewfile
+
+# create python venv and activate it
+python3 -m venv .venv
+source .venv/bin/activate
+
+# install dependencies
+pip install requests
+pip install pyyaml
