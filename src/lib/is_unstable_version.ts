854c810dd5c04ec27fe85d941617147352eb9005
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2024-06-11 10:49:10 +0200

    chore: fix linter

--- src/lib/is_unstable_version.ts
+++ src/lib/is_unstable_version.ts
@@ -1,5 +1,5 @@
 export function isUnstableVersion(): boolean {
-  if (process.env.SNYK_INTERNAL_IS_UNSTABLE === "1") {
+  if (process.env.SNYK_INTERNAL_IS_UNSTABLE === '1') {
     return true;
   }
   return false;
