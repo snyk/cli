d55670144384878b4c2998744faae37eafd98560
jabaran <jabaran@gmail.com>
2024-08-11 18:56:19 -0400

    Adding support for GH Advanced Security SARIF Upload
    Github annoucned changes to their SARIF [upload](https://github.blog/changelog/2024-05-06-code-scanning-will-stop-combining-runs-from-a-single-upload/) the impact is when a SARIF that is being uploaded, each run must have unique "category", as defined by GitHub [here](https://docs.github.com/en/code-security/code-scanning/integrating-with-code-scanning/sarif-support-for-code-scanning#runautomationdetails-object)
    
    GH's new requirement presents a new problem for when a file is removed from source because GH will not have an empty result for the previous file to close any previously opened items as would happen with `--all-projects` previously since GH would merge all results into a single result and closed items no longer present, but now that the closure is  based on tool.driver.name + Category and we have unique files, this will be more problematic.
    
    "Open source's" solution is the most obvious, include the targetFile. Since Snyk-iac, is already populating the field with the static value of "snyk-iac".  It seemed wise to combine riff off that and use "snyk-sca" for OpenSource. Then I separated those values with a pipe and included the `targetFile`
    
    This pattern was then applied to "snyk-iac" and added to "snyk-container". I wasn't able to find unit tests for SARIF output for "snyk-iac" or "snyk-container" or flags to test the output; however, given the simliarities in the implementation to snyk-sca I felt it was worth submitting.

--- src/lib/formatters/open-source-sarif-output.ts
+++ src/lib/formatters/open-source-sarif-output.ts
@@ -27,6 +27,7 @@ export function createSarifOutputForOpenSource(
       'https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json',
     version: '2.1.0',
     runs: testResults.map(replaceLockfileWithManifest).map((testResult) => ({
+      automationDetails : getAutomationDetails(testResult),
       tool: {
         driver: {
           name: 'Snyk Open Source',
@@ -41,6 +42,19 @@ export function createSarifOutputForOpenSource(
       },
       results: getResults(testResult),
     })),
+  };
+}
+
+// Github anncouned changes to their SARIF upload -- https://github.blog/changelog/2024-05-06-code-scanning-will-stop-combining-runs-from-a-single-upload/
+// the impact is when a SARIF that is being uploaded, each run must have unique category, as defined by GitHub here, https://docs.github.com/en/code-security/code-scanning/integrating-with-code-scanning/sarif-support-for-code-scanning#runautomationdetails-object
+// This presents a new problem of when a file is removed from source since GH will not have an empty result to close any previously opened items since GH open/closes
+// based on the SARIF tool.driver.name + Category. Open source's solution is the most obvious, inlcude the targetFile. Snyk-iac, is using this field set to a static "snyk-iac". Combing what
+// was being done there with the file name to generate the unique value. Using | as a separator to make it easier to parse out tool vs targetFile. 
+function getAutomationDetails(testResult: TestResult)
+{
+  let automationId = !!process.env.SET_AUTOMATION_DETAILS_ID ? `snyk-sca|${testResult.displayTargetFile || testResult.targetFile }/` : ""
+  return {
+    id : automationId,
   };
 }
 
