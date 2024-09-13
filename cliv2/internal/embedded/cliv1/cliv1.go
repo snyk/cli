d559c921d40456887043002d4f683734f8c482a7
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2024-09-13 16:04:04 +0200

    fix: use runtimeInfo to derive the version for cliv1 path

--- cliv2/internal/embedded/cliv1/cliv1.go
+++ cliv2/internal/embedded/cliv1/cliv1.go
@@ -2,11 +2,9 @@ package cliv1
 
 import (
 	_ "embed"
-	"path"
 	"strings"
 
 	"github.com/snyk/cli/cliv2/internal/embedded"
-	"github.com/snyk/cli/cliv2/internal/utils"
 )
 
 // The actual version gets injected at build time
@@ -14,15 +12,6 @@ var snykCLIVersion string = "0.0.0"
 
 func CLIV1Version() string {
 	return strings.TrimSpace(snykCLIVersion)
-}
-
-// Get the full path to where we expect the CLIv1 to be in the cache
-// If it doesn't exist, this is the path where we will then extract it
-func GetFullCLIV1TargetPath(cacheDir string) (string, error) {
-	cliv1Filename := getCLIv1Filename()
-	versionTag := CLIV1Version()
-	fullPath := path.Join(utils.GetVersionCacheDirectory(cacheDir, versionTag), cliv1Filename)
-	return fullPath, nil
 }
 
 func ExtractTo(targetFullPath string) error {
