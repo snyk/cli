48895fa93b2e99934b14cfaede0f44b1808f8bee
Sandor Trombitas <sandor.trombitas@snyk.io>
2024-09-17 10:26:53 +0300

    chore: update resolve deps package

--- src/lib/policy/pluck-policies.ts
+++ src/lib/policy/pluck-policies.ts
@@ -1,12 +1,16 @@
 const flatten = require('lodash.flatten');
-import { PackageExpanded } from 'snyk-resolve-deps';
+import { PackageExpanded } from 'snyk-resolve-deps/dist/types';
 
 export function pluckPolicies(pkg: PackageExpanded): string[] | string {
   if (!pkg) {
     return [];
   }
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore: broken type
   if (pkg.snyk) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore: broken type
     return pkg.snyk;
   }
 
