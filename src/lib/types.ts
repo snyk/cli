4756f270f16fcd1588fec1f4d4be846c32e31271
adrobuta <alexandra.drobut@snyk.io>
2024-12-10 10:09:59 +0200

    feat: container test/monitor add support for --exclude-node-modules option

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -144,6 +144,7 @@ export interface MonitorOptions {
   // Used with the Docker plugin only. Allows application scanning.
   'app-vulns'?: boolean;
   'exclude-app-vulns'?: boolean;
+  'exclude-node-modules'?: boolean;
   initScript?: string;
   yarnWorkspaces?: boolean;
   'max-depth'?: number;
