2faf2d16338b7d7b67e6faa116db6e4408864d15
adrobuta <alexandra.drobut@snyk.io>
2024-12-19 16:38:23 +0200

    fix: add container test doc info for --exclude-node-modules

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -75,6 +75,7 @@ export interface Options {
   // Used with the Docker plugin only. Allows application scanning.
   'app-vulns'?: boolean;
   'exclude-app-vulns'?: boolean;
+  'exclude-node-modules'?: boolean;
   debug?: boolean;
   sarif?: boolean;
   'group-issues'?: boolean;
