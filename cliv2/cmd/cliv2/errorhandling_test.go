6e05bb83789f5719ae9f50477dec69ced50bf8aa
Luke Watts <luke@snyk.io>
2025-02-20 19:17:59 +0100

    test: add validation that nested ExitError are preserved

--- cliv2/cmd/cliv2/errorhandling_test.go
+++ cliv2/cmd/cliv2/errorhandling_test.go
@@ -20,6 +20,18 @@ func Test_decorateError(t *testing.T) {
 		assert.Nil(t, decorateError(nil, meta))
 	})
 
+	t.Run("preserves nested ExitError", func(t *testing.T) {
+		err1 := cli.NewConnectionTimeoutError("")
+		err2 := &exec.ExitError{
+			ProcessState: &os.ProcessState{},
+		}
+		actualErr := decorateError(errors.Join(err1, err2), meta)
+		// Assert that err2 is present in actualErr
+		if !errors.Is(actualErr, err2) {
+			t.Errorf("Expected actualErr to contain err2, but it did not")
+		}
+	})
+
 	t.Run("adds metadata to snyk_error", func(t *testing.T) {
 		metaValues := map[string]any{"Foo": "bar"}
 		err := cli.NewConnectionTimeoutError("")
