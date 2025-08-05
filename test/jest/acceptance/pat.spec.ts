ad8e4a7ab979b0638bd9637bc7c4c7abff394cb4
Jason Luong <jason.luong@snyk.io>
2025-08-05 16:31:16 +0100

    feat: support PAT auto region configuration

--- test/jest/acceptance/pat.spec.ts
+++ test/jest/acceptance/pat.spec.ts
@@ -1,22 +1,24 @@
 import { fakeServer, getFirstIPv4Address } from '../../acceptance/fake-server';
 import { runSnykCLI } from '../util/runSnykCLI';
 import { getServerPort } from '../util/getServerPort';
+import { getCliConfig, restoreCliConfig } from '../../acceptance/config-helper';
+import { createPAT } from '../util/createPAT';
 
 jest.setTimeout(1000 * 60);
 
 describe('PAT', () => {
   let server: ReturnType<typeof fakeServer>;
   let env: Record<string, string>;
-
-  const pat = 'snyk_uat.12345678.abcdefg-hijklmnop.qrstuvwxyz-123456';
+  let pat: string;
+  let initialConfig: Record<string, string> = {};
 
   beforeAll((done) => {
     const apiPath = '/api/v1';
     const apiPort = getServerPort(process);
-    const apiBaseUrl = `http://${getFirstIPv4Address()}:${apiPort}`;
+    const host = `http://${getFirstIPv4Address()}:${apiPort}`;
+    pat = createPAT({ host });
     env = {
       ...process.env,
-      SNYK_API: apiBaseUrl,
       SNYK_TOKEN: pat,
       SNYK_DISABLE_ANALYTICS: '1',
     };
@@ -25,17 +27,49 @@ describe('PAT', () => {
     server.listen(apiPort, () => done());
   });
 
-  afterEach(() => {
-    server.restore();
-  });
-
   afterAll((done) => {
     server.close(() => done());
   });
 
-  it('uses pat for authorised requests', async () => {
-    const helpCmd = await runSnykCLI(`whoami --experimental -d`, { env });
-    expect(helpCmd.code).toEqual(0);
+  beforeEach(async () => {
+    // backup snyk config
+    initialConfig = await getCliConfig();
+    await runSnykCLI(`config clear`, {
+      env,
+    });
+  });
+
+  afterEach(async () => {
+    await restoreCliConfig(initialConfig);
+    server.restore();
+  });
+
+  it('derives api from pat for authorised requests', async () => {
+    const whoamicmd = await runSnykCLI(`whoami --experimental -d`, {
+      env,
+      logErrors: true,
+    });
+    expect(whoamicmd.code).toEqual(0);
+
+    expect(server.getRequests().length).toBeGreaterThanOrEqual(1);
+    server.getRequests().forEach((request) => {
+      expect(request).toMatchObject({
+        headers: {
+          authorization: `token ${pat}`,
+        },
+      });
+    });
+  });
+
+  it('api derived from pat takes precedence over SNYK_API', async () => {
+    const whoamicmd = await runSnykCLI(`whoami --experimental -d`, {
+      env: {
+        ...env,
+        SNYK_API: 'https://api.this.should.not.be.used.io',
+      },
+      logErrors: true,
+    });
+    expect(whoamicmd.code).toEqual(0);
 
     expect(server.getRequests().length).toBeGreaterThanOrEqual(1);
     server.getRequests().forEach((request) => {
@@ -49,16 +83,15 @@ describe('PAT', () => {
 
   it('auth error when pat is invalid', async () => {
     const invalidPat = 'snyk_uat.12345678.thisisa-ninvalidp.atnotvalid-123456';
-    const whoamiCmd = await runSnykCLI(`whoami --experimental -d`, {
+    const whoamicmd = await runSnykCLI(`whoami --experimental -d`, {
       env: {
         ...env,
         // make a real API request with an invalid token
         SNYK_TOKEN: invalidPat,
-        SNYK_API: 'https://api.snyk.io',
       },
     });
 
-    expect(whoamiCmd.code).toEqual(2);
-    expect(whoamiCmd.stdout).toContain('Authentication error');
+    expect(whoamicmd.code).toEqual(2);
+    expect(whoamicmd.stdout).toContain('Authentication error');
   });
 });
