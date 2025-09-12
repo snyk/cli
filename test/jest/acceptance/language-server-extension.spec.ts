35a897c3002e1a0e6ebf7c9c8821a8ca322712e7
Kaspar Lyngsie <kaspar.moss@snyk.io>
2025-09-12 12:44:04 +0200

    chore: removing no-op test that delays testing by 45 seconds

--- test/jest/acceptance/language-server-extension.spec.ts
+++ test/jest/acceptance/language-server-extension.spec.ts
@@ -1,8 +1,4 @@
 import { runSnykCLI } from '../util/runSnykCLI';
-import { pathToFileURL } from 'url';
-import { sleep } from '../../../src/lib/common';
-import * as cp from 'child_process';
-import * as rpc from 'vscode-jsonrpc/node';
 
 jest.setTimeout(1000 * 120);
 
@@ -26,86 +22,5 @@ describe('Language Server Extension', () => {
     expect(result.code).toBe(0);
     expect(cliResult.code).toBe(0);
     expect(result.stdout).not.toEqual(cliResult.stdout);
-  });
-
-  it('run and wait for diagnostics', async () => {
-    let cmd = '';
-    if (process.env.TEST_SNYK_COMMAND !== undefined) {
-      cmd = process.env.TEST_SNYK_COMMAND;
-    }
-
-    const cli = cp.spawn(cmd, ['language-server'], { stdio: 'pipe' }); // Use stdin and stdout for communication:
-
-    const connection = rpc.createMessageConnection(
-      new rpc.StreamMessageReader(cli.stdout),
-      new rpc.StreamMessageWriter(cli.stdin),
-    );
-
-    // create an RPC endpoint for the process
-    connection.listen();
-
-    await connection.sendRequest('initialize', {
-      processId: process.pid,
-      capabilities: {
-        window: {
-          workDoneProgress: true,
-        },
-      },
-      clientInfo: {
-        name: 'FakeIDE',
-        version: '4.5.6',
-      },
-      workspaceFolders: [
-        {
-          name: 'workspace',
-          uri: pathToFileURL('.').href,
-        },
-      ],
-      rootUri: null,
-      initializationOptions: {
-        activateSnykCodeSecurity: 'false',
-        activateSnykCodeQuality: 'false',
-        activateSnykOpenSource: 'true',
-        activateSnykIac: 'false',
-        endpoint: process.env.TEST_SNYK_API,
-        token: process.env.TEST_SNYK_TOKEN,
-        manageBinariesAutomatically: 'false',
-        enableTrustedFoldersFeature: 'false',
-        integrationName: 'MyFakePlugin',
-        integrationVersion: '1.2.3',
-        enableTelemetry: 'false',
-        cliPath: cmd,
-      },
-    });
-
-    let diagnosticCount = 0;
-    connection.onNotification(
-      'textDocument/publishDiagnostics',
-      (param: string) => {
-        console.debug('Received notification: ' + param);
-        diagnosticCount++;
-      },
-    );
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
-    connection.onNotification('window/logMessage', (_: string) => {});
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
-    connection.onNotification((_: string) => {});
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
-    connection.onRequest((_: string) => {});
-
-    await connection.sendRequest('initialized', {});
-
-    for (let i = 0; i < 45; i++) {
-      console.debug('Waiting for diagnostics...');
-      if (diagnosticCount > 0) {
-        break;
-      }
-      await sleep(1000);
-    }
-
-    cli.kill(9);
   });
 });
