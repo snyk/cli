bbd851e4c68e025df3ffa0f21c39897eff8ea986
Luke Watts <luke@snyk.io>
2025-10-03 12:05:37 +0200

    chore(ci): migrate auth tests

--- test/smoke/auth.spec.ts
+++ test/smoke/auth.spec.ts
@@ -10,7 +10,34 @@ describe('Snyk CLI Authorization', () => {
       },
     });
 
-    expect(stdout).toContain('Snyk is missing auth token in order to run inside CI');
+    expect(stdout).toContain(
+      'Snyk is missing auth token in order to run inside CI',
+    );
     expect(stderr).toBe('');
+  });
+
+  it('fails if given bogus token', async () => {
+    const { stdout, stderr } = await runSnykCLI(
+      'auth 00000000-0000-0000-0000-000000000000',
+    );
+    expect(stdout).toContain(
+      'Authentication failed. Please check the API token',
+    );
+    expect(stderr).toBe('');
+  });
+
+  it('updates config file if given legit token', async () => {
+    const { stdout, stderr } = await runSnykCLI(
+      `auth ${process.env.TEST_SNYK_TOKEN}`,
+    );
+    expect(stdout).toContain(
+      'Your account has been authenticated. Snyk is now ready to be used.',
+    );
+    expect(stderr).toBe('');
+
+    const { stdout: cfgStdout, stderr: cfgStderr } =
+      await runSnykCLI('config get api');
+    expect(cfgStdout).toContain(process.env.TEST_SNYK_TOKEN);
+    expect(cfgStderr).toBe('');
   });
 });
--- test/smoke/jest.config.ts
+++ test/smoke/jest.config.ts
@@ -1,6 +1,16 @@
 import { createJestConfig } from '../createJestConfig';
 
 export default createJestConfig({
-    displayName: 'smoke',
-    ignoreDirectories: ['node_modules', 'dist', 'build', '.vscode', '.idea', '.git', '.nyc_output', '.turbo', '.cache'],
- });
+  displayName: 'smoke',
+  ignoreDirectories: [
+    'node_modules',
+    'dist',
+    'build',
+    '.vscode',
+    '.idea',
+    '.git',
+    '.nyc_output',
+    '.turbo',
+    '.cache',
+  ],
+});
