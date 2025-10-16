6f468dde18a073ca2947cbe4ef27aaaa06c5fae5
Luke Watts <luke@snyk.io>
2025-10-16 12:03:57 +0200

    test: iterate

--- test/jest/acceptance/exitcode.spec.ts
+++ test/jest/acceptance/exitcode.spec.ts
@@ -3,24 +3,23 @@ import { isWindowsOperatingSystem } from '../../utils';
 
 jest.setTimeout(1000 * 60);
 
-describe('exit code behaviour', () => {
-  if (isWindowsOperatingSystem()) {
-    // Address as part CLI-1207
-    return;
-  }
-  it.each([
-    { input: 0, expected: 0 },
-    { input: 1, expected: 1 },
-    { input: 2, expected: 2 },
-    { input: 3, expected: 3 },
-    { input: -1, expected: 2 },
-  ])(
-    'map legacy cli exit code $input to $expected',
-    async ({ input, expected }) => {
-      const { code } = await runSnykCLI(
-        `woof --exit-code=${input} --language=cat -d`,
-      );
-      expect(code).toEqual(expected);
-    },
-  );
-});
+if (!isWindowsOperatingSystem()) {
+  // Address as part CLI-1207
+  describe('exit code behaviour', () => {
+    it.each([
+      { input: 0, expected: 0 },
+      { input: 1, expected: 1 },
+      { input: 2, expected: 2 },
+      { input: 3, expected: 3 },
+      { input: -1, expected: 2 },
+    ])(
+      'map legacy cli exit code $input to $expected',
+      async ({ input, expected }) => {
+        const { code } = await runSnykCLI(
+          `woof --exit-code=${input} --language=cat -d`,
+        );
+        expect(code).toEqual(expected);
+      },
+    );
+  });
+}
