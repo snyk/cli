e647122dc469f425c45217172322031c9f1a153c
Thomas Schafer <thomasschafer97@gmail.com>
2026-02-06 15:32:21 +0000

    chore: update Go to 1.25 and golangci-lint to v2
    Co-Authored-By: Tom Brewster <brew42@hotmail.com>

--- scripts/install-swift.sh
+++ scripts/install-swift.sh
@@ -3,13 +3,13 @@ set -exuo pipefail
 
 pushd /usr/local/
   if [[ "$1" == "x86_64" ]]; then
-    FILE=swift-5.8.1-RELEASE-ubuntu20.04
-    curl --compressed --output swift.tar.gz https://download.swift.org/swift-5.8.1-release/ubuntu2004/swift-5.8.1-RELEASE/$FILE.tar.gz
+    FILE=swift-5.10.1-RELEASE-debian12
+    curl --compressed --output swift.tar.gz https://download.swift.org/swift-5.10.1-release/debian12/swift-5.10.1-RELEASE/$FILE.tar.gz
   fi
 
   if [[ "$1" == "aarch64" ]]; then
-    FILE=swift-5.8.1-RELEASE-ubuntu20.04-aarch64
-    curl --compressed --output swift.tar.gz https://download.swift.org/swift-5.8.1-release/ubuntu2004-"$1"/swift-5.8.1-RELEASE/$FILE.tar.gz
+    FILE=swift-5.10.1-RELEASE-debian12-aarch64
+    curl --compressed --output swift.tar.gz https://download.swift.org/swift-5.10.1-release/debian12-"$1"/swift-5.10.1-RELEASE/$FILE.tar.gz
   fi
   tar zxf swift.tar.gz
   ln -s "$FILE" swift
