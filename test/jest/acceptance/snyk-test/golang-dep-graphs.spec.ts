8aacfd02bf817794778fe8b50e9a402de9a3581f
Maximilian Combüchen <max.combuchen@snyk.io>
2026-03-20 15:35:57 +0100

    test: set up fake server in golang acceptance test

--- test/jest/acceptance/snyk-test/golang-dep-graphs.spec.ts
+++ test/jest/acceptance/snyk-test/golang-dep-graphs.spec.ts
@@ -1,17 +1,46 @@
 import type { DepGraphData } from '@snyk/dep-graph';
 
+import { fakeServer, getFirstIPv4Address } from '../../../acceptance/fake-server';
+import { getServerPort } from '../../util/getServerPort';
 import { runSnykCLI } from '../../util/runSnykCLI';
 import { createProjectFromWorkspace } from '../../util/createProject';
 
 jest.setTimeout(1000 * 60);
 
 describe('gomodules dep-graphs', () => {
+  let server: ReturnType<typeof fakeServer>;
+  let env: Record<string, any> = {};
+  const ipAddr = getFirstIPv4Address();
+  const port = getServerPort(process);
+  const baseApi = '/api/v1';
+
+  beforeAll((done) => {
+    env = {
+      ...process.env,
+      SNYK_API: 'http://' + ipAddr + ':' + port + baseApi,
+      SNYK_HOST: 'http://' + ipAddr + ':' + port,
+      SNYK_TOKEN: '123456789',
+    };
+    server = fakeServer(baseApi, 'snykToken');
+    server.setFeatureFlag('disableGoPackageUrlsInCli', false);
+    server.listen(port, done);
+  });
+
+  afterEach(() => {
+    server.restore();
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+    
   it('identifies replaced modules', async () => {
     const project = await createProjectFromWorkspace(
       'golang-gomodules-many-deps',
     );
     const { code, stdout } = await runSnykCLI('test --print-graph', {
       cwd: project.path(),
+      env,
     });
     expect(code).toEqual(0);
     expect(stdout).toBeDefined();
