7b7e1f380730deff0e9167e092d648c7b636a739
Will Rollason <william.rollason@snyk.io>
2026-04-10 14:09:50 +0100

    wip: add back + new test

--- test/jest/acceptance/snyk-test/unmanaged-pre-prod.spec.ts
+++ test/jest/acceptance/snyk-test/unmanaged-pre-prod.spec.ts
@@ -1,6 +1,10 @@
 import { describeIf } from '../../../utils';
-import { createProjectFromWorkspace } from '../../util/createProject';
+import {
+  createProjectFromFixture,
+  createProjectFromWorkspace,
+} from '../../util/createProject';
 import { runSnykCLI } from '../../util/runSnykCLI';
+import * as path from 'path';
 
 jest.setTimeout(1000 * 60 * 5);
 
@@ -51,5 +55,34 @@ describeIf(hasPreProdCredentials)('unmanaged pre-prod user journey', () => {
     expect(code).toEqual(1);
     expect(JSON.parse(stdout)).toEqual(expect.any(Array));
     expect(stdout).toContain('"purl": "pkg:generic/zlib@');
+  });
+
+  test('runs `snyk sbom --unmanaged --max-depth=1` against pre-prod', async () => {
+    const project = await createProjectFromFixture(
+      path.join('unmanaged', 'extraction'),
+    );
+    const { code, stdout, stderr } = await runSnykCLI(
+      `sbom --unmanaged --max-depth=1 --format=cyclonedx1.4+json --org=${process.env.TEST_SNYK_ORG_SLUGNAME} --debug`,
+      {
+        cwd: project.path(),
+        env,
+      },
+    );
+
+    if (code !== 0) {
+      console.debug(stderr);
+      console.debug('---------------------------');
+      console.debug(stdout);
+    }
+
+    expect(code).toEqual(0);
+
+    let sbom;
+    expect(() => {
+      sbom = JSON.parse(stdout);
+    }).not.toThrow();
+
+    expect(sbom.metadata.component.name).toEqual('root-node');
+    expect(sbom.components.length).toBeGreaterThanOrEqual(1);
   });
 });
