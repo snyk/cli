b2c8d32d21bf40ec2c9330f181a9eb2e52145c4c
Roberto López López <roberto.lopez-lopez@snyk.io>
2026-05-21 13:00:54 +0200

    feat: update CircleCI workflow and `createJestConfig` to support `TEST_SNYK_IGNORE_LIST` for selective test skipping

--- test/setup.js
+++ test/setup.js
@@ -63,6 +63,8 @@ module.exports = async function () {
       SNYK_VERSION +
       '\n Allow to skip tests [TEST_SNYK_DONT_SKIP_ANYTHING] ... ' +
       !isDontSkipTestsEnabled() +
+      '\n Ignore those test files [TEST_SNYK_IGNORE_LIST] ... ' +
+      process.env.TEST_SNYK_IGNORE_LIST +
       '\n Run FIPS tests      [TEST_SNYK_FIPS] ................. ' +
       fipsTestsEnabled() +
       '\n Organization        [TEST_SNYK_ORG_SLUGNAME] ......... ' +
